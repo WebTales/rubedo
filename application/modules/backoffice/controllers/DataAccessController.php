4e801893bf810cfe5237452ef0fe4b085296c3d2
Julien Bourdin <julien.bourdin@webtales.fr>
2012-07-24 20:29:25 +0200

    Minor adjustement

--- application/modules/backoffice/controllers/DataAccessController.php
+++ application/modules/backoffice/controllers/DataAccessController.php
@@ -5,41 +5,36 @@ use Rubedo\Mongo\DataAccess, Rubedo\Mongo;
 class Backoffice_DataAccessController extends Zend_Controller_Action
 {
 
-    public function init()
+    public function init ()
     {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
+    public function indexAction ()
     {
         $store = $this->getRequest()->getParam('store');
-
-
         
         $DataReader = new DataAccess($store);
         
         $dataStore = iterator_to_array($DataReader->find());
-       
         
-        $this->getHelper('Layout')->disableLayout();
-        $this->getHelper('ViewRenderer')->setNoRender();
-        $this->getResponse()->setHeader('Content-Type',
-                "application/json",true);
         
-
-        if(empty($dataStore)){
         
-            $oldStore = file_get_contents(APPLICATION_PATH.'/rubedo-backoffice-ui/www/data/'.$store.'.json');
+        if (empty($dataStore)) {
+                       
+            $oldStore = file_get_contents(
+                    APPLICATION_PATH . '/rubedo-backoffice-ui/www/data/' . $store .
+                             '.json');
             $this->getResponse()->setBody($oldStore);
-            /*$dataStore = json_decode(trim($oldStore),true);
-            var_dump($dataStore);die();*/
+            //$dataStore = Zend_Json::decode($oldStore);
+ 
         }else{
             $this->getResponse()->setBody(json_encode($dataStore));
         }
-        
-
+        $this->getHelper('Layout')->disableLayout();
+        $this->getHelper('ViewRenderer')->setNoRender();
+        $this->getResponse()->setHeader('Content-Type', "application/json", 
+                true);
     }
-
-
 }
 
