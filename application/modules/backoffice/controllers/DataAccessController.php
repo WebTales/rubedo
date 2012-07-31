e1897d2b968267e16825317d4dd40eab228f8e7f
Julien Bourdin <julien.bourdin@webtales.fr>
2012-07-31 16:38:14 +0200

    Generic handling of CRUD request

--- application/modules/backoffice/controllers/DataAccessController.php
+++ application/modules/backoffice/controllers/DataAccessController.php
@@ -16,7 +16,7 @@
 use Rubedo\Mongo\DataAccess, Rubedo\Mongo;
 
 /**
- * Controller dealing with the data access
+ * Controller providing CRUD API and dealing with the data access
  *
  * Receveive Ajax Calls for read & write from the UI to the Mongo DB
  *
@@ -30,40 +30,133 @@ class Backoffice_DataAccessController extends Zend_Controller_Action
 {
 
     /**
+     * Name of the store which is also to the collection name
+     *
+     * @var string
+     */
+    protected $_store;
+
+    /**
+     * Data Access Service
+     *
+     * @var DataAccess
+     */
+    protected $_dataReader;
+
+    /**
+     * Disable layout & rendering, set content type to json
+     * init the store parameter if transmitted
+     *
+     * @see Zend_Controller_Action::init()
+     */
+    public function init ()
+    {
+        // refuse write action not send by POST
+        if (! $this->getRequest()->isPost() && $this->getRequest()->getActionName() !== 'index') {
+            throw new \Exception('This action should be called by POST request');
+        }
+        
+        // set the store value from the request is sent
+        if (! isset($this->_store)) {
+            $this->_store = $this->getRequest()->getParam('store');
+        }
+        
+        // disable layout and set content type
+        $this->getHelper('Layout')->disableLayout();
+        $this->getHelper('ViewRenderer')->setNoRender();
+        $this->getResponse()->setHeader('Content-Type', "application/json", true);
+        
+        // init the data access service
+        $this->_dataReader = new DataAccess($this->_store);
+    }
+
+    /**
      * The default read Action
      *
-     * Return the content of the collection base on the "store" parameter
+     * Return the content of the collection, get filters from the request
+     * params
+     * 
+     * @todo remove the temp hack when database starter is ready
      */
     public function indexAction ()
     {
-        $store = $this->getRequest()
-            ->getParam('store');
         
-        $dataReader = new DataAccess($store);
         
-        $dataStore = $dataReader->find();
         
+        
+        $request = $this->getRequest();
+        
+        $page = $request->getParam('page', 1);
+        $start = $request->getParam('start', 0);
+        $limit = $request->getParam('limit', 25);
+        $group = $request->getParam('group', false);
+        if ($group) {
+            $group = Zend_Json::decode($group);
+        }
+        $sort = $request->getParam('sort', false);
+        if ($sort) {
+            $sort = Zend_Json::decode($sort);
+        }
+        
+        
+        $dataStore = $this->_dataReader->read();
+        
+        // temp hack to use the json files of the UI prototype
         if (empty($dataStore)) {
             
             $oldStore = file_get_contents(APPLICATION_PATH . '/rubedo-backoffice-ui/www/data/' . $store . '.json');
             $dataStore = Zend_Json::decode($oldStore);
             foreach ($dataStore as $data) {
-                $dataReader->insert($data, true);
+                $this->_dataReader->insert($data, true);
             }
-            $dataStore = $dataReader->find();
-            $this->getResponse()
-                ->setBody(json_encode(array_values($dataStore)));
+            $dataStore = $this->_dataReader->read();
         }
         
-        $this->getResponse()
-            ->setBody(json_encode(array_values($dataStore)));
+        // temp hack to test these option on a single store
+        if ($this->_store == 'PersonalPrefs') {
+            $reponse = array();
+            $reponse['data'] = array_values($dataStore);
+            $reponse['total'] = count($reponse['data']);
+            $reponse['success'] = TRUE;
+            $reponse['message'] = 'OK';
+            $this->getResponse()->setBody(json_encode($reponse));
+            return;
+        }
         
-        $this->getHelper('Layout')
-            ->disableLayout();
-        $this->getHelper('ViewRenderer')
-            ->setNoRender();
-        $this->getResponse()
-            ->setHeader('Content-Type', "application/json", true);
+        // return the data in a JSON content
+        $this->getResponse()->setBody(json_encode(array_values($dataStore)));
+    }
+
+    /**
+     * The destroy action of the CRUD API
+     */
+    public function destroyAction ()
+    {
+        $request = $this->getRequest();
+        $returnArray = array();
+        
+        Zend_Debug::dump($this->getRequest()->getParams());
+        Zend_Debug::dump($this->getRequest()->getPost());
+        Zend_Debug::dump($this->getRequest()->getRawBody());
+        
+        // $this -> getResponse() -> setBody(json_encode($returnArray));
+    }
+
+    /**
+     * The create action of the CRUD API
+     */
+    public function createAction ()
+    {
+        $request = $this->getRequest();
+        $returnArray = array();
+    }
+
+    /**
+     * The update action of the CRUD API
+     */
+    public function updateAction ()
+    {
+        $request = $this->getRequest();
+        $returnArray = array();
     }
 }
-
