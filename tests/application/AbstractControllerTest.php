faab3e0e40a42c87201f6ecbe97309f630833d7d
Julien Bourdin <julien.bourdin@webtales.Fr>
2012-08-21 14:45:15 +0200

    Test documentation

--- tests/application/AbstractControllerTest.php
+++ tests/application/AbstractControllerTest.php
@@ -1,15 +1,41 @@
 <?php
+/**
+ * Rubedo
+ *
+ * LICENSE
+ *
+ * yet to be written
+ *
+ * @category Rubedo-Test
+ * @package Rubedo-Test
+ * @copyright Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license yet to be written
+ * @version $Id$
+ */
 
+/**
+ * Abstract Controller Test Case
+ *
+ *
+ * @author jbourdin
+ * @category Rubedo-Test
+ * @package Rubedo-Test
+ */
 abstract class AbstractControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 {
-
-       public function setUp()
+	/**
+	 * Pre test context
+	 */
+    public function setUp()
     {
         $this->bootstrap = new Zend_Application('testing', APPLICATION_PATH . '/configs/application.ini');
 
         parent::setUp();
     }
 
+	/**
+	 * Post test cleaning for isolation
+	 */
     public function tearDown()
     {
         $this->resetRequest();
@@ -18,6 +44,4 @@ abstract class AbstractControllerTest extends Zend_Test_PHPUnit_ControllerTestCa
         parent::tearDown();
     }
 
-
 }
-
--- tests/application/backoffice/DataAccessControllerTest.php
+++ tests/application/backoffice/DataAccessControllerTest.php
@@ -1,8 +1,52 @@
 <?php
+/**
+ * Rubedo
+ *
+ * LICENSE
+ *
+ * yet to be written
+ *
+ * @category Rubedo-Test
+ * @package Rubedo-Test
+ * @copyright Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license yet to be written
+ * @version $Id$
+ */
 
+/**
+ * Back Office / Data Access Test Suite
+ *
+ *
+ * @author jbourdin
+ * @category Rubedo-Test
+ * @package Rubedo-Test
+ */
 class Backoffice_DataAccessControllerTest extends AbstractControllerTest
 {
 
+    /**
+     * Acceptance : Normal call of the read method
+     */
+    public function testDispatchRead()
+    {
+        $mockService = $this->getMock('Rubedo\Mongo\DataAccess');
+        $mockService->expects($this->once())->method('Read')->will($this->returnValue(array('id' => 1)));
+
+        Rubedo\Services\Manager::setMockService('MongoDataAccess', $mockService);
+
+        $front = Zend_Controller_Front::getInstance();
+        $front->setParam('noErrorHandler', true);
+
+        $this->dispatch('/backoffice/data-access/index/store/fake');
+
+        $this->assertModule('backoffice');
+        $this->assertController('data-access');
+        $this->assertAction('index');
+    }
+
+    /**
+     *  Acceptance : Legacy Call : redirect xxx.json call for store reading
+     */
     public function testDispatchJson()
     {
         $mockService = $this->getMock('Rubedo\Mongo\DataAccess');
@@ -12,31 +56,19 @@ class Backoffice_DataAccessControllerTest extends AbstractControllerTest
 
         $front = Zend_Controller_Front::getInstance();
         $front->setParam('noErrorHandler', true);
-		
+
         $this->dispatch('/backoffice/data/Blocs.json?_dc=1345541033897&page=1&start=0&limit=25');
-		
+
         $this->assertModule('backoffice');
         $this->assertController('data-access');
         $this->assertAction('index');
-    }
-	
-	public function testDispatchRead()
-    {
-        $mockService = $this->getMock('Rubedo\Mongo\DataAccess');
-        $mockService->expects($this->once())->method('Read')->will($this->returnValue(array('id' => 1)));
+        $this->assertEquals('Blocs', $this->getRequest()->getParam('store'));
 
-        Rubedo\Services\Manager::setMockService('MongoDataAccess', $mockService);
-
-        $front = Zend_Controller_Front::getInstance();
-        $front->setParam('noErrorHandler', true);
-		
-        $this->dispatch('/backoffice/data-access/index/store/fake');
-		
-        $this->assertModule('backoffice');
-        $this->assertController('data-access');
-        $this->assertAction('index');
     }
 
+    /**
+     * Acceptance : Normal call of the Delete method
+     */
     public function testDispatchDelete()
     {
         $mockService = $this->getMock('Rubedo\Mongo\DataAccess');
@@ -45,17 +77,20 @@ class Backoffice_DataAccessControllerTest extends AbstractControllerTest
         $this->getRequest()->setParam('data', json_encode(array('id' => 1)));
 
         Rubedo\Services\Manager::setMockService('MongoDataAccess', $mockService);
-		
+
         $front = Zend_Controller_Front::getInstance();
         $front->setParam('noErrorHandler', true);
-		
+
         $this->dispatch('/backoffice/data-access/delete/store/fake');
-		
+
         $this->assertModule('backoffice');
         $this->assertController('data-access');
         $this->assertAction('delete');
     }
 
+    /**
+     * Acceptance : Normal call of the Update method
+     */
     public function testDispatchUpdate()
     {
         $mockService = $this->getMock('Rubedo\Mongo\DataAccess');
@@ -63,32 +98,35 @@ class Backoffice_DataAccessControllerTest extends AbstractControllerTest
 
         $this->getRequest()->setParam('data', json_encode(array('id' => 1, 'content' => 'content')));
 
-		Rubedo\Services\Manager::setMockService('MongoDataAccess', $mockService);
-		
+        Rubedo\Services\Manager::setMockService('MongoDataAccess', $mockService);
+
         $front = Zend_Controller_Front::getInstance();
         $front->setParam('noErrorHandler', true);
-		
+
         $this->dispatch('/backoffice/data-access/update/store/fake');
-		
+
         $this->assertModule('backoffice');
         $this->assertController('data-access');
         $this->assertAction('update');
     }
 
+    /**
+     * Acceptance : Normal call of the Create method
+     */
     public function testDispatchCreate()
     {
-    	$mockService = $this->getMock('Rubedo\Mongo\DataAccess');
+        $mockService = $this->getMock('Rubedo\Mongo\DataAccess');
         $mockService->expects($this->once())->method('create')->will($this->returnValue(array('ok' => 1)));
 
         $this->getRequest()->setParam('data', json_encode(array('content' => 'content')));
 
-		Rubedo\Services\Manager::setMockService('MongoDataAccess', $mockService);
-		
+        Rubedo\Services\Manager::setMockService('MongoDataAccess', $mockService);
+
         $front = Zend_Controller_Front::getInstance();
         $front->setParam('noErrorHandler', true);
-		
+
         $this->dispatch('/backoffice/data-access/create/store/fake');
-		
+
         $this->assertModule('backoffice');
         $this->assertController('data-access');
         $this->assertAction('create');
