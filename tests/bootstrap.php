1f5d45949d57f88dd5f54d1f8d2101ef9f3a1115
Julien Bourdin <julien.bourdin@webtales.fr>
2012-08-22 01:19:00 +0200

    local path should not be commited !

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FIL
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
 
 // Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../Core'),'/Users/jbourdin/Projects/phactory/lib/',  realpath(APPLICATION_PATH . '/../library'), get_include_path())));
+set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../Core'),realpath(APPLICATION_PATH . '/../library'), get_include_path())));
 
 require_once 'Zend/Loader/Autoloader.php';
 Zend_Loader_Autoloader::getInstance();
