63c5ff76b177acddfd2a5550b74d21c675e3c15a
Julien Bourdin <julien@HP-Folio-13-Nicolas-Duvollet.(none)>
2012-09-26 11:42:04 +0200

    Fix naming choices

---
+++ application/modules/backoffice/controllers/FieldTypesController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Rubedo
+ *
+ * LICENSE
+ *
+ * yet to be written
+ *
+ * @category Rubedo
+ * @package Rubedo
+ * @copyright Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license yet to be written
+ * @version $Id$
+ */
+
+require_once('DataAccessController.php');
+ 
+/**
+ * Controller providing CRUD API for the themes JSON
+ *
+ * Receveive Ajax Calls for read & write from the UI to the Mongo DB
+ *
+ *
+ * @author jbourdin
+ * @category Rubedo
+ * @package Rubedo
+ *
+ */
+class Backoffice_FieldTypesController extends Backoffice_DataAccessController
+{
+    /**
+     * Name of the store which is also to the collection name
+     * 
+     * @see Backoffice_DataAccessController::$_store
+     * @var string
+     */
+    protected $_store = 'TypesChamps';
+
+}
