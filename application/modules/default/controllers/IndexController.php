2df33afa678f433307f0f682ccac0ffae0905d57
Julien Bourdin <jbourdin@gmail.com>
2012-10-16 13:52:49 +0200

    Init pageParams from UrlService

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -25,14 +25,25 @@
 class IndexController extends AbstractController
 {
 
+	/**
+	 * Current front office page parameters
+	 * @var array
+	 */
+	protected $_pageParams = array();
 
+	public function init(){
+		parent::init();
+		
+		$calledUri = $this->getRequest()->getRequestUri();
+		$serviceUrl =  Rubedo\Services\Manager::getService('Url');
+		$this->_pageParams = $serviceUrl->getPageInfo($calledUri);		
+	}
 
     /**
      * Main Action : render the Front Office view
      */
     public function indexAction()
     {
-        // action body
     }
 
 
