9ce5bb45d7b706cef8851e7ca50726cdbb47268d
Julien Bourdin <jbourdin@gmail.com>
2012-10-16 18:19:02 +0200

    Default controller invoking template rendering

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -22,8 +22,7 @@
  * @category Rubedo
  * @package Rubedo
  */
-class IndexController extends AbstractController
-{
+class IndexController extends AbstractController {
 
 	/**
 	 * Current front office page parameters
@@ -31,21 +30,49 @@ class IndexController extends AbstractController
 	 */
 	protected $_pageParams = array();
 
-	public function init(){
+	public function init() {
 		parent::init();
+
+		$this->_serviceUrl = Rubedo\Services\Manager::getService('Url');
+		$this->_serviceTemplate = Rubedo\Services\Manager::getService('FrontOfficeTemplates');
 		
-		$calledUri = $this->getRequest()->getRequestUri();
-		$serviceUrl =  Rubedo\Services\Manager::getService('Url');
-		$this->_pageParams = $serviceUrl->getPageInfo($calledUri);		
+		$defaultNamespace = new Zend_Session_Namespace('Default');
+		$lang = $defaultNamespace->lang;
+		$this->_serviceTemplate->init($lang);
+
 	}
 
-    /**
-     * Main Action : render the Front Office view
-     */
-    public function indexAction()
-    {
-    }
+	/**
+	 * Main Action : render the Front Office view
+	 */
+	public function indexAction() {
+		
+		
+		$this->getHelper('ViewRenderer')->setNoRender();
+		$this->getHelper('Layout')->disableLayout();
 
+		$calledUri = $this->getRequest()->getRequestUri();
+		$this->_pageParams = $this->_serviceUrl->getPageInfo($calledUri);
+		
+		$defaultNamespace = new Zend_Session_Namespace('Default');
+		$lang = $defaultNamespace->lang;
+		
+		
+		$twigVar = array();
+		$twigVar['theme'] = $defaultNamespace->themeCSS;
+		$twigVar['lang'] = $lang;
+		
+		foreach($this->_pageParams['blocks'] as $block) {
+			$helper= 'helper'.$block['Module'];
+			$output = $block['Output'];
+			$input = $block['Input'];
+			$twigVar[$output] = $this->_helper->$helper($input);
+		}
+		
+		$content = $this->_serviceTemplate->render($this->_pageParams['template'], $twigVar);
+	
+		$this->getResponse()->appendBody($content, 'default');
+
+	}
 
 }
-
