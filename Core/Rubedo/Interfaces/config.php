bf5698d39536ff52dd51cec849e80c6c266916a8
Julien Bourdin <jbourdin@gmail.com>
2012-10-17 16:38:15 +0200

    Add a block service

--- Core/Rubedo/Interfaces/config.php
+++ Core/Rubedo/Interfaces/config.php
@@ -29,13 +29,14 @@ class config
      * @var array array service name => interface name
      */
     protected static $_interfaceArray = array(
-    'MongoDataAccess' 		=> 	'Rubedo\\Interfaces\\Mongo\\IDataAccess', 
-    'Acl'					=>	'Rubedo\\Interfaces\\Acl\\IAcl',
-	'CurrentUser'			=>	'Rubedo\\Interfaces\\User\\ICurrentUser',
-	'CurrentTime'			=>	'Rubedo\\Interfaces\\Time\\ICurrentTime',
-	'Url'					=>	'Rubedo\\Interfaces\\Router\\IUrl',
-	'PageInfo'					=>	'Rubedo\\Interfaces\\Router\\IPageInfo',
-	'FrontOfficeTemplates'	=>	'Rubedo\Interfaces\Templates\IFrontOfficeTemplates',
+	    'MongoDataAccess' 		=> 	'Rubedo\\Interfaces\\Mongo\\IDataAccess', 
+	    'Acl'					=>	'Rubedo\\Interfaces\\Acl\\IAcl',
+		'CurrentUser'			=>	'Rubedo\\Interfaces\\User\\ICurrentUser',
+		'CurrentTime'			=>	'Rubedo\\Interfaces\\Time\\ICurrentTime',
+		'Url'					=>	'Rubedo\\Interfaces\\Router\\IUrl',
+		'PageInfo'				=>	'Rubedo\\Interfaces\\Router\\IPageInfo',
+		'FrontOfficeTemplates'	=>	'Rubedo\\Interfaces\\Templates\\IFrontOfficeTemplates',
+		'Block'					=>	'Rubedo\\Interfaces\\Content\\IBlock',
 	);
 
     /**
