8cf713c27a4eed53fdb781033357e7f469188c11
mickael goncalves <mickael.goncalves@webtales.fr>
2012-10-25 10:12:03 +0200

    Fix changePassword function fail

--- application/modules/backoffice/controllers/UsersController.php
+++ application/modules/backoffice/controllers/UsersController.php
@@ -43,47 +43,43 @@ class Backoffice_UsersController extends Backoffice_DataAccessController {
     private $_collection;
 
 	public function changePasswordAction(){
-		$data = $this -> getRequest() -> getParam('data');
-		
 		$hashService = \Rubedo\Services\Manager::getService('Hash');
 		
-		if (!is_null($data)) {
-			$insertData = Zend_Json::decode($data);
-			if (is_array($insertData)) {
-				$password = $insertData['password'];
-				$salt = rand();
-				unset($insertData['leaf']);
-				
-				$password = $hashService->derivatePassword($password, $salt);
-				
-				$insertData['password'] = $password;
-
-		        if(!isset($insertData['version'])){
-		        	$insertData['version'] = 1;
-		        } else {
-		        	$insertData['version'] += 1;
-		        }
+		$password = $_POST['password'];
+		$id = $_POST['id'];
+		$salt = rand();
 		
-		        $resultArray = $this->_collection->insert($insertData, array("safe" => $safe));
-		        if ($resultArray['ok'] == 1) {
-		            $insertData['id'] = (string)$insertData['_id'];
-		            unset($insertData['_id']);
-		            $returnArray = array('success' => true);
-		        } else {
-		            $returnArray = array('success' => false);
-		        }
-		
-		        return $returnArray;
+		if (!empty($password) && !empty($id)) {
+			$password = $hashService->derivatePassword($password, $salt);
+			
+			$insertData['password'] = $password;
+			$insertData['salt'] = $salt;
+			$insertData['_id'] = $id;
 
-			} else {
-				$returnArray = array('success' => false, "msg" => 'Not an array');
-			}
+	        if(!isset($insertData['version'])){
+	        	$insertData['version'] = 1;
+	        } else {
+	        	$insertData['version'] += 1;
+	        }
+	
+	        $resultArray = $this->_collection->insert($insertData, array("safe" => $safe));
+	        if ($resultArray['ok'] == 1) {
+	            $insertData['id'] = (string)$insertData['_id'];
+	            unset($insertData['_id']);
+	            $returnArray = array('success' => true);
+	        } else {
+	            $returnArray = array('success' => false);
+	        }
+	
+	        return $returnArray;
 		} else {
 			$returnArray = array('success' => false, "msg" => 'No Data');
 		}
+		
 		if (!$returnArray['success']) {
 			$this -> getResponse() -> setHttpResponseCode(500);
 		}
+		
 		return $this->_helper->json($returnArray);
 	}
 
