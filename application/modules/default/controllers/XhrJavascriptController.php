e83d2175081082acab732584e491f38d113ec14e
Julien Bourdin <jbourdin@gmail.com>
2012-10-30 16:03:12 +0100

    Use Correct Twig Service

--- application/modules/default/controllers/XhrJavascriptController.php
+++ application/modules/default/controllers/XhrJavascriptController.php
@@ -1,13 +1,26 @@
 <?php
 
-class JavascriptController extends Zend_Controller_Action
+class XhrJavascriptController extends Zend_Controller_Action
 {
 	
-    public function getAction()
+    public function getScriptAction()
     {
+    	$this->_serviceTemplate = Rubedo\Services\Manager::getService('FrontOfficeTemplates');
+		$session = Rubedo\Services\Manager::getService('Session');
+        $lang = $session->get('lang','fr');
+		$this->_serviceTemplate->init($lang);
+		
     	$script = 'javascript/'.$this->_getParam('script').'.js';
 		$twigVar = array();
-        $this->twig($script,$twigVar);
+		
+
+		$content = $this->_serviceTemplate->render($script, $twigVar);
+
+
+		$this->getHelper('ViewRenderer')->setNoRender();
+		$this->getHelper('Layout')->disableLayout();
+		
+        $this->getResponse()->appendBody($content, 'default');
     }
 	
 }
