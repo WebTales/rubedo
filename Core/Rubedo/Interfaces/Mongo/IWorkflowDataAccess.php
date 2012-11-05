58bb1a7fd623f293ed95558227a35e2d5aaf5298
mickael goncalves <mickael.goncalves@webtales.fr>
2012-11-05 14:57:47 +0100

    add workflow data access service and its test (still in development)

---
+++ Core/Rubedo/Interfaces/Mongo/IWorkflowDataAccess.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Rubedo
+ *
+ * LICENSE
+ *
+ * yet to be written
+ *
+ * @category Rubedo
+ * @package Rubedo
+ * @copyright Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license yet to be written
+ * @version $Id$
+ */
+namespace Rubedo\Interfaces\Mongo;
+
+/**
+ * Interface of data access services
+ *
+ *
+ * @author jbourdin
+ * @category Rubedo
+ * @package Rubedo
+ */
+interface IWorkflowDataAccess extends IDataAccess{
+	/**
+	 * 
+	 * @todo write the doc !
+	 */
+	public function publish($objectId);
+}
