96ebb6188d86af674d2cb4420c019fa19466484d
Julien Bourdin <jbourdin@gmail.com>
2012-11-09 18:17:20 +0100

    Working grid render for contents

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -66,37 +66,46 @@ class IndexController extends Zend_Controller_Action
         $this->_serviceBlock = Rubedo\Services\Manager::getService('Block');
 
         $session = Rubedo\Services\Manager::getService('Session');
-        $lang = $session->get('lang','fr');
-		$this->_serviceTemplate->init($lang);
+        $lang = $session->get('lang', 'fr');
+        $this->_serviceTemplate->init($lang);
 
         $calledUri = $this->getRequest()->getRequestUri();
         $pageId = $this->_serviceUrl->getPageId($calledUri);
         $this->_pageParams = $this->_servicePage->getPageInfo($pageId);
 
         $twigVar = array();
-        $twigVar['theme'] = $session->get('themeCSS','default');
+        $twigVar['theme'] = $session->get('themeCSS', 'default');
         $twigVar['lang'] = $lang;
 
         foreach ($this->_pageParams['blocks'] as $block) {
             $twigVar = array_merge($twigVar, $this->_serviceBlock->getBlockData($block, $pageId, $this));
         }
-		
-		if($pageId == "newpage"){
-			$newTwigVar['structure'] = array(
-											array('template'=>'root/blocks/carrousel.html','data'=>array('items'=>$twigVar['carousel_content'])),
-											array('template'=>'root/blocks/contentlist.html','data'=>array('contents'=>$twigVar['contentlist_content'])),
-											);
-			$twigVar  = $newTwigVar;
-		}
-		
-		//Zend_Debug::dump($twigVar);
+
+        if ($pageId == "newpage") {
+            $newTwigVar['structure'] = array();
+            $newTwigVar['structure'][] = array('nested' => array(1 => array('params' => array('span' => 9), 'nested' => array( array('template' => 'root/blocks/hero.html', 'data' => array()))), 2 => array('params' => array('span' => 3), 'nested' => array( array('template' => 'root/blocks/carrousel.html', 'data' => array('items' => $twigVar['carousel_content']))))));
+            $newTwigVar['structure'][] = array('nested' => array(1 => array('params' => array('span' => 12), 'nested' => array( array('template' => 'root/blocks/contentlist.html', 'data' => array('contents' => $twigVar['contentlist_content'])))), ));
+			
+			$newTwigVar['theme'] = $twigVar['theme'];
+			
+            /*
+             $newTwigVar['structure'] = array(
+             array('template'=>'root/blocks/carrousel.html','data'=>array('items'=>$twigVar['carousel_content'])),
+             array('template'=>'root/blocks/contentlist.html','data'=>array('contents'=>$twigVar['contentlist_content'])),
+             );
+
+             */
+            $twigVar = $newTwigVar;
+        }
+
+        //Zend_Debug::dump($twigVar,$this->_pageParams['template']);
         //die();
+
         $content = $this->_serviceTemplate->render($this->_pageParams['template'], $twigVar);
 
+        $this->getHelper('ViewRenderer')->setNoRender();
+        $this->getHelper('Layout')->disableLayout();
 
-		$this->getHelper('ViewRenderer')->setNoRender();
-		$this->getHelper('Layout')->disableLayout();
-		
         $this->getResponse()->appendBody($content, 'default');
 
     }
