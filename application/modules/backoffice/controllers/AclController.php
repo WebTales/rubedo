8b132238a3a4d595677071718c0b48db351d40db
Julien Bourdin <jbourdin@gmail.com>
2012-11-13 13:58:31 +0100

    add an ACL controller for UI right management

---
+++ application/modules/backoffice/controllers/AclController.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Rubedo
+ *
+ * LICENSE
+ *
+ * yet to be written
+ *
+ * @category Rubedo
+ * @package Rubedo
+ * @copyright Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license yet to be written
+ * @version $Id$
+ */
+
+use Rubedo\Mongo\DataAccess, Rubedo\Mongo, Rubedo\Services;
+
+/**
+ * Controller providing access control list
+ *
+ * Receveive Ajax Calls with needed ressources, send true or false for each of them
+ *
+ *
+ * @author jbourdin
+ * @category Rubedo
+ * @package Rubedo
+ *
+ */
+class Backoffice_AclController extends Zend_Controller_Action
+{
+	/**
+     * should json be prettified
+     *
+     * @var bool
+     */
+    protected $_prettyJson = true;
+	
+	/**
+     * Set the response body with Json content
+     * Option : json is made human readable
+     * @param mixed $data data to be json encoded
+     */
+    protected function _returnJson($data) {
+        // disable layout and set content type
+        $this->getHelper('Layout')->disableLayout();
+        $this->getHelper('ViewRenderer')->setNoRender();
+        $this->getResponse()->setHeader('Content-Type', "application/json", true);
+
+        $returnValue = Zend_Json::encode($data);
+        if ($this->_prettyJson) {
+            $returnValue = Zend_Json::prettyPrint($returnValue);
+        }
+        $this->getResponse()->setBody($returnValue);
+    }
+	
+	function indexAction(){
+		$AclArray = array();
+		$dataJson = $this->getRequest()->getParam('data');
+		if(isset($dataJson)){
+			$dataArray = Zend_Json::decode($dataJson);
+			foreach($dataArray as $key => $value){
+				$AclArray[$key] = true;
+			}
+		}
+		
+		return $this->_returnJson($AclArray);
+	}
+}
