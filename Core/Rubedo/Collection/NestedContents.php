99ea8a03378a4602727f7e23c2d2c91441010b1c
Julien Bourdin <jbourdin@gmail.com>
2012-11-14 17:27:34 +0100

    More about nested contents interface

--- Core/Rubedo/Collection/NestedContents.php
+++ Core/Rubedo/Collection/NestedContents.php
@@ -25,42 +25,70 @@ use Rubedo\Interfaces\Collection\INestedContents;
  */
 class NestedContents implements INestedContents
 {
-	/**
-	 * name of the collection
-	 * 
-	 * @var string
-	 */
+    /**
+     * name of the collection
+     *
+     * @var string
+     */
     protected $_collectionName;
 
-	/**
-	 * data access service
-	 * 
-	 * @var\Rubedo\Mongo\DataAccess
-	 */
+    /**
+     * data access service
+     *
+     * @var\Rubedo\Mongo\DataAccess
+     */
     protected $_dataService;
-	
-	protected function _init() {
+
+    public function __construct() {
         // init the data access service
         $this->_collectionName = 'Contents';
         $this->_dataService = Manager::getService('MongoDataAccess');
         $this->_dataService->init($this->_collectionName);
     }
 
-    public function __construct() {
-        $this->_init();
-    }
-	
-	/**
+    /**
      * Do a find request on nested contents of a given content
      *
-	 * @param string $parentContentId parent id of nested contents
-	 * @param array $filters filter the list with mongo syntax
-	 * @param array $sort sort the list with mongo syntax
+     * @param string $parentContentId parent id of nested contents
+     * @param array $filters filter the list with mongo syntax
+     * @param array $sort sort the list with mongo syntax
      * @return array
      */
-    public function getList($parentContentId,$filters = null, $sort = null){
-    	
-		
+    public function getList($parentContentId, $filters = null, $sort = null) {
+
     }
-	
+
+    /**
+     * Create an objet in the current collection
+     *
+     * @param string $parentContentId parent id of nested contents
+     * @param array $obj data object
+     * @param bool $safe should we wait for a server response
+     * @return array
+     */
+    public function create($parentContentId, array $obj, $safe = true) {
+    }
+
+    /**
+     * Update an objet in the current collection
+     *
+     * @param string $parentContentId parent id of nested contents
+     * @param array $obj data object
+     * @param bool $safe should we wait for a server response
+     * @return array
+     */
+    public function update($parentContentId, array $obj, $safe = true) {
+    }
+
+    /**
+     * Delete objets in the current collection
+     *
+     * @param string $parentContentId parent id of nested contents
+     * @param array $obj data object
+     * @param bool $safe should we wait for a server response
+     * @return array
+     */
+    public function destroy($parentContentId, array $obj, $safe = true) {
+    }
+
 }
--- Core/Rubedo/Interfaces/Collection/INestedContents.php
+++ Core/Rubedo/Interfaces/Collection/INestedContents.php
@@ -22,16 +22,47 @@ namespace Rubedo\Interfaces\Collection;
  * @category Rubedo
  * @package Rubedo
  */
-interface INestedContents {
-	
-	/**
+interface INestedContents
+{
+
+    /**
      * Do a find request on nested contents of a given content
      *
-	 * @param string $parentContentId parent id of nested contents
-	 * @param array $filters filter the list with mongo syntax
-	 * @param array $sort sort the list with mongo syntax
+     * @param string $parentContentId parent id of nested contents
+     * @param array $filters filter the list with mongo syntax
+     * @param array $sort sort the list with mongo syntax
      * @return array
      */
-    public function getList($parentContentId,$filters = null, $sort = null);
-	
+    public function getList($parentContentId, $filters = null, $sort = null);
+
+    /**
+     * Create an objet in the current collection
+     *
+     * @param string $parentContentId parent id of nested contents
+     * @param array $obj data object
+     * @param bool $safe should we wait for a server response
+     * @return array
+     */
+    public function create($parentContentId, array $obj, $safe = true);
+
+    /**
+     * Update an objet in the current collection
+     *
+     * @param string $parentContentId parent id of nested contents
+     * @param array $obj data object
+     * @param bool $safe should we wait for a server response
+     * @return array
+     */
+    public function update($parentContentId, array $obj, $safe = true);
+
+    /**
+     * Delete objets in the current collection
+     *
+     * @param string $parentContentId parent id of nested contents
+     * @param array $obj data object
+     * @param bool $safe should we wait for a server response
+     * @return array
+     */
+    public function destroy($parentContentId, array $obj, $safe = true);
+
 }
