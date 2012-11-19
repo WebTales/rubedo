a7bdaa3aa3fde0aba4125cc1009d4c7c367d198c
mickael goncalves <mickael.goncalves@webtales.fr>
2012-11-19 10:46:53 +0100

    Creation of the test class for abstract collection

---
+++ tests/Core/Rubedo/Collection/AbstractCollectionTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Rubedo
+ *
+ * LICENSE
+ *
+ * yet to be written
+ *
+ * @category Rubedo-Test
+ * @package Rubedo-Test
+ * @copyright Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license yet to be written
+ * @version $Id$
+ */
+
+/**
+ * Test suite of the collection service :
+ * @author jbourdin
+ * @category Rubedo-Test
+ * @package Rubedo-Test
+ */
+abstract class AbstractCollectionTest implements PHPUnit_Framework_TestCase
+{
+	
+}
