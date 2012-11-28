8c2748c7810e0d599667e9383ccb0b13a929d7c6
Julien Bourdin <julien.bourdin@webtales.fr>
2012-11-28 09:48:33 +0100

    add getFilePath and getTemplateDir

--- Core/Rubedo/Templates/FrontOfficeTemplates.php
+++ Core/Rubedo/Templates/FrontOfficeTemplates.php
@@ -38,22 +38,26 @@ class FrontOfficeTemplates implements  IFrontOfficeTemplates
      */
     protected $_options = array();
 
+    protected static $templateDir = null;
+    
+    protected static $_currentTheme = null;
+
     /**
      * initialise Twig Context
      * @param string $lang current language
      */
-    public function init($lang = 'default') {
+    public function init($lang = 'default')
+    {
         $this->_options = array('templateDir' => APPLICATION_PATH . "/../data/templates", 'cache' => APPLICATION_PATH . "/../cache/twig", 'debug' => true, 'auto_reload' => true);
-        if(isset($this->_service)) {
+        if (isset($this->_service)) {
             $this->_options = $this->_service->getCurrentOptions();
         }
 
         $loader = new \Twig_Loader_Filesystem($this->_options['templateDir']);
         $this->_twig = new \Twig_Environment($loader, $this->_options);
-		
-		$this->_twig->addExtension(new \Twig_Extension_Debug());
-		
-		
+
+        $this->_twig->addExtension(new \Twig_Extension_Debug());
+
         $this->_twig->addExtension(new \Twig_Extension_Translate($lang));
 
         $this->_twig->addExtension(new \Twig_Extension_Highlight());
@@ -67,82 +71,120 @@ class FrontOfficeTemplates implements  IFrontOfficeTemplates
      * @param array $vars array of data to be rendered
      * @return string HTML produced by twig
      */
-    public function render($template, array $vars) {
+    public function render($template, array $vars)
+    {
         $templateObj = $this->_twig->loadTemplate($template);
         return $templateObj->render($vars);
     }
-	
-	public static function parseJson($json, $template, $id)
+
+    public function getTemplateDir()
     {
-    	$masque = json_decode(file_get_contents($json), TRUE);
-		
-		$found = false;
-		foreach ($masque as $key => $val) {
-			if ($val['id'] == $id) {
-				$found = true;
-				$rows = $val['rows'];
-				break;
-			}
-		}
-		
-		if ($found) {
-			$out = '<div class="container-fluid">'."\n";
-			$out .= self::parseRows($rows,1);
-			$out .= '</div>'."\n";
-			
-			$file = fopen($template, 'w+');
-	 
-			fputs($file, $out);
-			 
-			fclose($file);
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	/**
+        if (!isset(self::$templateDir)) {
+            $this->_options = array('templateDir' => APPLICATION_PATH . "/../public/themes", 'cache' => APPLICATION_PATH . "/../cache/twig", 'debug' => true, 'auto_reload' => true);
+            if (isset($this->_service)) {
+                $this->_options = $this->_service->getCurrentOptions();
+            }
+
+            self::$templateDir = $this->_options['templateDir'];
+        }
+        return self::$templateDir;
+    }
+
+    public function getFileThemePath($path)
+    {
+        if (is_file($this->getTemplateDir() . '/' . $this->getCurrentTheme() . '/' . $path)) {
+            return '/' . $this->getCurrentTheme() . '/' . $path;
+        } else {
+            return '/default/' . $path;
+        }
+    }
+    
+    public function getCurrentTheme(){
+        if (!isset(self::$templateDir)) {
+
+            self::$_currentTheme = 'default';
+        }
+        return self::$_currentTheme;
+    }
+
+    public function setCurrentTheme($theme){
+        self::$_currentTheme = $theme;
+    }
+
+    public static function parseJson($json, $template, $id)
+    {
+        $masque = json_decode(file_get_contents($json), TRUE);
+
+        $found = false;
+        foreach ($masque as $key => $val) {
+            if ($val['id'] == $id) {
+                $found = true;
+                $rows = $val['rows'];
+                break;
+            }
+        }
+
+        if ($found) {
+            $out = '<div class="container-fluid">' . "\n";
+            $out .= self::parseRows($rows, 1);
+            $out .= '</div>' . "\n";
+
+            $file = fopen($template, 'w+');
+
+            fputs($file, $out);
+
+            fclose($file);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * Parses a rows
-     * 
-	 * @param rows rows to parse
-	 * @param t number of tabulations
-	 * @return parsed content
-     */	
-	public static function parseRows($rows,$t=0) {
-		$out = '';
-		foreach ($rows as $key => $val) {
-			$out .= self::tab($t).'<div class="row-fluid">'."\n";
-			$t++;
-			$cols = $val['columns'];
-			foreach ($cols as $key => $val) {
-				// in a column
-				$out .= self::tab($t).'<div class="';
-				$t++;
-				if (isset($val['offset']) && $val['offset'] <> '0') {
-					$out .= 'offset'.$val['offset'].' ';
-				}
-				$out .= 'span'.$val['span'].'">'."\n";
-				if (isset($val['rows']) && $val['rows'] != null) {
-					// are there any rows in this column ?
-					$out .= self::parseRows($val['rows'],$t);
-				} else {
-					// put a block here
-					// TODO : block identifier
-					$out .= self::tab($t).'{% block zone_x %}{% endblock %}'."\n";
-				}
-				$t--;
-				$out .= self::tab($t).'</div>'."\n";
-			}
-			$t--;
-			$out .= self::tab($t).'</div>'."\n";
-		}
-		return $out;
-	}
-	
-	public static function tab($n) {
-		$out = '';
-		for ($i = 0; $i < $n; $i++) $out.="\t";
-		return $out;
-	}
+     *
+     * @param rows rows to parse
+     * @param t number of tabulations
+     * @return parsed content
+     */
+    public static function parseRows($rows, $t = 0)
+    {
+        $out = '';
+        foreach ($rows as $key => $val) {
+            $out .= self::tab($t) . '<div class="row-fluid">' . "\n";
+            $t++;
+            $cols = $val['columns'];
+            foreach ($cols as $key => $val) {
+                // in a column
+                $out .= self::tab($t) . '<div class="';
+                $t++;
+                if (isset($val['offset']) && $val['offset'] <> '0') {
+                    $out .= 'offset' . $val['offset'] . ' ';
+                }
+                $out .= 'span' . $val['span'] . '">' . "\n";
+                if (isset($val['rows']) && $val['rows'] != null) {
+                    // are there any rows in this column ?
+                    $out .= self::parseRows($val['rows'], $t);
+                } else {
+                    // put a block here
+                    // TODO : block identifier
+                    $out .= self::tab($t) . '{% block zone_x %}{% endblock %}' . "\n";
+                }
+                $t--;
+                $out .= self::tab($t) . '</div>' . "\n";
+            }
+            $t--;
+            $out .= self::tab($t) . '</div>' . "\n";
+        }
+        return $out;
+    }
+
+    public static function tab($n)
+    {
+        $out = '';
+        for ($i = 0; $i < $n; $i++)
+            $out .= "\t";
+        return $out;
+    }
 
 }
