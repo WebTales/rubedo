75a18f6b5fccbca5d5082096c1f064025726df34
Julien Bourdin <jbourdin@gmail.com>
2012-12-04 10:58:03 +0100

    refactore : use derivate password instead of Copy/Paste

--- Core/Rubedo/Security/Hash.php
+++ Core/Rubedo/Security/Hash.php
@@ -25,26 +25,27 @@ use Rubedo\Interfaces\Security\IHash;
  * @category Rubedo
  * @package Rubedo
  */
-class Hash implements IHash {
-	
-	/**
-	 * Contain the current algo for the hash
-	 */
+class Hash implements IHash
+{
+
+    /**
+     * Contain the current algo for the hash
+     */
     private $_algo = 'sha512';
 
     /**
      * Hash the string given in parameter
      *
      * @param $string is the string destined to be hashed
-	 * @param $salt is the string hashed with the string
+     * @param $salt is the string hashed with the string
      *
      * @return $hash The string hashed
      */
     public function hashString($string, $salt) {
-		if (gettype($string) !== 'string'){
-			throw new \Rubedo\Exceptions\Hash('$string should be a string');
-		}
-		
+        if (gettype($string) !== 'string') {
+            throw new \Rubedo\Exceptions\Hash('$string should be a string');
+        }
+
         $hash = hash($this->_algo, $salt . $string);
 
         return $hash;
@@ -59,9 +60,9 @@ class Hash implements IHash {
      * @return $hash password hashed
      */
     public function derivatePassword($password, $salt) {
-		if (gettype($password) !== 'string'){
-			throw new \Rubedo\Exceptions\Hash('$password should be a string');
-		}
+        if (gettype($password) !== 'string') {
+            throw new \Rubedo\Exceptions\Hash('$password should be a string');
+        }
 
         for ($i = 0; $i < 10; $i++) {
             $password = hash($this->_algo, $salt . $password);
@@ -81,13 +82,11 @@ class Hash implements IHash {
      * @return bool
      */
     public function checkPassword($hash, $password, $salt) {
-    	if (gettype($password) !== 'string'){
-			throw new \Rubedo\Exceptions\Hash('$password should be a string');
-		}
-		
-        for ($i = 0; $i < 10; $i++) {
-            $password = hash($this->_algo, $salt . $password);
+        if (gettype($password) !== 'string') {
+            throw new \Rubedo\Exceptions\Hash('$password should be a string');
         }
+
+        $password = $this->derivatePassword($password, $salt);
 
         if ($password === $hash) {
             return true;
@@ -96,18 +95,18 @@ class Hash implements IHash {
         }
     }
 
+    public function generateRandomString($length = 10) {
+        // Create a random string for the salt
+        $characters = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmonopqrstuvwxyz123456789');
+        $nbChar = count($characters);
+        //shuffle($caracters);
+        for ($i = 0; $i < $length; $i++) {
+            $resultArray[] = $characters[rand(0, $nbChar - 1)];
+        }
+        //$caracters = array_slice($caracters, 0, $length);
+        $salt = implode('', $resultArray);
 
-	public function generateRandomString($length = 10){
-		// Create a random string for the salt
-		$characters = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmonopqrstuvwxyz123456789');
-		$nbChar = count($characters);
-	    //shuffle($caracters);
-		for($i = 0;$i < $length;$i++){
-			$resultArray[]= $characters[rand(0,$nbChar-1)];
-		}
-	    //$caracters = array_slice($caracters, 0, $length);
-	    $salt = implode('', $resultArray);
-		
-		return $salt;
-	}
+        return $salt;
+    }
+
 }
