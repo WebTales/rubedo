44e7e8f05365ac2c4784a8b765d85f4c1308d074
Nicolas Duvollet <duvolletn@HP-Folio-13-Nicolas-Duvollet.(none)>
2012-12-05 13:12:49 +0100

    Adding function to save live changes

--- application/modules/default/controllers/XhrEditController.php
+++ application/modules/default/controllers/XhrEditController.php
@@ -20,34 +20,53 @@
  * @category Rubedo
  * @package Rubedo
  */
-class XhrEditController extends Zend_Controller_Action
-{
-    /**
-     * variable for the Session service
-     *
-     * @param 	Rubedo\Interfaces\User\ISession
-     */
-    protected $_session;
+class XhrEditController extends Zend_Controller_Action {
+	/**
+	 * variable for the Session service
+	 *
+	 * @param 	Rubedo\Interfaces\User\ISession
+	 */
+	protected $_session;
 
-    /**
-     * Init the session service
-     */
-    public function init() {
-        $this->_session = Rubedo\Services\Manager::getService('Session');
-    }
+	/**
+	 * variable for the Data service
+	 *
+	 * @param 	Rubedo\Interfaces\User\ISession
+	 */
+	protected $_dataService;
 
-    /**
-     * Allow to define the current theme
-     */
-    public function indexAction() {
-    	$returnArray = array();
-		$returnArray['success'] =  false;
-		
-		
-        if (!$returnArray['success']) {
-            $this->getResponse()->setHttpResponseCode(500);
-        }
-		return $this->_helper->json($returnArray);
-    }
+	/**
+	 * Init the session service
+	 */
+	public function init() {
+		$this -> _dataService = Rubedo\services\Manager::getService('Contents');
+	}
+
+	/**
+	 * Allow to define the current theme
+	 */
+	public function indexAction() {
+
+		$contentId = $this -> getRequest() -> getParam('id');
+		$data = $this -> getRequest() -> getParam('data');
+		if (!empty($contentId['id'])) {
+			$contentId = explode("_", $contentId);
+			$id = $contentId[0];
+			$field = $contentId[1];
+
+			$baseData = $this -> _dataService -> findById($id, true, false);
+			$baseData['fields'][$field] = $data;
+			$returnArray = $this -> _dataService -> update($baseData, true, true);
+
+		} else {
+			$returnArray['success'] = false;
+			$returnArray['msg'] = 'No content id given.';
+		}
+		if (!$returnArray['success']) {
+			$this -> getResponse() -> setHttpResponseCode(500);
+		}
+		return $this -> _helper -> json($returnArray);
+
+	}
 
 }
