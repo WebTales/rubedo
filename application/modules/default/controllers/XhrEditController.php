b73d64414a892892c9627ed52b819746a98bc796
Nicolas Duvollet <duvolletn@HP-Folio-13-Nicolas-Duvollet.(none)>
2012-12-05 15:23:23 +0100

    format php code

--- application/modules/default/controllers/XhrEditController.php
+++ application/modules/default/controllers/XhrEditController.php
@@ -20,53 +20,54 @@
  * @category Rubedo
  * @package Rubedo
  */
-class XhrEditController extends Zend_Controller_Action {
-	/**
-	 * variable for the Session service
-	 *
-	 * @param 	Rubedo\Interfaces\User\ISession
-	 */
-	protected $_session;
+class XhrEditController extends Zend_Controller_Action
+{
+    /**
+     * variable for the Session service
+     *
+     * @param 	Rubedo\Interfaces\User\ISession
+     */
+    protected $_session;
 
-	/**
-	 * variable for the Data service
-	 *
-	 * @param 	Rubedo\Interfaces\User\ISession
-	 */
-	protected $_dataService;
+    /**
+     * variable for the Data service
+     *
+     * @param 	Rubedo\Interfaces\User\ISession
+     */
+    protected $_dataService;
 
-	/**
-	 * Init the session service
-	 */
-	public function init() {
-		$this -> _dataService = Rubedo\services\Manager::getService('Contents');
-	}
+    /**
+     * Init the session service
+     */
+    public function init() {
+        $this->_dataService = Rubedo\services\Manager::getService('Contents');
+    }
 
-	/**
-	 * Allow to define the current theme
-	 */
-	public function indexAction() {
+    /**
+     * Allow to define the current theme
+     */
+    public function indexAction() {
 
-		$contentId = $this -> getRequest() -> getParam('id');
-		$data = $this -> getRequest() -> getParam('data');
-		if (!empty($contentId['id'])) {
-			$contentId = explode("_", $contentId);
-			$id = $contentId[0];
-			$field = $contentId[1];
+        $contentId = $this->getRequest()->getParam('id');
+        $data = $this->getRequest()->getParam('data');
+        if (!empty($contentId['id'])) {
+            $contentId = explode("_", $contentId);
+            $id = $contentId[0];
+            $field = $contentId[1];
 
-			$baseData = $this -> _dataService -> findById($id, true, false);
-			$baseData['fields'][$field] = $data;
-			$returnArray = $this -> _dataService -> update($baseData, true, true);
+            $baseData = $this->_dataService->findById($id, true, false);
+            $baseData['fields'][$field] = $data;
+            $returnArray = $this->_dataService->update($baseData, true, true);
 
-		} else {
-			$returnArray['success'] = false;
-			$returnArray['msg'] = 'No content id given.';
-		}
-		if (!$returnArray['success']) {
-			$this -> getResponse() -> setHttpResponseCode(500);
-		}
-		return $this -> _helper -> json($returnArray);
+        } else {
+            $returnArray['success'] = false;
+            $returnArray['msg'] = 'No content id given.';
+        }
+        if (!$returnArray['success']) {
+            $this->getResponse()->setHttpResponseCode(500);
+        }
+        return $this->_helper->json($returnArray);
 
-	}
+    }
 
 }
