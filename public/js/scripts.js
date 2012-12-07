45c97b90e0d5f7e370607d49365af0cbdc5f152c
Julien Bourdin <jbourdin@gmail.com>
2012-12-07 16:29:28 +0100

    fix jquery conflict

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
     
-    $.getJSON('/xhr-authentication/is-logged-in', function(data) {
+    jQuery.getJSON('/xhr-authentication/is-logged-in', function(data) {
         if (data.loggedIn) {
-            $('#connect-btn').html('<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-user"></i> ' + data.username + ' <span class="caret"></span></a><ul class="dropdown-menu"><li><a href="#">Mon compte</a></li><li class="divider"></li><li><a href="/backoffice/" >Accès Back Office</a></li><li class="divider"></li><li><a href="#" id="logout">Se déconnecter</a></li></ul>');
-            $.getScript('/xhr-javascript/get-script?script=rubedo-edit', function() {
+            //jQuery('#connect-btn').html('<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-user"></i> ' + data.username + ' <span class="caret"></span></a><ul class="dropdown-menu"><li><a href="#">Mon compte</a></li><li class="divider"></li><li><a href="/backoffice/" >Accès Back Office</a></li><li class="divider"></li><li><a href="#" id="logout">Se déconnecter</a></li></ul>');
+            jQuery.getScript('/xhr-javascript/get-script?script=rubedo-edit', function() {
 
             });
-            $('#logout').click(function() {
+            /*jQuery('#logout').click(function() {
                 $.getJSON('/xhr-authentication/logout/', function(data) {
                     if (data.success) {
                         location.reload();
                     }
                 });
                 return false;
-            });
+            });*/
         } else {
-            $('#connect-btn').html('<a class="btn" data-toggle="modal" href="#connect"><i class="icon-user"></i> Connexion</a>');
+            //jQuery('#connect-btn').html('<a class="btn" data-toggle="modal" href="#connect"><i class="icon-user"></i> Connexion</a>');
         }
     });
 }); 
