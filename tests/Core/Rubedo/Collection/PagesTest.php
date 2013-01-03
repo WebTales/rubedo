4650111261e6dc3468cb3198e8d04429902268fd
Nicolas Duvollet <duvolletn@HP-Folio-13-Nicolas-Duvollet.(none)>
2013-01-03 16:39:29 +0100

    Add unitary test for Pages service

---
+++ tests/Core/Rubedo/Collection/PagesTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Rubedo
+ *
+ * LICENSE
+ *
+ * yet to be written
+ *
+ * @category Rubedo-Test
+ * @package Rubedo-Test
+ * @copyright Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license yet to be written
+ * @version $Id$
+ */
+
+Use Rubedo\Collection\Pages;
+
+/**
+ * Test suite of the collection service :
+ * @author jbourdin
+ * @category Rubedo-Test
+ * @package Rubedo-Test
+ */
+class PagesTest extends PHPUnit_Framework_TestCase {
+		/**
+     * clear the DB of the previous test data
+     */
+    public function tearDown() {
+        Rubedo\Services\Manager::resetMocks();
+    }
+
+    /**
+     * init the Zend Application for tests
+     */
+    public function setUp() {
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+		$this->bootstrap->bootstrap();
+        $this->_mockDataAccessService = $this->getMock('Rubedo\\Mongo\\DataAccess');
+        Rubedo\Services\Manager::setMockService('MongoDataAccess', $this->_mockDataAccessService);
+		$this->_mockUrlCacheService = $this->getMock('Rubedo\\Collection\\UrlCache');
+        Rubedo\Services\Manager::setMockService('UrlCache', $this->_mockUrlCacheService);
+        parent::setUp();
+    }
+
+	public function testNormalMatchSegment(){
+		$this->_mockDataAccessService->expects($this->once())->method('findOne');
+		$urlSegment="segment";
+		$parentId="parent";
+		$siteId="site";
+		$PageService=new Rubedo\Collection\Pages();
+		$PageService->matchSegment($urlSegment, $parentId, $siteId);
+	}
+	
+	public function testNormalDestroy()
+	{
+		$this->_mockDataAccessService->expects($this->once())->method('destroy');
+		$this->_mockUrlCacheService->expects($this->once())->method('customDelete');
+		$obj['id']='test';
+		$PageService=new Rubedo\Collection\Pages();
+		$PageService->destroy($obj);
+	}
+	public function testNormalUpdate(){
+		$this->_mockDataAccessService->expects($this->once())->method('update');
+		$this->_mockUrlCacheService->expects($this->once())->method('customDelete');
+		$obj['id']='test';
+		$PageService=new Rubedo\Collection\Pages();
+		$PageService->update($obj);
+	}
+	
+	public function testNormalFindByNameAndSite()
+	{
+		$this->_mockDataAccessService->expects($this->once())->method('findOne');
+		$name="name";
+		$siteId="site";
+		$PageService=new Rubedo\Collection\Pages();
+		$PageService->findByNameAndSite($name, $siteId);
+	}
+}
