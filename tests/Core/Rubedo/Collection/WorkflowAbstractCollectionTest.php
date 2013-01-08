bb80e13ce5b489608c98c83a665e7d732b3e4937
Nicolas Duvollet <duvolletn@HP-Folio-13-Nicolas-Duvollet.(none)>
2013-01-08 12:41:52 +0100

    erase unitary tests

--- tests/Core/Rubedo/Collection/WorkflowAbstractCollectionTest.php
+++ tests/Core/Rubedo/Collection/WorkflowAbstractCollectionTest.php
@@ -51,19 +51,7 @@ class WorkflowAbstractCollectionTest extends PHPUnit_Framework_TestCase {
 
         parent::setUp();
     }
-	/*
-	 * Test if FindById call method setLive once when live is true
-	 */
-	public function testNormalFindByIdWithLiveToTrue(){
-		$this->_mockWorkflowDataAccessService->expects($this->once())->method('setLive');
-		$this->_mockWorkflowDataAccessService->expects($this->never())->method('setWorkspace');
-		$this->_mockWorkflowDataAccessService->expects($this->once())->method('findById');
-		
-		$contentId="id";
-		$collection = new testWorkflowCollection();
-		$collection->findById($contentId);
-		
-	}
+
 	/*
 	 * Test if FindById call method setWorkspace once when live is false
 	 */
@@ -77,36 +65,7 @@ class WorkflowAbstractCollectionTest extends PHPUnit_Framework_TestCase {
 		$collection->findById($contentId,false);
 		
 	}
-		/*
-		 * test if create function works fine when live param is false and create function return status published
-		 */
-	public function testCreateWithLiveFalseAndStatusPublished(){
-		$createReturn["data"]["status"]="published";
-		$createReturn["data"]["id"]="unId";
-		$this->_mockWorkflowDataAccessService->expects($this->never())->method('setLive');
-		$this->_mockWorkflowDataAccessService->expects($this->once())->method('setWorkspace');
-		$this->_mockWorkflowDataAccessService->expects($this->once())->method('create')->will($this->returnValue($createReturn));
-		$this->_mockWorkflowDataAccessService->expects($this->once())->method('publish');
-		
-		$obj["test"]="test";
-		$collection = new testWorkflowCollection();
-		$result =  $collection->create($obj, array('safe'=>true),false);
-	}
-		/*
-		 * test if create function works fine when live param istrue and create function not return status published
-		 */
-	public function testCreateWithLiveStatusNotPublished(){
-		$createReturn["data"]["status"]="notpublished";
-		$createReturn["data"]["id"]="unId";
-		$this->_mockWorkflowDataAccessService->expects($this->once())->method('setLive');
-		$this->_mockWorkflowDataAccessService->expects($this->never())->method('setWorkspace');
-		$this->_mockWorkflowDataAccessService->expects($this->once())->method('create')->will($this->returnValue($createReturn));
-		$this->_mockWorkflowDataAccessService->expects($this->never())->method('publish');
-		
-		$obj["test"]="test";
-		$collection = new testWorkflowCollection();
-		$result =  $collection->create($obj, array('safe'=>true),true);
-	}
+
 	/*
 	 * test if readChild function works fine
 	 */
