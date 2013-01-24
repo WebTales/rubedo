44ddf0b93bd24faf0542847997c7bbfc672b3c98
Didier Fanchon <didier.fanchon@webtales.fr>
2013-01-24 14:03:58 +0100

    bug fix +1

--- application/modules/backoffice/controllers/ElasticSearchController.php
+++ application/modules/backoffice/controllers/ElasticSearchController.php
@@ -39,13 +39,14 @@ class Backoffice_ElasticSearchController extends Zend_Controller_Action {
         $query = \Rubedo\Services\Manager::getService('ElasticDataSearch');
         
         $query->init();
-        if (isset($params['limit'])) $params['pagesize'] = $params['limit'];
-		if (isset($params['page'])) $params['pager'] = $params['page'];
+        if (isset($params['limit'])) $params['pagesize'] = (int) $params['limit'];
+		if (isset($params['page'])) $params['pager'] = (int) $params['page'];
 		if (isset($params['sort'])) {
 			$params['orderBy'] = $params['sort']['property'];
 			$params['orderByDirection'] = $params['sort']['direction'];
 		}
         $search = $query->search($params);
+
         $elasticaResultSet = $search["resultSet"];
 		$filters = $search["filters"];
 		
