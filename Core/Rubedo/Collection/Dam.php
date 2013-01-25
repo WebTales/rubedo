ef9e7c5d13368886c80b11d2462d0a13ba40f26f
Julien Bourdin <jbourdin@gmail.com>
2013-01-25 14:50:16 +0100

    add fileSize in data

--- Core/Rubedo/Collection/Dam.php
+++ Core/Rubedo/Collection/Dam.php
@@ -34,12 +34,44 @@ class Dam extends AbstractCollection implements IDam
         $this->_collectionName = 'Dam';
         parent::__construct();
     }
-    
-    public function destroy(array $obj, $options = array('safe'=>true)){
+
+    public function destroy (array $obj, $options = array('safe'=>true))
+    {
         $obj = $this->_dataService->findById($obj['id']);
-        $destroyOriginal = Manager::getService('Files')->destroy(array('id'=>$obj['originalFileId']));
+        $destroyOriginal = Manager::getService('Files')->destroy(array(
+            'id' => $obj['originalFileId']
+        ));
         
-        return parent::destroy($obj,$options);
-        
+        return parent::destroy($obj, $options);
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \Rubedo\Collection\AbstractCollection::update()
+     */
+    public function update (array $obj, $options = array('safe'=>true,))
+    {
+        $originalFilePointer = Manager::getService('Files')->findById($obj['originalFileId']);
+        if (! $originalFilePointer instanceof \MongoGridFSFile) {
+            throw new \Exception('no file found');
+        }
+        $obj['fileSize'] = $originalFilePointer->getSize();
+        return parent::update($obj, $options);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * 
+     * @see \Rubedo\Collection\AbstractCollection::create()
+     */
+    public function create (array $obj, $options = array('safe'=>true,))
+    {
+        $originalFilePointer = Manager::getService('Files')->findById($obj['originalFileId']);
+        if (! $originalFilePointer instanceof \MongoGridFSFile) {
+            throw new \Exception('no file found');
+        }
+        $obj['fileSize'] = $originalFilePointer->getSize();
+        return parent::create ($obj, $options);
     }
 }
