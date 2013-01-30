9f5ad6ba96f80877344b7fe8efbc309f3c2fa053
Julien Bourdin <jbourdin@gmail.com>
2013-01-30 15:58:34 +0100

    no longer use read and write taxonomy

--- Core/Rubedo/Collection/Contents.php
+++ Core/Rubedo/Collection/Contents.php
@@ -94,9 +94,9 @@ class Contents extends WorkflowAbstractCollection implements IContents
      */
     public function create (array $obj, $options = array('safe'=>true), $live = false)
     {
-        if(!isset($obj['taxonomy']['navigation']) || empty($obj['taxonomy']['navigation'])){
-            $obj['taxonomy']['navigation'] = Manager::getService('CurrentUser')->getWriteNavigationTaxonomy ();
-        }
+//         if(!isset($obj['taxonomy']['navigation']) || empty($obj['taxonomy']['navigation'])){
+//             $obj['taxonomy']['navigation'] = Manager::getService('CurrentUser')->getWriteNavigationTaxonomy ();
+//         }
         $obj = $this->_filterInputData($obj);
 
         if ($this->_isValidInput) {
@@ -121,9 +121,9 @@ class Contents extends WorkflowAbstractCollection implements IContents
      */
     public function update (array $obj, $options = array('safe'=>true), $live = true)
     {
-        if(!isset($obj['taxonomy']['navigation']) || empty($obj['taxonomy']['navigation'])){
-            $obj['taxonomy']['navigation'] = Manager::getService('CurrentUser')->getWriteNavigationTaxonomy ();
-        }
+//         if(!isset($obj['taxonomy']['navigation']) || empty($obj['taxonomy']['navigation'])){
+//             $obj['taxonomy']['navigation'] = Manager::getService('CurrentUser')->getWriteNavigationTaxonomy ();
+//         }
         $obj = $this->_filterInputData($obj);
         if ($this->_isValidInput) {
             $returnArray = parent::update($obj, $options, $live);
