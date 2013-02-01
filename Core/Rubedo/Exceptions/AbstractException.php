f3051aec4a794147952c16e931a0e3b4648d1a31
Julien Bourdin <jbourdin@gmail.com>
2013-02-01 14:13:13 +0100

    add Exception Strategy

---
+++ Core/Rubedo/Exceptions/AbstractException.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Rubedo -- ECM solution
+ * Copyright (c) 2012, WebTales (http://www.webtales.fr/).
+ * All rights reserved.
+ * licensing@webtales.fr
+ *
+ * Open Source License
+ * ------------------------------------------------------------------------------------------
+ * Rubedo is licensed under the terms of the Open Source GPL 3.0 license. 
+ *
+ * @category   Rubedo
+ * @package    Rubedo
+ * @copyright  Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license    http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
+ */
+namespace Rubedo\Exceptions;
+
+
+/**
+ * Exception occuring when an access right isn't granted
+ *
+ * @author jbourdin
+ * @category Rubedo
+ * @package Rubedo
+ *
+ */
+abstract class AbstractException extends \Exception
+{
+
+    public function __construct ($message = null, $code = null, $previous = null) {
+        $language = 'fr';
+        $message = $this->_translate($message, $language);
+        parent::__construct($message, $code, $previous);
+        
+    }
+    
+    protected function _translate($message,$language='en'){
+        return $message;
+    }
+}
