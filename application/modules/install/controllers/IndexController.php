c01616b653ff77b4f0104595a570f1e9f6bef11a
Julien Bourdin <jbourdin@gmail.com>
2013-02-05 16:56:33 +0100

    extract form from controller to a specific model

--- application/modules/install/controllers/IndexController.php
+++ application/modules/install/controllers/IndexController.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Rubedo
+ * Rubedo -- ECM solution
+ * Copyright (c) 2012, WebTales (http://www.webtales.fr/).
+ * All rights reserved.
+ * licensing@webtales.fr
  *
- * LICENSE
- *
- * yet to be written
+ * Open Source License
+ * ------------------------------------------------------------------------------------------
+ * Rubedo is licensed under the terms of the Open Source GPL 3.0 license. 
  *
  * @category   Rubedo
  * @package    Rubedo
  * @copyright  Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
- * @license    yet to be written
- * @version    $Id:
+ * @license    http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
  */
 use Rubedo\Mongo\DataAccess, Rubedo\Mongo;
 
@@ -79,41 +81,7 @@ class Install_IndexController extends Zend_Controller_Action
         
         $mongoOptions = $this->_applicationOptions["datastream"]["mongo"];
         
-        $serverNameField = new Zend_Form_Element_Text('server');
-        $serverNameField->setRequired(true);
-        $serverNameField->setValue(isset($mongoOptions['server']) ? $mongoOptions['server'] : 'localhost/rubedo');
-        $serverNameField->setLabel('Server Name');
-        
-        $serverPortField = new Zend_Form_Element_Text('serverport');
-        // $serverPortField->setRequired(true);
-        $serverPortField->setValue(isset($mongoOptions['port']) ? $mongoOptions['port'] : null);
-        $serverPortField->addValidator('digits');
-        $serverPortField->setLabel('Server Port');
-        
-        $dbNameField = new Zend_Form_Element_Text('db');
-        $dbNameField->setRequired(true);
-        $dbNameField->setValue(isset($mongoOptions['db']) ? $mongoOptions['db'] : null);
-        $dbNameField->setLabel('Db Name');
-        
-        $serverLoginField = new Zend_Form_Element_Text('login');
-        $serverLoginField->setValue(isset($mongoOptions['login']) ? $mongoOptions['login'] : null);
-        $serverLoginField->setLabel('Username');
-        
-        $serverPasswordField = new Zend_Form_Element_Text('password');
-        $serverPasswordField->setValue(isset($mongoOptions['password']) ? $mongoOptions['password'] : null);
-        $serverPasswordField->setLabel('Password');
-        
-        $submitButton = new Zend_Form_Element_Submit('Submit');
-        $submitButton->setAttrib('class', 'btn btn-large btn-primary');
-        
-        $dbForm = new Zend_Form();
-        $dbForm->setMethod('post');
-        $dbForm->addElement($serverNameField);
-        // $dbForm->addElement($serverPortField);
-        $dbForm->addElement($dbNameField);
-        $dbForm->addElement($serverLoginField);
-        $dbForm->addElement($serverPasswordField);
-        $dbForm->addElement($submitButton);
+        $dbForm = Install_Model_DbConfigForm::getDtForm($mongoOptions);
         
         $mongoAccess = new DataAccess();
         
@@ -126,16 +94,14 @@ class Install_IndexController extends Zend_Controller_Action
             } else {
                 $initCollection = $mongoAccess->init('Users');
                 $params = $this->_applicationOptions["datastream"]["mongo"];
-                
             }
             $connectionValid = true;
         } catch (Exception $exception) {
             $connectionValid = false;
-            
         }
         if ($connectionValid) {
             $this->view->isReady = true;
-            $this->_localConfig["datastream"]["mongo"]=$params;
+            $this->_localConfig["datastream"]["mongo"] = $params;
         } else {
             $this->view->hasError = true;
             $this->view->errorMsgs = 'Rubedo can\'t connect itself to specified DB';
