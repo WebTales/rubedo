c37b2280e64512c382c803d2a02a4c36df428a19
Julien Bourdin <julien.bourdin@webtales.fr>
2013-02-05 09:40:16 +0100

    more params for installer

--- application/modules/install/controllers/IndexController.php
+++ application/modules/install/controllers/IndexController.php
@@ -16,7 +16,7 @@ use Rubedo\Mongo\DataAccess, Rubedo\Mongo;
 
 /**
  * Installer Controller
- * 
+ *
  *
  * @author jbourdin
  * @category Rubedo
@@ -25,18 +25,55 @@ use Rubedo\Mongo\DataAccess, Rubedo\Mongo;
 class Install_IndexController extends Zend_Controller_Action
 {
 
-    public function init(){
-        
-    }
-    
-    /**
-     * Default Action, return the Ext/Js HTML loader
-     */
-    public function indexAction ()
+    protected $_localConfigDir;
+
+    protected $_localConfigFile;
+
+    protected $_localConfig;
+
+    public function init ()
     {
-		
+        $this->_localConfigDir = realpath(APPLICATION_PATH . '/configs/local/');
+        $this->_localConfigFile = $this->_localConfigDir . '/config.json';
+        $this->_loadLocalConfig();
     }
 
-   
+    public function indexAction ()
+    {
+        
+        $this->_saveLocalConfig();
+    }
+
+    protected function _isConfigWritable ()
+    {
+        $isWritable = false;
+        if (is_file($this->_localConfigFile)) {
+            return is_writable($this->_localConfigFile);
+        } else {
+            return is_writable($this->_localConfigDir);
+        }
+    }
+
+    protected function _saveLocalConfig ()
+    {
+        $iniWriter = new Zend_Config_Writer_Json();
+        $iniWriter->setConfig($this->_localConfig);
+        $iniWriter->setFilename($this->_localConfigFile);
+        $iniWriter->setPrettyPrint(true);
+        $iniWriter->write();
+    }
+
+    protected function _loadLocalConfig ()
+    {
+        if (is_file($this->_localConfigFile)) {
+            $this->_localConfig = new Zend_Config_Json($this->_localConfigFile, 
+                    null, 
+                    array(
+                            'allowModifications' => true
+                    ));
+        } else{
+            $this->_localConfig = new Zend_Config(array(),true);
+        }
+    }
 }
 
