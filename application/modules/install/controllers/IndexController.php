698dba4720c68f2affb8b1ba6fe4439d988cc50a
Julien Bourdin <jbourdin@gmail.com>
2013-02-05 13:39:51 +0100

    implement DB config

--- application/modules/install/controllers/IndexController.php
+++ application/modules/install/controllers/IndexController.php
@@ -77,19 +77,30 @@ class Install_IndexController extends Zend_Controller_Action
             $this->_localConfig['installed']['action'] = 'set-db';
         }
         
-        $serverNameField = new Zend_Form_Element_Text('servername');
+        $mongoOptions = $this->_applicationOptions["datastream"]["mongo"];
+        
+        $serverNameField = new Zend_Form_Element_Text('server');
         $serverNameField->setRequired(true);
+        $serverNameField->setValue(isset($mongoOptions['server']) ? $mongoOptions['server'] : 'localhost/rubedo');
         $serverNameField->setLabel('Server Name');
         
         $serverPortField = new Zend_Form_Element_Text('serverport');
-        $serverPortField->setRequired(true);
+        // $serverPortField->setRequired(true);
+        $serverPortField->setValue(isset($mongoOptions['port']) ? $mongoOptions['port'] : null);
         $serverPortField->addValidator('digits');
         $serverPortField->setLabel('Server Port');
         
-        $serverLoginField = new Zend_Form_Element_Text('serverlogin');
+        $dbNameField = new Zend_Form_Element_Text('db');
+        $dbNameField->setRequired(true);
+        $dbNameField->setValue(isset($mongoOptions['db']) ? $mongoOptions['db'] : null);
+        $dbNameField->setLabel('Db Name');
+        
+        $serverLoginField = new Zend_Form_Element_Text('login');
+        $serverLoginField->setValue(isset($mongoOptions['login']) ? $mongoOptions['login'] : null);
         $serverLoginField->setLabel('Username');
         
-        $serverPasswordField = new Zend_Form_Element_Password('serverpass');
+        $serverPasswordField = new Zend_Form_Element_Text('password');
+        $serverPasswordField->setValue(isset($mongoOptions['password']) ? $mongoOptions['password'] : null);
         $serverPasswordField->setLabel('Password');
         
         $submitButton = new Zend_Form_Element_Submit('Submit');
@@ -98,18 +109,52 @@ class Install_IndexController extends Zend_Controller_Action
         $dbForm = new Zend_Form();
         $dbForm->setMethod('post');
         $dbForm->addElement($serverNameField);
-        $dbForm->addElement($serverPortField);
+        // $dbForm->addElement($serverPortField);
+        $dbForm->addElement($dbNameField);
         $dbForm->addElement($serverLoginField);
         $dbForm->addElement($serverPasswordField);
         $dbForm->addElement($submitButton);
         
-        if ($this->getRequest()->isPost() && $dbForm->isValid($this->getAllParams())) {
+        $mongoAccess = new DataAccess();
+        
+        try {
+            if ($this->getRequest()->isPost() && $dbForm->isValid($this->getAllParams())) {
+                $params = $dbForm->getValues();
+                $mongo = $this->_buildConnectionString($params);
+                $dbName = $params['db'];
+                $initCollection = $mongoAccess->init('Users', $dbName, $mongo);
+            } else {
+                $initCollection = $mongoAccess->init('Users');
+                $params = $this->_applicationOptions["datastream"]["mongo"];
+                
+            }
+            $connectionValid = true;
+        } catch (Exception $exception) {
+            $connectionValid = false;
+            
+        }
+        if ($connectionValid) {
             $this->view->isReady = true;
+            $this->_localConfig["datastream"]["mongo"]=$params;
+        } else {
+            $this->view->hasError = true;
+            $this->view->errorMsgs = 'Rubedo can\'t connect itself to specified DB';
         }
         
         $this->view->form = $dbForm;
         
         $this->_saveLocalConfig();
+    }
+
+    protected function _buildConnectionString ($options)
+    {
+        $connectionString = 'mongodb://';
+        if (! empty($options['login'])) {
+            $connectionString .= $options['login'];
+            $connectionString .= ':' . $options['password'] . '@';
+        }
+        $connectionString .= $options['server'];
+        return $connectionString;
     }
 
     protected function _isConfigWritable ()
