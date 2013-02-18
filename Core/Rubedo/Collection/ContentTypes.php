8d9f38460b85b7b8b94e57c6c6a0d7868fa3be43
mickael goncalves <mickael.goncalves@webtales.fr>
2013-02-18 11:47:21 +0100

    Applying deactivation of rights for specific cases on ContentTypes service

--- Core/Rubedo/Collection/ContentTypes.php
+++ Core/Rubedo/Collection/ContentTypes.php
@@ -228,24 +228,26 @@ class ContentTypes extends AbstractCollection implements IContentTypes
      * @see \Rubedo\Collection\AbstractCollection::getList()
      */
     public function getList ($filters = null, $sort = null, $start = null, $limit = null)
-    {
+    {			
         $list = parent::getList($filters,$sort,$start,$limit);
+
         foreach ($list['data'] as &$obj){
             $obj = $this->_addReadableProperty($obj);
         }
+
         return $list;
     }
 
     protected function _addReadableProperty ($obj)
     {
         if (! self::isUserFilterDisabled()) {	
-	        if (! isset($obj['workspaces'])) {
+	        if (! isset($obj['workspaces']) || $obj['workspaces']=="") {
 	            $obj['workspaces'] = array(
 	                'global'
 	            );
 	        }
 	        $writeWorkspaces = Manager::getService('CurrentUser')->getWriteWorkspaces();
-	        
+
 	        if (count(array_intersect($obj['workspaces'], $writeWorkspaces)) == 0) {
 	            $obj['readOnly'] = true;
 	        } else {
