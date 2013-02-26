3c1ab1841747fffe96eac1d8ec57b24053ace884
Julien Bourdin <jbourdin@gmail.com>
2013-02-26 17:53:25 +0100

    begin Agenda

---
+++ application/modules/blocks/controllers/AgendaController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Rubedo
+ *
+ * LICENSE
+ *
+ * yet to be written
+ *
+ * @category   Rubedo
+ * @package    Rubedo
+ * @copyright  Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @license    yet to be written
+ * @version    $Id:
+ */
+
+require_once ('ContentListController.php');
+
+/**
+ *
+ * @author jbourdin
+ * @category Rubedo
+ * @package Rubedo
+ */
+class Blocks_AgendaController extends Blocks_ContentListController
+{
+    protected $_defaultTemplate = 'agenda';
+    
+}
