61bb8145d61640f8fa7b66ecf8ae6eeb3035fb9d
mickael goncalves <mickael.goncalves@webtales.fr>
2013-03-01 12:15:41 +0100

    Add jQuery to the flickr gallery

--- public/templates/root/js/gallery.js
+++ public/templates/root/js/gallery.js
@@ -1,5 +1,7 @@
-function changePage(pageNumber, itemCount, itemsPerPage, maxPage,prefix,width,height,query,url) {
-	if(jQuery('#'+prefix+' > #page'+pageNumber).length == 0){
+function changePage(pageNumber, itemCount, itemsPerPage, maxPage,prefix,width,height,query,url,user,tags,tagMode) {
+	if(jQuery('#'+prefix+' > #'+prefix+'-page'+pageNumber).length == 0){
+		jQuery('#'+prefix+' > .progress-gallery').removeClass('hide');
+		jQuery('#'+prefix+' > .active-items').hide();
 	var request = jQuery.ajax({
 		url : url,
 		type : "POST",
@@ -11,23 +13,37 @@ function changePage(pageNumber, itemCount, itemsPerPage, maxPage,prefix,width,he
 			'width': (width) ? width : null,
 			'height': (height) ? height : null,
 			'prefix':prefix,
-			'query':query
+			'query': (query) ? query : null,
+			'user': (user) ? user : null,
+			'tags': (tags) ? tags : null,
+			'tagMode': (tagMode) ? tagMode : null
 		},
 		dataType : "json"
 	});
 
 	request.done(function(data) {
+		jQuery('#'+prefix+' > .progress-gallery').addClass('hide');
 		var newHtml = data.html;
 		jQuery('#'+prefix).append(newHtml);
+		jQuery('#'+prefix+' > .active-items').removeClass('active-items');
+		jQuery('#'+prefix+' > #'+prefix+'-page'+pageNumber).show();
+		jQuery('#'+prefix+' > #'+prefix+'-page'+pageNumber).addClass('active-items');
 	});
 
 	request.fail(function(jqXHR, textStatus) {
+		jQuery('#'+prefix+' > .progress-gallery').addClass('hide');
+		var errorHtml ='<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button><h4>Erreur !</h4>Impossible de charger les images</div>';
+		jQuery('#'+prefix).prepend(errorHtml);
+		jQuery('#'+prefix+' > .active-items').show();
+		console.log(jqXHR);
 	});
+	}else{
+		jQuery('#'+prefix+' > .active-items').hide();
+		jQuery('#'+prefix+' > .active-items').removeClass('active-items');
+		jQuery('#'+prefix+' > #'+prefix+'-page'+pageNumber).show();
+		jQuery('#'+prefix+' > #'+prefix+'-page'+pageNumber).addClass('active-items');
 	}
-	jQuery('#'+prefix+' > .active').hide();
-	jQuery('#'+prefix+' > .active').removeClass('active');
-	jQuery('#'+prefix+' > #page'+pageNumber).show();
-	jQuery('#'+prefix+' > #page'+pageNumber).addClass('active');
+	
 	return false;
 }
 	
