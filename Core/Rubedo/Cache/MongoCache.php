754f6446c3260e1af8fe550be7e5943c36933121
Julien Bourdin <julien.bourdin@webtales.fr>
2013-03-12 15:37:51 +0100

    update copyright to 2013

--- Core/Rubedo/Cache/MongoCache.php
+++ Core/Rubedo/Cache/MongoCache.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Rubedo -- ECM solution Copyright (c) 2012, WebTales
+ * Rubedo -- ECM solution Copyright (c) 2013, WebTales
  * (http://www.webtales.fr/). All rights reserved. licensing@webtales.fr
  * Open Source License
  * ------------------------------------------------------------------------------------------
@@ -9,7 +9,7 @@
  *
  * @category Rubedo
  * @package Rubedo
- * @copyright Copyright (c) 2012-2012 WebTales (http://www.webtales.fr)
+ * @copyright Copyright (c) 2012-2013 WebTales (http://www.webtales.fr)
  * @license http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
  */
 namespace Rubedo\Cache;
