acc4c16c616163788527a5421de42dcf16cdd6eb
nicolas.duvollet@webtales.fr <duvolletn@HP-Folio-13-Nicolas-Duvollet>
2013-03-20 16:11:00 +0100

    add controller for forms block

---
+++ application/modules/blocks/controllers/FormsController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Rubedo -- ECM solution
+ * Copyright (c) 2013, WebTales (http://www.webtales.fr/).
+ * All rights reserved.
+ * licensing@webtales.fr
+ *
+ * Open Source License
+ * ------------------------------------------------------------------------------------------
+ * Rubedo is licensed under the terms of the Open Source GPL 3.0 license. 
+ *
+ * @category   Rubedo
+ * @package    Rubedo
+ * @copyright  Copyright (c) 2012-2013 WebTales (http://www.webtales.fr)
+ * @license    http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
+ */
+Use Rubedo\Services\Manager;
+
+require_once ('AbstractController.php');
+
+/**
+ *
+ * @author dfanchon
+ * @category Rubedo
+ * @package Rubedo
+ */
+class Blocks_FormsController extends Blocks_AbstractController
+{
+
+    /**
+     * Default Action, return the Ext/Js HTML loader
+     */
+    public function indexAction ()
+    {
+       
+    }
+}
