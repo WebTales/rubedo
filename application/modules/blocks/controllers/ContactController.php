7688eb11341515fd687a6c45045f30dc7c541b90
goncalvesm <mickael.goncalves@webtales.fr>
2013-03-27 14:46:16 +0100

    Add empty contact controller

---
+++ application/modules/blocks/controllers/ContactController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Rubedo -- ECM solution
+ * Copyright (c) 2013, WebTales (http://www.webtales.fr/).
+ * All rights reserved.
+ * licensing@webtales.fr
+ *
+ * Open Source License
+ * ------------------------------------------------------------------------------------------
+ * Rubedo is licensed under the terms of the Open Source GPL 3.0 license. 
+ *
+ * @category   Rubedo
+ * @package    Rubedo
+ * @copyright  Copyright (c) 2012-2013 WebTales (http://www.webtales.fr)
+ * @license    http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
+ */
+Use Rubedo\Services\Manager;
+
+require_once ('AbstractController.php');
+
+/**
+ *
+ * @author mgoncalves
+ * @category Rubedo
+ * @package Rubedo
+ */
+class Blocks_ContactController extends Blocks_AbstractController
+{
+
+    protected $_defaultTemplate = 'contact';
+    
+    public function indexAction ()
+    {
+        $blockConfig = $this->getRequest()->getParam('block-config');
+        
+        $output["blockConfig"]=$blockConfig;
+        
+        if (isset($blockConfig['displayType'])) {
+            $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath(
+                    "blocks/" . $blockConfig['displayType'] . ".html.twig");
+        } else {
+            $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath(
+                    "blocks/" . $this->_defaultTemplate . ".html.twig");
+        }
+        
+        $css = array();
+        $js = array();
+        $this->_sendResponse($output, $template, $css, $js);
+    }
+}
