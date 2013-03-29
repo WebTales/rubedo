abb41d4a22589c39df378ba953995b08f9614396
nicolas.duvollet@webtales.fr <duvolletn@HP-Folio-13-Nicolas-Duvollet>
2013-03-29 10:58:16 +0100

    update js

--- public/templates/root/js/forms.js
+++ public/templates/root/js/forms.js
@@ -6,27 +6,37 @@ function addcheck() {
 
 function checkField(){
 	jQuery(".conditional").each(function(){
+		var self=this;
 		var value=jQuery(this).attr('data-value').split(";");
 		var target=jQuery(this).attr('data-target');
-		if(value.length>1)
+		console.log(value.length);
+		//check children
+		if(value.length>=1)
 			{
 			var checked;
 			jQuery("."+target).each(function(){
-				if(jQuery(this).is(":checked"))
+				console.log(jQuery(this).prop('checked'));
+				if(jQuery(this).prop('checked'))
 					{
-					checked=jQuery.inArray(jQuery(this).val(),value)!="-1"?true:false;
+					//foreach parent fields if is checked and value is in array of values display children field
+					if(jQuery.inArray(jQuery(this).val(),value)!="-1"){
+					jQuery(self).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("hide").addClass("show");}
 					}
-			})
+				if(!jQuery(this).prop('checked'))
+					{
+					if(jQuery.inArray(jQuery(this).val(),value)!="-1")
+					jQuery(self).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("show").addClass("hide");
+					}
+				
+			});
 			}
 		else{
 			checked=jQuery.inArray(jQuery("#"+target).val(),value)!="-1"?true:false;
+			if(checked==true)
+				jQuery(this).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("hide").addClass("show");
+			else{
+				jQuery(this).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("show").addClass("hide");
+			}
 		}
-		if(checked==true)
-			jQuery(this).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("hide").addClass("show");
-		else{
-			jQuery(this).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("show").addClass("hide");
-		}
-		
-		
 	});
 }
