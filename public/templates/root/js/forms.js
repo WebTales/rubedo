7b14a0383f796dfa9f4d97d5a17f67b7aa5432d4
nicolas.duvollet@webtales.fr <duvolletn@HP-Folio-13-Nicolas-Duvollet>
2013-03-29 13:01:55 +0100

    fix display bug for conditional questions

--- public/templates/root/js/forms.js
+++ public/templates/root/js/forms.js
@@ -9,34 +9,29 @@ function checkField(){
 		var self=this;
 		var value=jQuery(this).attr('data-value').split(";");
 		var target=jQuery(this).attr('data-target');
-		console.log(value.length);
 		//check children
-		if(value.length>=1)
-			{
 			var checked;
 			jQuery("."+target).each(function(){
-				console.log(jQuery(this).prop('checked'));
-				if(jQuery(this).prop('checked'))
-					{
-					//foreach parent fields if is checked and value is in array of values display children field
+				if(jQuery(this).attr('type')=="checkbox"||jQuery(this).attr('type')=="radio")
+				{
+					if(jQuery(this).prop('checked'))
+						{
+						//foreach parent fields if is checked and value is in array of values display children field
+						if(jQuery.inArray(jQuery(this).val(),value)!="-1"){
+						jQuery(self).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("hide").addClass("show");}
+						}
+					if(!jQuery(this).prop('checked'))
+						{
+						if(jQuery.inArray(jQuery(this).val(),value)!="-1")
+							jQuery(self).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("show").addClass("hide");
+						}
+				}else{
 					if(jQuery.inArray(jQuery(this).val(),value)!="-1"){
-					jQuery(self).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("hide").addClass("show");}
+						jQuery(self).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("hide").addClass("show");
+					}else{
+						jQuery(self).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("show").addClass("hide");
 					}
-				if(!jQuery(this).prop('checked'))
-					{
-					if(jQuery.inArray(jQuery(this).val(),value)!="-1")
-					jQuery(self).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("show").addClass("hide");
-					}
-				
+				}
 			});
-			}
-		else{
-			checked=jQuery.inArray(jQuery("#"+target).val(),value)!="-1"?true:false;
-			if(checked==true)
-				jQuery(this).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("hide").addClass("show");
-			else{
-				jQuery(this).parentsUntil(jQuery(".rubedo-form"),".control-group").removeClass("show").addClass("hide");
-			}
-		}
 	});
 }
