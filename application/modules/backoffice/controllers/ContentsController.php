c26f1f9702c9df7b9061f18206cd1f4c18fbf2f8
Alexandru Dobre <alexdobre89@gmail.com>
2013-04-09 16:40:13 +0200

    add get-ordered-list to ro actions

--- application/modules/backoffice/controllers/ContentsController.php
+++ application/modules/backoffice/controllers/ContentsController.php
@@ -33,7 +33,7 @@ class Backoffice_ContentsController extends Backoffice_DataAccessController
 	/**
 	 * Array with the read only actions
 	 */
-	protected $_readOnlyAction = array('index', 'find-one', 'read-child', 'tree', 'clear-orphan-contents','count-orphan-contents',);
+	protected $_readOnlyAction = array('index', 'find-one', 'read-child', 'tree', 'clear-orphan-contents','count-orphan-contents','get-ordered-list');
 	
     public function init() {
         parent::init();
