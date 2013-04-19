d5e37112b21378a6e7abf507ad58867122a03788
Julien Bourdin <jbourdin@gmail.com>
2013-04-19 15:47:10 +0200

    Still working on notifications

--- Core/Rubedo/Mail/Notification.php
+++ Core/Rubedo/Mail/Notification.php
@@ -26,12 +26,13 @@ use Rubedo\Interfaces\Mail\INotification, Rubedo\Services\Manager;
  */
 class Notification implements INotification
 {
+
     protected static $_sendNotification = true;
-    
+
     protected static $_options = array();
-    
-    
+
     /**
+     *
      * @return the $sendNotification
      */
     public static function getSendNotification ()
@@ -39,38 +40,37 @@ class Notification implements INotification
         return Notification::$_sendNotification;
     }
 
-	/**
-     * @param boolean $sendNotification
+    /**
+     *
+     * @param boolean $sendNotification            
      */
     public static function setSendNotification ($sendNotification)
     {
         Notification::$_sendNotification = $sendNotification;
     }
 
-    
-    
-    public function getOptions ($name,$defaultValue=null)
+    public function getOptions ($name, $defaultValue = null)
     {
-        if(isset(self::$_options[$name])){
+        if (isset(self::$_options[$name])) {
             return self::$_options[$name];
-        }else{
+        } else {
             return $defaultValue;
         }
-        
     }
 
-    public static function setOptions ($name,$value)
+    public static function setOptions ($name, $value)
     {
         Notification::$_options[$name] = $value;
     }
 
-
     public function getNewMessage ()
     {
         $this->mailService = Manager::getService('Mailer');
-         
+        
         $message = $this->mailService->getNewMessage();
-        $message->setFrom(array($this->getOptions('fromEmailNotification')));
+        $message->setFrom(array(
+            $this->getOptions('fromEmailNotification')=>'Rubedo'
+        ));
         
         return $message;
     }
@@ -89,76 +89,80 @@ class Notification implements INotification
                 break;
         }
     }
-    
-    protected function setTo($message,$userIdArray){
-        $userService = Manager::getService("Users");
-        $toArray = array();
-        foreach($userIdArray as $userId){
-            $user = $userService->findById($userId);
-            $name = (isset($user['name']) && ! empty($user['name'])) ? $user['name'] : $user['login'];
-            $toArray[$user['email']]=$name;
-            
-            
-        }
-        $message->setTo($toArray);
-    }
 
-    protected function _directUrl($id){
+    protected function _directUrl ($id)
+    {
         return ($this->getOptions('isBackofficeSsl') ? 'https' : 'http') . '://' . $this->getOptions('defaultBackofficeHost') . '/backoffice/?content=' . $id;
     }
-    
+
     protected function _notifyPublished ($obj)
     {
         if (! isset($obj["lastPendingUser"])) {
             return;
         }
-        $twigVar = array();
-        $publishAuthor = Manager::getService('CurrentUser')->getCurrentUserSummary();
-        $twigVar['publishingAuthor'] = (isset($publishAuthor['name']) && ! empty($publishAuthor['name'])) ? $publishAuthor['name'] : $publishAuthor['login'];
-        $twigVar['title'] = $obj['text'];
-        $twigVar['directUrl'] = $this->_directUrl($obj['id']);
-        
-        $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("notification/published-body.html.twig");
-        $mailBody = Manager::getService('FrontOfficeTemplates')->render($template, $twigVar);
-        
-        $message = $this->getNewMessage();
-        $this->setTo($message, array(
+        $userIdArray = array(
             $obj["lastPendingUser"]["id"]
-        ));
-        $message->setSubject('[' . $this->getOptions('defaultBackofficeHost') . '] Publication du contenu "' . $obj['text'] . '"');
-        $message->setBody($mailBody, 'text/html');
-        
-        $result = $this->mailService->sendMessage($message);
-        return $result;
+        );
+        $template = 'published-body.html.twig';
+        $subject = '[' . $this->getOptions('defaultBackofficeHost') . '] Publication du contenu "' . $obj['text'] . '"';
+        return $this->_sendNotification($userIdArray, $obj, $template, $subject);
     }
-    
-    protected function _notifyRefused($obj){
+
+    protected function _notifyRefused ($obj)
+    {
         if (! isset($obj["lastPendingUser"])) {
             return;
         }
+        $userIdArray = array(
+            $obj["lastPendingUser"]["id"]
+        );
+        $template = 'refused-body.html.twig';
+        $subject = '[' . $this->getOptions('defaultBackofficeHost') . '] Refus du contenu "' . $obj['text'] . '"';
+        return $this->_sendNotification($userIdArray, $obj, $template, $subject);
+    }
+
+    protected function _notifyPending ($obj)
+    {
+        $userIdArray = array();
+        $template = 'pending-body.html.twig';
+        $subject = '[' . $this->getOptions('defaultBackofficeHost') . '] Soumission d\'un contenu "' . $obj['text'] . '"';
+        return $this->_sendNotification($userIdArray, $obj, $template, $subject);
+    }
+
+    protected function _sendNotification ($userIdArray, $obj, $template, $subject, $hideTo = true)
+    {
         $twigVar = array();
         $publishAuthor = Manager::getService('CurrentUser')->getCurrentUserSummary();
         $twigVar['publishingAuthor'] = (isset($publishAuthor['name']) && ! empty($publishAuthor['name'])) ? $publishAuthor['name'] : $publishAuthor['login'];
         $twigVar['title'] = $obj['text'];
         $twigVar['directUrl'] = $this->_directUrl($obj['id']);
         
-        $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("notification/refused-body.html.twig");
+        $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("notification/" . $template);
         $mailBody = Manager::getService('FrontOfficeTemplates')->render($template, $twigVar);
         
         $message = $this->getNewMessage();
-        $this->setTo($message, array(
-            $obj["lastPendingUser"]["id"]
-        ));
-        $message->setSubject('[' . $this->getOptions('defaultBackofficeHost') . '] Refus du contenu "' . $obj['text'] . '"');
+        $this->setTo($message, $userIdArray, $hideTo);
+        $message->setSubject($subject);
         $message->setBody($mailBody, 'text/html');
         
         $result = $this->mailService->sendMessage($message);
         return $result;
     }
-    
-    protected function _notifyPending($obj){
-    
+
+    protected function setTo ($message, $userIdArray, $hideTo = false)
+    {
+        $userService = Manager::getService("Users");
+        $toArray = array();
+        foreach ($userIdArray as $userId) {
+            $user = $userService->findById($userId);
+            $name = (isset($user['name']) && ! empty($user['name'])) ? $user['name'] : $user['login'];
+            $toArray[$user['email']] = $name;
+        }
+        if($hideTo){
+            $message->setBcc($toArray);
+        }else{
+            $message->setTo($toArray);
+        }
+        
     }
-    
-    
 }
