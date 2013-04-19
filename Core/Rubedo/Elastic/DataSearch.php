f2f6737e48cc73a4ce60438fe627fe6e529dbd84
Didier Fanchon <didier.fanchon@webtales.fr>
2013-04-19 16:53:55 +0200

    Label fix

--- Core/Rubedo/Elastic/DataSearch.php
+++ Core/Rubedo/Elastic/DataSearch.php
@@ -556,7 +556,7 @@ class DataSearch extends DataAbstract implements IDataSearch
                     
                     case 'date':
                         
-                        $temp['label'] = 'Date de modification :';
+                        $temp['label'] = 'Date de modification';
                         if (array_key_exists('ranges', $temp) and count($temp['ranges']) > 1) {
                             foreach ($temp['ranges'] as $key => $value) {                               
                                 $temp['ranges'][$key]['label'] = $timeLabel[$temp['ranges'][$key]['from']];
