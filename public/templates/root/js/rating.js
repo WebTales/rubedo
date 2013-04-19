35fe1fdb7e637f595de8db520e9507a37ce1a474
nicolas.duvollet@webtales.fr <duvolletn@HP-Folio-13-Nicolas-Duvollet>
2013-04-19 16:50:04 +0200

    update template

--- public/templates/root/js/rating.js
+++ public/templates/root/js/rating.js
@@ -5,17 +5,21 @@ jQuery(".star-edit").click(function(){
 });
 
 jQuery(".star-edit").hover(function(){
-
 	var self=this;
 	jQuery(".star-edit").each(function(){
-		//console.log(jQuery(this).attr("data-value")+"----------"+jQuery(self).attr("data-value"));
-		if(parseInt(jQuery(this).attr("data-value"))>parseInt(jQuery(self).attr("data-value"))&& starEdit==true)
+		//console.log(jQuery(this).attr("data-value")+"_____"+jQuery(self).attr("data-value"));
+		if(parseInt(jQuery(this).attr("data-value"))>=parseInt(jQuery(self).attr("data-value"))&& starEdit==true)
 			{
-				jQuery(this).removeClass("icon-star").addClass("icon-star-empty");
+			console.log('passe ici');
+				if(parseInt(jQuery(this).attr("data-value"))>=parseInt(jQuery(this).attr("data-min-value"))){
+					console.log("et là");
+				jQuery(this).removeClass("icon-star").addClass("icon-star-empty");}
 			}
-		else if(parseInt(jQuery(this).attr("data-value"))<=parseInt(jQuery(self).attr("data-value"))&& starEdit==true)
+		if(parseInt(jQuery(this).attr("data-value"))<=parseInt(jQuery(self).attr("data-value"))&&starEdit==true)
 			{
-			jQuery(this).removeClass("icon-star-empty").addClass("icon-star");
+				if(parseInt(jQuery(this).attr("data-value"))<=parseInt(jQuery(this).attr("data-max-value"))){
+					jQuery(this).removeClass("icon-star-empty").addClass("icon-star");
+					}
 			}
 	})
 });
