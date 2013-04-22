890b31854d90347d2427d86ce714b0493248ae57
nicolas.duvollet@webtales.fr <duvolletn@HP-Folio-13-Nicolas-Duvollet>
2013-04-22 11:39:08 +0200

    Update localiser field and gMap class, to use multivalued fields

--- public/templates/root/js/map.js
+++ public/templates/root/js/map.js
@@ -4,17 +4,7 @@ function loadMap()
 {
 	var id = jQuery(this).attr('id');
 	var config=new Object();
-	if(jQuery(this).attr("data-lat") && jQuery(this).attr("data-lon"))
-		{
-	config.options={
-			'latitude':jQuery(this).attr("data-lat"),
-			'longitude':jQuery(this).attr("data-lon")
-	}
-		}
-	else
-		{
 		config.options=jQuery.parseJSON(jQuery(this).attr("data-values"));
-		}
 	config.title=jQuery(this).attr('data-title');
 	config.text=jQuery(this).attr('data-value');
 	if(jQuery(this).attr('data-field')){config.field=jQuery(this).attr('data-field');}else{config.field=false;}
