6a4f6bdbc3fcb09fa1cd64bc37fa2f3357594a9e
Julien Bourdin <jbourdin@gmail.com>
2013-04-29 15:53:45 +0200

    fix name

--- application/modules/backoffice/controllers/ContentsController.php
+++ application/modules/backoffice/controllers/ContentsController.php
@@ -33,7 +33,7 @@ class Backoffice_ContentsController extends Backoffice_DataAccessController
 	/**
 	 * Array with the read only actions
 	 */
-	protected $_readOnlyAction = array('index', 'find-one', 'read-child', 'tree', 'clear-orphan-contents','count-orphan-contents','get-ordered-list','delete-by-content-id');
+	protected $_readOnlyAction = array('index', 'find-one', 'read-child', 'tree', 'clear-orphan-contents','count-orphan-contents','get-ordered-list','delete-by-content-type-id');
 	
     public function init() {
         parent::init();
@@ -221,7 +221,7 @@ class Backoffice_ContentsController extends Backoffice_DataAccessController
 		$this->_returnJson($result);
 	}
 	
-	public function deleteByContentIdAction(){
+	public function deleteByContentTypeIdAction(){
 	    $typeId = $this->getParam('type-id');
 	    if(!$typeId){
 	        throw new Rubedo\Exceptions\User('Un type de contenu est nécessaire en argument type-id');
