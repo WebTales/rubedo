b8b4a7485de718ce1d38a5e696be747ee0e6ef9b
goncalvesm <mickael.goncalves@webtales.fr>
2013-05-30 16:24:58 +0200

    Display a message when the configuration is bad

--- application/modules/blocks/controllers/AdvancedContactController.php
+++ application/modules/blocks/controllers/AdvancedContactController.php
@@ -32,65 +32,75 @@ class Blocks_AdvancedContactController extends Blocks_AbstractController
     public function indexAction ()
     {
     	$blockConfig = $this->getRequest()->getParam('block-config');
-    	$formName = $blockConfig["formName"];
-    	$output = $this->getAllParams();
+    	$formName = isset($blockConfig["formName"]) ? $blockConfig["formName"] : null;
     	
-    	$errors = array();
-    	
-    	$objectPath = "Blocks_Model_".$formName;
-    	
-    	$formPath = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/forms/".$formName.".html.twig");
-    	$form = Manager::getService('FrontOfficeTemplates')->render($formPath, array());
-    	
-    	/*if(isset($blockConfig['captcha'])){
-    		$form = new $objectPath(null, $blockConfig['captcha']);
+    	if($formName !== null) {
+        	$output = $this->getAllParams();
+        	
+        	$errors = array();
+        	
+        	$objectPath = "Blocks_Model_".$formName;
+        	
+        	$formPath = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/forms/".$formName.".html.twig");
+        	
+        	if(file_exists($formPath)) {
+            	$form = Manager::getService('FrontOfficeTemplates')->render($formPath, array());
+            	
+            	/*if(isset($blockConfig['captcha'])){
+            		$form = new $objectPath(null, $blockConfig['captcha']);
+            	} else {
+            		$form = new $objectPath();
+            	}*/
+            	
+            	//Check if the form was send
+            	if ($this->getParam("post")) {
+            	    
+        	        $twigVar = $this->_request->getPost();
+        	        
+        	        $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/email-templates/".$formName.".html.twig");
+                    $mailBody = Manager::getService('FrontOfficeTemplates')->render($template, $twigVar);
+        	        
+        			//Create a mailer object
+        			$mailerService = Manager::getService('Mailer');
+        			$mailerObject = $mailerService->getNewMessage();
+        			$stringRecipients = "";
+        			
+        			//Get recipients from block config
+        			$recipients = $blockConfig['contacts'];
+        			
+        			if($recipients){
+            			//Build e-mail
+            			$subject = "[Rubedo]";
+            			
+            			$mailerObject->setSubject($subject);
+            			$mailerObject->setFrom("admin@rubedo.fr");
+            			$mailerObject->setTo($recipients);
+            			$mailerObject->setBody($mailBody);
+            			
+            			//Send e-mail
+            			$sendResult = $mailerService->sendMessage($mailerObject, $errors);
+            			
+            			if(!$sendResult){
+            			    $errors[] = "L'envoi du mail à échoué, merci de réessayer ultèrieurement";
+            			} else {
+            			    $output['sendResult'] = true;
+            			}
+        			} else {
+        			    $errors[] = "Merci de renseigner un destinataire dans les paramètres de configuration du bloc contact avancé.";
+        			}
+            	}
+            	
+            	if(count($errors)>0){
+            		$output['errors'] = $errors;
+            	}
+            	
+                $output["blockConfig"]=$blockConfig;
+        	} else {
+        	    $form = "<div class=\"alert alert-danger\">Le nom de template spécifié n'éxiste pas.</div>";
+        	}
     	} else {
-    		$form = new $objectPath();
-    	}*/
-    	
-    	//Check if the form was send
-    	if ($this->getParam("post")) {
-    	    
-	        $twigVar = $this->_request->getPost();
-	        
-	        $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/email-templates/".$formName.".html.twig");
-            $mailBody = Manager::getService('FrontOfficeTemplates')->render($template, $twigVar);
-	        
-			//Create a mailer object
-			$mailerService = Manager::getService('Mailer');
-			$mailerObject = $mailerService->getNewMessage();
-			$stringRecipients = "";
-			
-			//Get recipients from block config
-			$recipients = $blockConfig['contacts'];
-			
-			if($recipients){
-    			//Build e-mail
-    			$subject = "[Rubedo]";
-    			
-    			$mailerObject->setSubject($subject);
-    			$mailerObject->setFrom("admin@rubedo.fr");
-    			$mailerObject->setTo($recipients);
-    			$mailerObject->setBody($mailBody);
-    			
-    			//Send e-mail
-    			$sendResult = $mailerService->sendMessage($mailerObject, $errors);
-    			
-    			if(!$sendResult){
-    			    $errors[] = "L'envoi du mail à échoué, merci de réessayer ultèrieurement";
-    			} else {
-    			    $output['sendResult'] = true;
-    			}
-			} else {
-			    $errors[] = "Merci de renseigner un destinataire dans les paramètres de configuration du bloc contact avancé.";
-			}
+    	    $form = "<div class=\"alert alert-danger\">Vous devez spécifier un nom de template.</div>";
     	}
-    	
-    	if(count($errors)>0){
-    		$output['errors'] = $errors;
-    	}
-    	
-        $output["blockConfig"]=$blockConfig;
         
         if (isset($blockConfig['displayType']) && !empty($blockConfig['displayType'])) {
             $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath(
