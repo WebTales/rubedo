9add3881360a894904faf4bf571ad7e7377af4da
Julien Bourdin <jbourdin@gmail.com>
2013-06-24 18:19:36 +0200

    Set version information

--- Core/Rubedo/Version/Version.php
+++ Core/Rubedo/Version/Version.php
@@ -31,7 +31,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.2.1dev';
+    const VERSION = '1.2.1';
 
     /**
      * Github Service Identifier for version information is retreived from
