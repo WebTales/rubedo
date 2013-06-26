897226888ec979104ddcf0e77136ac4dd199d0d3
Julien Bourdin <julien.bourdin@webtales.fr>
2013-06-26 18:52:43 +0200

    prepare for 1.3.0

--- Core/Rubedo/Version/Version.php
+++ Core/Rubedo/Version/Version.php
@@ -31,7 +31,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.2.2dev';
+    const VERSION = '1.3.0dev';
 
     /**
      * Github Service Identifier for version information is retreived from
