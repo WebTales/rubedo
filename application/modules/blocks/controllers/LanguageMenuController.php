bd8f9ac167504403272b1f0a8e8503a618408aef
Julien Bourdin <jbourdin@gmail.com>
2013-07-08 18:02:05 +0200

    implement menu language

--- application/modules/blocks/controllers/LanguageMenuController.php
+++ application/modules/blocks/controllers/LanguageMenuController.php
@@ -21,6 +21,8 @@ require_once ('AbstractController.php');
 
 /**
  *
+ *
+ *
  * Block to display front office language menu
  *
  * @author jbourdin
@@ -33,26 +35,45 @@ class Blocks_LanguageMenuController extends Blocks_AbstractController
     /**
      * Default Action, return the Ext/Js HTML loader
      */
-    public function indexAction ()
+    public function indexAction()
     {
         $output = $this->getAllParams();
-        $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/language-menu.html.twig");
-
-        $currentLocale =  Manager::getService('CurrentLocalization')->getCurrentLocalization();
+        
+        if (isset($output['block-config']['displayAs'])) {
+            switch ($output['block-config']['displayAs']) {
+                case "menu":
+                    $displayType = 'menu';
+                    break;
+                case "select":
+                default:
+                    $displayType = 'combo';
+                    break;
+            }
+        } else {
+            $displayType = 'combo';
+        }
+        
+        $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/language-menu/" . $displayType . ".html.twig");
+        
+        $currentLocale = Manager::getService('CurrentLocalization')->getCurrentLocalization();
         
         $output['currentLanguage'] = Manager::getService('Languages')->findByLocale($currentLocale);
         
         $site = $this->getParam('site');
-        if(isset($site['languages'])){
+        if (isset($site['languages'])) {
             $filters = Filter::factory();
-            $filters->addFilter(Filter::factory('In')->setName('locale')->setValue($site['languages']));
-            $filters->addFilter(Filter::factory('Value')->setName('active')->setValue(true));
-            $languageResult =  Manager::getService('Languages')->getList($filters);
+            $filters->addFilter(Filter::factory('In')->setName('locale')
+                ->setValue($site['languages']));
+            $filters->addFilter(Filter::factory('Value')->setName('active')
+                ->setValue(true));
+            $languageResult = Manager::getService('Languages')->getList($filters);
             $output['languages'] = $languageResult['data'];
-        }     
+        }
         
         $css = array();
-        $js = array('/templates/' . Manager::getService('FrontOfficeTemplates')->getFileThemePath("js/language.js"));
+        $js = array(
+            '/templates/' . Manager::getService('FrontOfficeTemplates')->getFileThemePath("js/language.js")
+        );
         $this->_sendResponse($output, $template, $css, $js);
     }
 }
