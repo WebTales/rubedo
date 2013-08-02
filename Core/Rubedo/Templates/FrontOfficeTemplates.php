6bc73c54c74120007ef735e92fcd92b62d69cbe4
Julien Bourdin <jbourdin@gmail.com>
2013-08-02 15:17:46 +0200

    Add a 'ucfirst' filter for twig. Translate event template

--- Core/Rubedo/Templates/FrontOfficeTemplates.php
+++ Core/Rubedo/Templates/FrontOfficeTemplates.php
@@ -121,6 +121,7 @@ class FrontOfficeTemplates implements IFrontOfficeTemplates
         $this->_twig->addFunction('getDam', new \Twig_Function_Function('\\Rubedo\\Templates\\FrontOfficeTemplates::getDam'));
         $this->_twig->addFunction('getContent', new \Twig_Function_Function('\\Rubedo\\Templates\\FrontOfficeTemplates::getContent'));
         $this->_twig->addFunction('isInRootline', new \Twig_Function_Function('\\Rubedo\\Templates\\FrontOfficeTemplates::isInRootline'));
+        $this->_twig->addFilter(new \Twig_SimpleFilter('ucfirst','\\Rubedo\\Templates\\FrontOfficeTemplates::mbucfirst'));
     }
 
     /**
@@ -330,4 +331,17 @@ class FrontOfficeTemplates implements IFrontOfficeTemplates
     public static function isInRootline($pageId) {
         return Manager::getService("Pages")->isInRootline($pageId);
     }
+    
+    public static function mbucfirst($string) {
+        $e ='utf-8';
+        if (function_exists('mb_strtoupper') && function_exists('mb_substr') && !empty($string)) { 
+            $string = mb_strtolower($string, $e); 
+            $upper = mb_strtoupper($string, $e); 
+            preg_match('#(.)#us', $upper, $matches); 
+            $string = $matches[1] . mb_substr($string, 1, mb_strlen($string, $e), $e); 
+        } else { 
+            $string = ucfirst($string); 
+        } 
+        return $string; 
+    } 
 }
