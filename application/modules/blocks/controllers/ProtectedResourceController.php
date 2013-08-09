c73955e800bf9fcf5acf023bc35daa19b6793a4d
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-08-09 16:16:18 +0200

    change protected resource block rendering

--- application/modules/blocks/controllers/ProtectedResourceController.php
+++ application/modules/blocks/controllers/ProtectedResourceController.php
@@ -42,8 +42,11 @@ class Blocks_ProtectedResourceController extends Blocks_AbstractController
         $blockConfig = $this->getParam('block-config', array());
         $output = $this->getAllParams();
         
-        if (isset($blockConfig['introduction'])) {
-            $output['introduction'] = $blockConfig['introduction'];
+        if ((isset($blockConfig['introduction']))&&($blockConfig['introduction']!="")) {
+            $content = Manager::getService('Contents')->findById($blockConfig["introduction"],true,false);
+            $output['contentId'] = $blockConfig["introduction"];
+            $output['text'] = $content["fields"]["body"];
+            $output["locale"] = isset($content["locale"]) ? $content["locale"] : null;
         }
         
         $output['mailingListId'] = $blockConfig['mailingListId'];
