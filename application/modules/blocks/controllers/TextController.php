13898f8c7d7d97682f4c55b54ba78806fba9bdfa
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-08-09 12:52:06 +0200

    fix mongoId error on simpletext and richtext blocks

--- application/modules/blocks/controllers/TextController.php
+++ application/modules/blocks/controllers/TextController.php
@@ -33,7 +33,10 @@ class Blocks_TextController extends Blocks_AbstractController
     public function indexAction ()
     {
         $blockConfig = $this->getParam('block-config', array());
-        $content = Manager::getService('Contents')->findById($blockConfig["contentId"],true,false);
+        $content=array();
+        if ($blockConfig["contentId"]){
+            $content = Manager::getService('Contents')->findById($blockConfig["contentId"],true,false);
+        }
         $output = $this->getAllParams();
         $output['text'] = $content["fields"]["body"];
         $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/text.html.twig");
