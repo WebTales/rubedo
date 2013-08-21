2b61cddba878a088e842c642eb598a755fe68ba1
Julien Bourdin <jbourdin@gmail.com>
2013-08-21 11:06:55 +0200

    begin routing and debugging of new MVC paths

--- module/Rubedo/config/module.config.php
+++ module/Rubedo/config/module.config.php
@@ -152,7 +152,76 @@ foreach ($serviceArray as $key => $value) {
     $serviceMapArray[$key] = $value['class'];
     $serviceSharedMapArray[$key] = false;
 }
-return array(
+
+$config = array(
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+//                         'controller' => 'Application\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /application/:controller/:action
+            'application' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/backoffice',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Rubedo\Backoffice\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            '__NAMESPACE__' => 'Rubedo\Backoffice\Controller',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+//             'Application\Controller\Index' => 'Application\Controller\IndexController',
+//             'Application\Controller\Projects' => 'Application\Controller\ProjectsController'
+        ),
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'               => __DIR__ . '/../view/layout/layout.phtml',
+            //'application/index/index'     => __DIR__ . '/../view/application/index/index.phtml',
+            //'application/projects/index'  => __DIR__ . '/../view/application/projects/index.phtml',
+            'error/404'                   => __DIR__ . '/../view/error/404.phtml',
+            'error/index'                 => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
     'datastream' => array(
         'mongo' => array(
             'server' => 'localhost',
@@ -171,3 +240,5 @@ return array(
         'enableCache' => '0'
     ),
 );
+
+return $config;
