01c36911febc27277a58ca6e39aab83aeba75344
goncalvesm <mickael.goncalves@webtales.fr>
2013-08-21 11:14:45 +0200

    Refactor FO inline editing

--- application/modules/default/controllers/XhrEditController.php
+++ application/modules/default/controllers/XhrEditController.php
@@ -56,15 +56,15 @@ class XhrEditController extends Zend_Controller_Action
     public function indexAction ()
     {
         $data = Zend_Json::decode($this->getParam("data", null));
+        $locale = $this->getParam("locale", null);
         $errors = array();
         
         foreach ($data as $contentId => $value) {
+            //Get content id, the field name and the optionnal number of multivalued field
             $contentId = explode("_", $contentId);
             $id = $contentId[0];
             $field = $contentId[1];
             $localizable = false;
-            
-            $locale = isset($value["locale"]) ? $value["locale"] : null;
             
             $field = explode("-", $field);
             $name = $field[0];
@@ -72,36 +72,43 @@ class XhrEditController extends Zend_Controller_Action
             if (count($field) > 1) {
                 $index = $field[1];
             }
+            
+            //Check if we got this 3 informations
             if ($id === null || $data === null || $name === null) {
                 throw new \Rubedo\Exceptions\Server("You must provide the concerned content id, the new value and the field which had to be updated in database", "Exception27");
             }
+            
             // correcting value in case of false bool
             if ($data == 'false') {
                 $data = false;
             }
             
+            //Get the content and its content type
             $content = $this->_dataService->findById($id, true, false);
             $contentType = Manager::getService("ContentTypes")->findById($content['typeId']);
             
+            //Throw an exception if the content doesn't exist
+            if (! $content) {
+                throw new \Rubedo\Exceptions\Server('This content id does not exist: %1$s', "Exception28", $id);
+            }
+            
+            //Check if the field is localizable
             foreach ($contentType["fields"] as $fieldObj) {
                 if($fieldObj["config"]["name"] === $name) {
                     $localizable = isset($fieldObj["config"]["localizable"]) ? $fieldObj["config"]["localizable"] : false;
                 }
             }
             
+            //Force localizable state for system fields
             if($name == "text" || $name == "summary") {
                 $localizable = true;
             }
             
-            if (! $content) {
-                throw new \Rubedo\Exceptions\Server('This content id does not exist: %1$s', "Exception28", $id);
-            }
-            
+            //Check if the content has already being updated
             if ($content["status"] !== 'published') {
                 $errors[] = 'Content already have a draft version';
             } else {
                 if($localizable) {
-                    
                     if($locale !== null) {
                         //Create the translation if it doesn't exist
                         if(!isset($content["i18n"][$locale])) {
@@ -111,6 +118,7 @@ class XhrEditController extends Zend_Controller_Action
                             $content["i18n"][$locale]["locale"] = $locale;
                         }
                         
+                        //Update the content object
                         if (count($field) > 1) {
                             $content["i18n"][$locale]['fields'][$name][$index] = $value["newValue"];
                         } else {
@@ -120,6 +128,7 @@ class XhrEditController extends Zend_Controller_Action
                         $errors[] = "You must provide the current language of the content to update it (".$content['id'].")";
                     }
                 } else {
+                    //Update the content object
                     if (count($field) > 1) {
                         $content['fields'][$name][$index] = $value["newValue"];
                     } else {
@@ -127,14 +136,17 @@ class XhrEditController extends Zend_Controller_Action
                     }
                 }
                 
+                //Update the content in DB
                 $updateResult = $this->_dataService->update($content, array(), false);
                 
+                //Check if the content was successfully updated
                 if (! $updateResult['success']) {
                     $errors[] = "Failed to update the content \"" . $content["text"] . "\"";
                 }
             }
         }
         
+        //Send reponse
         if (count($errors) > 0) {
             return $this->_helper->json(array(
                 "success" => false,
