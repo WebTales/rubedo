4d7b662116a4cc3f0577a47772d8dfc45287c39b
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-09-02 18:32:17 +0200

    protect foreach loop with isset

--- application/modules/blocks/controllers/ContentSingleController.php
+++ application/modules/blocks/controllers/ContentSingleController.php
@@ -182,10 +182,12 @@ class Blocks_ContentSingleController extends Blocks_AbstractController
             }
             $hasCustomLayout=false;
             $customLayoutRows=array();
-            foreach ($type['layouts'] as $key => $value) {
-                if (($value['type']=="Detail")&&($value['active'])){
-                    $hasCustomLayout=true;
-                    $customLayoutRows=$value['rows'];
+            if (isset($type['layouts'])){
+                foreach ($type['layouts'] as $key => $value) {
+                    if (($value['type']=="Detail")&&($value['active'])){
+                        $hasCustomLayout=true;
+                        $customLayoutRows=$value['rows'];
+                    }
                 }
             }
             $output["data"] = $data;
