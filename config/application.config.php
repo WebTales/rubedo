74fff42299475023ff58684d3f1b3a7ec763f769
Julien Bourdin <jbourdin@gmail.com>
2013-09-03 10:34:19 +0200

    autoload extension in extensions directory

--- config/application.config.php
+++ config/application.config.php
@@ -1,10 +1,26 @@
 <?php
+// define a constant for the root dir of the application
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(__DIR__ . '/..'));
 
+// automatically discover extensions for Rubedo
+$extensionsArray = array();
+
+$extensionsIterator = new DirectoryIterator('extensions');
+foreach ($extensionsIterator as $item) {
+    if ($item->isDot()) {
+        continue;
+    }
+    if ($item->isDir()) {
+        $extensionsArray[] = $item->getFilename();
+    }
+}
+unset($extensionsIterator);
+
+//return configuration array. Similar to standard ZF2 app with an "extensions directory" and a list of modules from this directory
 return array(
-    'modules' => array(
+    'modules' => array_merge(array(
         'Rubedo'
-    ),
+    ), $extensionsArray),
     'module_listener_options' => array(
         'config_cache_enabled' => true,
         'module_map_cache_enabled' => true,
---
+++ extensions/readme.txt
@@ -0,0 +1,3 @@
+Here you can add Rubedo Extension.
+
+A Rubedo Extension is structured as a ZF2 module.
