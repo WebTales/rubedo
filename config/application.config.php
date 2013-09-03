40b70337bd7f65b01b57c86361a9838859fd4ee0
Julien Bourdin <jbourdin@gmail.com>
2013-09-03 10:24:29 +0200

    Prepare for extensions

--- config/application.config.php
+++ config/application.config.php
@@ -2,26 +2,18 @@
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(__DIR__ . '/..'));
 
 return array(
-    // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Rubedo'
     ),
-    // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
-        'config_cache_enabled'     => true,
+        'config_cache_enabled' => true,
         'module_map_cache_enabled' => true,
-        'cache_dir'                => 'cache/config',
-        // This should be an array of paths in which modules reside.
-        // If a string key is provided, the listener will consider that a module
-        // namespace, the value of that key the specific path to that module's
-        // Module class.
+        'cache_dir' => 'cache/config',
         'module_paths' => array(
             './module',
-            './vendor'
+            './vendor',
+            './extensions'
         ),
-        // An array of paths from which to glob configuration files after
-        // modules are loaded. These effectively overide configuration
-        // provided by modules themselves. Paths may use GLOB_BRACE notation.
         'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php'
         )
