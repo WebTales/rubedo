6b07e651267ac1eea0ec1be822de8b611ebc9d92
Julien Bourdin <jbourdin@gmail.com>
2013-09-17 16:57:18 +0200

    add Queue controller to controller list

--- module/Rubedo/config/frontoffice.controllers.config.php
+++ module/Rubedo/config/frontoffice.controllers.config.php
@@ -9,5 +9,6 @@ return array(
     'Rubedo\\Frontoffice\\Controller\\XhrAuthentication' => 'Rubedo\\Frontoffice\\Controller\\XhrAuthenticationController',
     'Rubedo\\Frontoffice\\Controller\\XhrEdit' => 'Rubedo\\Frontoffice\\Controller\\XhrEditController',
     'Rubedo\\Frontoffice\\Controller\\Tiny' => 'Rubedo\\Frontoffice\\Controller\\TinyController',
-    'Rubedo\\Frontoffice\\Controller\\Theme' => 'Rubedo\\Frontoffice\\Controller\\ThemeController'
+    'Rubedo\\Frontoffice\\Controller\\Theme' => 'Rubedo\\Frontoffice\\Controller\\ThemeController',
+    'Rubedo\\Frontoffice\\Controller\\Queue' => 'Rubedo\\Frontoffice\\Controller\\QueueController'
 );
