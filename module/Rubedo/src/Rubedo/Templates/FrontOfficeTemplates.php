3489f0f1c2311d7044dce643a7ccf935d3c325b0
Julien Bourdin <jbourdin@gmail.com>
2013-09-23 17:06:22 +0200

    fix other customtheme resources

--- module/Rubedo/src/Rubedo/Templates/FrontOfficeTemplates.php
+++ module/Rubedo/src/Rubedo/Templates/FrontOfficeTemplates.php
@@ -241,6 +241,9 @@ class FrontOfficeTemplates implements IFrontOfficeTemplates
             return false;
         }
         $config = $this->getConfig();
+        if($theme == 'customtheme'){
+            $theme = 'default';
+        }
         if (! isset($config['themes'][$theme])) {
             return false;
         }
