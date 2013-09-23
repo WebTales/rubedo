8f5828d78f6b105f107e77cc9807c2548c698e86
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-09-23 14:54:37 +0200

    fix css controller

--- module/Rubedo/src/Rubedo/Frontoffice/Controller/CssController.php
+++ module/Rubedo/src/Rubedo/Frontoffice/Controller/CssController.php
@@ -14,7 +14,11 @@
  * @copyright  Copyright (c) 2012-2013 WebTales (http://www.webtales.fr)
  * @license    http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
  */
+namespace Rubedo\Frontoffice\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
 use Rubedo\Services\Manager;
+use Zend\Json\Json;
 
 /**
  * Controller providing css for custom themes
@@ -26,25 +30,23 @@ use Rubedo\Services\Manager;
  * @package Rubedo
  *         
  */
-class CssController extends Zend_Controller_Action
+class CssController extends AbstractActionController
 {
     function indexAction ()
     {
-        $this->_helper->layout->disableLayout();
-        $this->_helper->viewRenderer->setNoRender();
-        $customThemeId = $this->getRequest()->getParam('id');
+        $customThemeId = $this->params()->fromQuery('id');
         $customTheme=Manager::getService('CustomThemes')->findById($customThemeId);
-        $less = new lessc;
+        $less = new \lessc;
         $less->setImportDir(array(APPLICATION_PATH."/public/components/webtales/bootstrap-less/less/"));
-        $baseThemeOverrides=Zend_Json::decode($customTheme['lessVarsJson']);
+        $baseThemeOverrides=Json::decode($customTheme['lessVarsJson'], Json::TYPE_ARRAY);
         $less->setVariables($baseThemeOverrides);
         $compiledCss=$less->compileFile(APPLICATION_PATH."/public/components/webtales/bootstrap-less/less/bootstrapoverrider.less");
-        $this->getResponse()->clearBody();
-        $this->getResponse()->clearHeaders();
-        $this->getResponse()->clearRawHeaders();
-        $this->getResponse()->setHeader('Content-Type', 'text/css');
-        $this->getResponse()->setHeader('Pragma', 'Public',true);
-        $this->getResponse()->setBody($compiledCss);
-        $this->getResponse()->sendHeaders();
+        $response = new \Zend\Http\Response();
+        $response->getHeaders()->addHeaders(array(
+            'Content-type' => 'text/css',
+            'Pragma' => 'Public'
+        ));
+        $response->setContent($compiledCss);
+        return($response);
     }
 }
