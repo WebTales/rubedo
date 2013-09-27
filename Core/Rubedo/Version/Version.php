ec4d4f5b384c8a60182b2684c091a95e2d79a14d
Julien Bourdin <jbourdin@gmail.com>
2013-09-27 16:17:45 +0200

    move to 1.4.1 version

--- Core/Rubedo/Version/Version.php
+++ Core/Rubedo/Version/Version.php
@@ -31,7 +31,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.4.1dev';
+    const VERSION = '1.4.1';
 
     /**
      * Github Service Identifier for version information is retreived from
