c42b7e20d393aa00b8a0ade79a11ac13da2d2e83
Julien Bourdin <julien.bourdin@webtales.fr>
2013-10-07 16:40:28 +0200

    force COMPOSER on windows scripts

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 php composer.phar self-update
+set COMPOSER=composer.json 
 php composer.phar install -o
 set COMPOSER=composer.front.json 
 php composer.phar install
--- update.bat
+++ update.bat
@@ -1,4 +1,5 @@
 php composer.phar self-update
+set COMPOSER=composer.json 
 php composer.phar update -o
 set COMPOSER=composer.front.json 
 php composer.phar update
