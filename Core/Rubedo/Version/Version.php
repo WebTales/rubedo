8c68a20c88a99da0f2dce4f1b3aa8338f0f1194a
Julien Bourdin <julien.bourdin@webtales.fr>
2013-10-15 15:16:38 +0200

    Set version to 1.4.3

--- Core/Rubedo/Version/Version.php
+++ Core/Rubedo/Version/Version.php
@@ -31,7 +31,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.4.2';
+    const VERSION = '1.4.3';
 
     /**
      * Github Service Identifier for version information is retreived from
