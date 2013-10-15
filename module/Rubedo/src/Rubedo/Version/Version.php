759e745e70acefd3b1bd2dec820218f1b0121fc1
Julien Bourdin <julien.bourdin@webtales.fr>
2013-10-15 23:26:35 +0200

    switch to next branch

--- module/Rubedo/src/Rubedo/Version/Version.php
+++ module/Rubedo/src/Rubedo/Version/Version.php
@@ -32,7 +32,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '2.0.0';
+    const VERSION = '2.1.0dev';
 
     /**
      * Github Service Identifier for version information is retreived from
