e12ddf1b9ac74d3eff1dacee2d133d92948fad28
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-11-06 16:30:22 +0100

    sign up confirmation by email

--- module/Rubedo/src/Rubedo/Blocks/Controller/SignUpController.php
+++ module/Rubedo/src/Rubedo/Blocks/Controller/SignUpController.php
@@ -18,6 +18,8 @@ namespace Rubedo\Blocks\Controller;
 
 use Rubedo\Services\Manager;
 use Zend\Debug\Debug;
+use Zend\View\Model\JsonModel;
+use Zend\Json\Json;
 
 /**
  *
@@ -58,12 +60,48 @@ class SignUpController extends AbstractController
                 $newUser['status']="approved";
             } else if ($userType['signUpType']=="moderated") {
                 $newUser['status']="pending";
+            } else if ($userType['signUpType']=="emailConfirmation") {
+                $newUser['status']="emailUnconfirmed";
+                $currentTimeService = Manager::getService('CurrentTime');
+                $currentTime = $currentTimeService->getCurrentTime();
+                $newUser['signupTime']=$currentTime;
             }
             
             $createdUser=Manager::getService('Users')->create($newUser);
             if (!$createdUser['success']) {
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/fail.html.twig");
                 return $this->_sendResponse($output, $template);
+            } else if ($userType['signUpType']=="emailConfirmation"){
+                //send email here
+                $emailVars=array();
+                $emailVars["name"]=$newUser["name"];
+                $emailVars["confirmUrl"]=$_SERVER['HTTP_REFERER'].'?confirmingEmail=1&userId='.$createdUser['data']['id'].'&signupTime='.$newUser["signupTime"];
+                $etemplate = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/confirm-email-body.html.twig");
+                $mailBody = Manager::getService('FrontOfficeTemplates')->render($etemplate, $emailVars);
+                $mailService = Manager::getService('Mailer');
+                $message = $mailService->getNewMessage();
+                $message->setTo(array(
+                    $newUser["email"]
+                ));
+                $config = Manager::getService('config');
+                $options = $config['rubedo_config'];
+                $message->setFrom(array($options['fromEmailNotification']=>"Rubedo"));
+                $message->setSubject('['.Manager::getService('Sites')->getHost($output['site']['id']).'] '.'Account creation');
+                
+                $message->setBody($mailBody);
+                $message->addPart($mailBody, 'text/html');
+                
+                $result = $mailService->sendMessage($message);
+                if ($result===1){
+                    $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/confirmEmail.html.twig");
+                    return $this->_sendResponse($output, $template);
+                } else {
+                    $output['signupMessage']="Unable to send confirmation email";
+                    $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
+                    return $this->_sendResponse($output, $template);
+                }
+                
+                
             } else {
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/done.html.twig");
                 return $this->_sendResponse($output, $template);
@@ -71,6 +109,43 @@ class SignUpController extends AbstractController
         }
         $blockConfig = $this->params()->fromQuery('block-config', array());
         $output = $this->params()->fromQuery();
+        if (isset($output["confirmingEmail"])){
+            
+            $userId = $this->params()->fromQuery("userId");
+            $signupTime = $this->params()->fromQuery("signupTime");
+            if ((!isset($userId))||(!isset($signupTime))){
+                $output['signupMessage']="Missing required parameters";
+                $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
+                return $this->_sendResponse($output, $template);
+            }
+            $user=Manager::getService("Users")->findById($userId);
+            if (! $user){
+                $output['signupMessage']="Unknown user";
+                $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
+                return $this->_sendResponse($output, $template);
+            }
+            if ($user['status']!="emailUnconfirmed"){
+                $output['signupMessage']="Account email already confimred";
+                $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
+                return $this->_sendResponse($output, $template);
+            }
+            if ($user['signupTime']!=$signupTime){
+                $output['signupMessage']="Invalid sign up time";
+                $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
+                return $this->_sendResponse($output, $template);
+            }
+            $user["status"]="approved";
+            $update=Manager::getService("Users")->update($user);
+            if (!$update['success']){
+                $output['signupMessage']="Error during user update";
+                $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
+                return $this->_sendResponse($output, $template);
+            } else {
+                $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailConfirmed.html.twig");
+                return $this->_sendResponse($output, $template);
+            }
+            
+        }
         if (! isset($blockConfig['userType'])) {
             return $this->_sendResponse(array(), "block.html.twig");
         }
@@ -91,6 +166,5 @@ class SignUpController extends AbstractController
         $js = array();
         return $this->_sendResponse($output, $template, $css, $js);
     }
-    
-   
+     
 }
