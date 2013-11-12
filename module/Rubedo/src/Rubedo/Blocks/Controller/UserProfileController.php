43aaa7319576b231a00b0126649f7b7f6b2648ad
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-11-12 17:49:18 +0100

    basic profile page

--- module/Rubedo/src/Rubedo/Blocks/Controller/UserProfileController.php
+++ module/Rubedo/src/Rubedo/Blocks/Controller/UserProfileController.php
@@ -17,6 +17,7 @@
 namespace Rubedo\Blocks\Controller;
 
 use Rubedo\Services\Manager;
+use Zend\Debug\Debug;
 
 /**
  *
@@ -31,6 +32,17 @@ class UserProfileController extends AbstractController
     {
         //$blockConfig = $this->params()->fromQuery('block-config', array());
         $output = $this->params()->fromQuery();
+        $currentUser=Manager::getService("CurrentUser")->getCurrentUser();
+        if (!$currentUser){
+            $output['errorMessage']="You need to be logged-in to view your profile";
+            $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/userProfile/error.html.twig");
+            return $this->_sendResponse($output, $template);
+        }
+
+        $user=$currentUser;
+        $userType=Manager::getService("UserTypes")->findById($user['typeId']);
+        $output['user']=$user;
+        $output['fieldTypes']=$userType['fields'];
         $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/userProfile.html.twig");
         $css = array();
         $js = array();
