191678342dbc296e27dc2191316a1726efc9e30d
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-12-11 14:15:48 +0100

    multiML subscription

--- templates/root/js/mailingList.js
+++ templates/root/js/mailingList.js
@@ -1,9 +1,23 @@
-function addEmail(email, mailingListId) {
+function addEmail() {
+	
+	
+	var email=jQuery("#mailingListEmail").val();
+	var mailingLists= [ ];
+	var canContinue=false;
+	jQuery(".mailingListCheck").each(function(thing, thing2){
+		if (jQuery(this).is(":checked")) {
+			mailingLists.push(jQuery(this).attr("name"));
+			canContinue=true;
+		}
+	});
+	if (canContinue){
+		
+	
 	var request = jQuery.ajax({
 		url : "/blocks/"+jQuery('html').attr('lang')+"/mailing-list/xhr-add-email",
 		type : "POST",
 		data : {
-			'mailing-list-id' : mailingListId,
+			'mailing-list-id' : JSON.stringify(mailingLists),
 			'email' : email,
 			'current-page':jQuery('body').attr('data-current-page')
 		},
@@ -35,4 +49,6 @@ function addEmail(email, mailingListId) {
 		jQuery("#mailingListAlert").removeClass("alert-success");
 		jQuery("#mailingListAlert").addClass("alert-danger");
 	});
+	
+	}
 }
