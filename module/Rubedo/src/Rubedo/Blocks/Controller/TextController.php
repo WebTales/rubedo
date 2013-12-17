98d3259e987e015a2c162240f33972d9a0e489ee
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-12-17 16:13:27 +0100

    fix fail

--- module/Rubedo/src/Rubedo/Blocks/Controller/TextController.php
+++ module/Rubedo/src/Rubedo/Blocks/Controller/TextController.php
@@ -34,6 +34,9 @@ class TextController extends AbstractController
         if ($blockConfig["contentId"]) {
             $content = Manager::getService('Contents')->findById($blockConfig["contentId"], true, false);
         }
+        if (! $content) {
+            return $this->_sendResponse(array(), "block.html.twig");
+        }
         $output = $this->params()->fromQuery();
         $output['contentId'] = $blockConfig["contentId"];
         $output['text'] = $content["fields"]["body"];
