a97b52007f9d470a3afafb299cd39e3f4495a9c1
Gaël Demette <gael.demette@webtales.fr>
2014-01-03 11:03:29 +0100

    Recovery system for password + Normalize

--- module/Rubedo/src/Rubedo/Blocks/Controller/SignUpController.php
+++ module/Rubedo/src/Rubedo/Blocks/Controller/SignUpController.php
@@ -7,7 +7,7 @@
  *
  * Open Source License
  * ------------------------------------------------------------------------------------------
- * Rubedo is licensed under the terms of the Open Source GPL 3.0 license. 
+ * Rubedo is licensed under the terms of the Open Source GPL 3.0 license.
  *
  * @category   Rubedo
  * @package    Rubedo
@@ -17,9 +17,6 @@
 namespace Rubedo\Blocks\Controller;
 
 use Rubedo\Services\Manager;
-use Zend\Debug\Debug;
-use Zend\View\Model\JsonModel;
-use Zend\Json\Json;
 
 /**
  *
@@ -30,75 +27,75 @@ use Zend\Json\Json;
 class SignUpController extends AbstractController
 {
 
-    public function indexAction ()
+    public function indexAction()
     {
-        if ($this->getRequest()->isPost()){
+        if ($this->getRequest()->isPost()) {
             $params = $this->params()->fromPost();
             $output = $this->params()->fromQuery();
-            if ((!isset($params['name']))||(!isset($params['email']))||(!isset($params['userTypeId']))){
+            if ((!isset($params['name'])) || (!isset($params['email'])) || (!isset($params['userTypeId']))) {
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/fail.html.twig");
                 return $this->_sendResponse($output, $template);
             }
-            $collectPassword=isset($output['block-config']['collectPassword']) ? $output['block-config']['collectPassword'] : false;
-            if ($collectPassword){
-                if ((!isset($params['password']))||(!isset($params['confirmPassword']))){
-                    $output['signupMessage']="Missing password parameters";
+            $collectPassword = isset($output['block-config']['collectPassword']) ? $output['block-config']['collectPassword'] : false;
+            if ($collectPassword) {
+                if ((!isset($params['password'])) || (!isset($params['confirmPassword']))) {
+                    $output['signupMessage'] = "Missing password parameters";
                     $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                     return $this->_sendResponse($output, $template);
                 }
-                if ($params['password']!=$params['confirmPassword']){
-                    $output['signupMessage']="Password parameters don't match";
+                if ($params['password'] != $params['confirmPassword']) {
+                    $output['signupMessage'] = "Password parameters don't match";
                     $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                     return $this->_sendResponse($output, $template);
                 }
             }
-            $alreadyExistingUser=Manager::getService("Users")->findByEmail($params['email']);
-            if ($alreadyExistingUser){
-                $output['signupMessage']="Email already in use.";
+            $alreadyExistingUser = Manager::getService("Users")->findByEmail($params['email']);
+            if ($alreadyExistingUser) {
+                $output['signupMessage'] = "Email already in use.";
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                 return $this->_sendResponse($output, $template);
             }
-            $userType=Manager::getService('UserTypes')->findById($params['userTypeId']);
-            if ($userType['signUpType']=="none") {
+            $userType = Manager::getService('UserTypes')->findById($params['userTypeId']);
+            if ($userType['signUpType'] == "none") {
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/fail.html.twig");
                 return $this->_sendResponse($output, $template);
             }
-            $newUser=array();
-            $newUser['name']=$params['name'];
-            $newUser['email']=$params['email'];
-            $newUser['login']=$params['email'];
-            $newUser['typeId']=$params['userTypeId'];
-            $newUser['defaultGroup']=$userType['defaultGroup'];
-            $newUser['groups']=array($userType['defaultGroup']);
-            $newUser['taxonomy']=array();
+            $newUser = array();
+            $newUser['name'] = $params['name'];
+            $newUser['email'] = $params['email'];
+            $newUser['login'] = $params['email'];
+            $newUser['typeId'] = $params['userTypeId'];
+            $newUser['defaultGroup'] = $userType['defaultGroup'];
+            $newUser['groups'] = array($userType['defaultGroup']);
+            $newUser['taxonomy'] = array();
             unset($params['name']);
             unset($params['email']);
             unset($params['userTypeId']);
-            if ($collectPassword){
-                $newPassword=$params['password'];
+            if ($collectPassword) {
+                $newPassword = $params['password'];
                 unset($params['password']);
                 unset($params['confirmPassword']);
             }
-            $newUser['fields']=$params;
-            if ($userType['signUpType']=="open") {
-                $newUser['status']="approved";
-            } else if ($userType['signUpType']=="moderated") {
-                $newUser['status']="pending";
-            } else if ($userType['signUpType']=="emailConfirmation") {
-                $newUser['status']="emailUnconfirmed";
+            $newUser['fields'] = $params;
+            if ($userType['signUpType'] == "open") {
+                $newUser['status'] = "approved";
+            } else if ($userType['signUpType'] == "moderated") {
+                $newUser['status'] = "pending";
+            } else if ($userType['signUpType'] == "emailConfirmation") {
+                $newUser['status'] = "emailUnconfirmed";
                 $currentTimeService = Manager::getService('CurrentTime');
                 $currentTime = $currentTimeService->getCurrentTime();
-                $newUser['signupTime']=$currentTime;
+                $newUser['signupTime'] = $currentTime;
             }
-            
-            $createdUser=Manager::getService('Users')->create($newUser);
-            if (($createdUser['success'])&&($collectPassword)){
+
+            $createdUser = Manager::getService('Users')->create($newUser);
+            if (($createdUser['success']) && ($collectPassword)) {
                 Manager::getService('Users')->changePassword($newPassword, $createdUser['data']['version'], $createdUser['data']['id']);
             }
             if (!$createdUser['success']) {
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/fail.html.twig");
                 return $this->_sendResponse($output, $template);
-            } else if ($userType['signUpType']=="emailConfirmation"){
+            } else if ($userType['signUpType'] == "emailConfirmation") {
                 $emailVars = array();
                 $emailVars["name"] = $newUser["name"];
                 $emailVars["confirmUrl"] = $_SERVER['HTTP_REFERER']
@@ -113,12 +110,12 @@ class SignUpController extends AbstractController
                 $config = Manager::getService('config');
                 $options = $config['rubedo_config'];
                 $currentLang = Manager::getService('CurrentLocalization')->getCurrentLocalization();
-                $subject = '['.Manager::getService('Sites')->getHost($output['site']['id']).'] '
+                $subject = '[' . Manager::getService('Sites')->getHost($output['site']['id']) . '] '
                     . Manager::getService('Translate')->getTranslation(
-                            'Blocks.SignUp.confirmEmail.subject',
-                            $currentLang,
-                            'en'
-                        );
+                        'Blocks.SignUp.confirmEmail.subject',
+                        $currentLang,
+                        'en'
+                    );
 
                 $message = $mailService->getNewMessage()
                     ->setTo(array(
@@ -129,16 +126,16 @@ class SignUpController extends AbstractController
                     ->setBody($mailBody, 'text/html');
                 $result = $mailService->sendMessage($message);
 
-                if ($result === 1){
+                if ($result === 1) {
                     $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/confirmEmail.html.twig");
                     return $this->_sendResponse($output, $template);
                 } else {
-                    $output['signupMessage']="Unable to send confirmation email";
+                    $output['signupMessage'] = "Unable to send confirmation email";
                     $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                     return $this->_sendResponse($output, $template);
                 }
 
-                
+
             } else {
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/done.html.twig");
                 return $this->_sendResponse($output, $template);
@@ -146,53 +143,53 @@ class SignUpController extends AbstractController
         }
         $blockConfig = $this->params()->fromQuery('block-config', array());
         $output = $this->params()->fromQuery();
-        if (isset($output["confirmingEmail"])){
-            
+        if (isset($output["confirmingEmail"])) {
+
             $userId = $this->params()->fromQuery("userId");
             $signupTime = $this->params()->fromQuery("signupTime");
-            if ((!isset($userId))||(!isset($signupTime))){
-                $output['signupMessage']="Missing required parameters";
+            if ((!isset($userId)) || (!isset($signupTime))) {
+                $output['signupMessage'] = "Missing required parameters";
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                 return $this->_sendResponse($output, $template);
             }
-            $user=Manager::getService("Users")->findById($userId);
-            if (! $user){
-                $output['signupMessage']="Unknown user";
+            $user = Manager::getService("Users")->findById($userId);
+            if (!$user) {
+                $output['signupMessage'] = "Unknown user";
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                 return $this->_sendResponse($output, $template);
             }
-            if ($user['status']!="emailUnconfirmed"){
-                $output['signupMessage']="Account email already confimred";
+            if ($user['status'] != "emailUnconfirmed") {
+                $output['signupMessage'] = "Account email already confimred";
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                 return $this->_sendResponse($output, $template);
             }
-            if ($user['signupTime']!=$signupTime){
-                $output['signupMessage']="Invalid sign up time";
+            if ($user['signupTime'] != $signupTime) {
+                $output['signupMessage'] = "Invalid sign up time";
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                 return $this->_sendResponse($output, $template);
             }
-            $user["status"]="approved";
-            $update=Manager::getService("Users")->update($user);
-            if (!$update['success']){
-                $output['signupMessage']="Error during user update";
+            $user["status"] = "approved";
+            $update = Manager::getService("Users")->update($user);
+            if (!$update['success']) {
+                $output['signupMessage'] = "Error during user update";
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailconfirmerror.html.twig");
                 return $this->_sendResponse($output, $template);
             } else {
                 $template = Manager::getService('FrontOfficeTemplates')->getFileThemePath("blocks/signup/emailConfirmed.html.twig");
                 return $this->_sendResponse($output, $template);
             }
-            
+
         }
-        if (! isset($blockConfig['userType'])) {
+        if (!isset($blockConfig['userType'])) {
             return $this->_sendResponse(array(), "block.html.twig");
         }
-        $output['userTypeId']=$blockConfig['userType'];
-        $userType=Manager::getService('UserTypes')->findById($blockConfig['userType']);
-        if ($userType['signUpType']=="none") {
+        $output['userTypeId'] = $blockConfig['userType'];
+        $userType = Manager::getService('UserTypes')->findById($blockConfig['userType']);
+        if ($userType['signUpType'] == "none") {
             return $this->_sendResponse(array(), "block.html.twig");
         }
-        $output['fields']=$userType['fields'];
-        $output['collectPassword']=isset($blockConfig['collectPassword']) ? $blockConfig['collectPassword'] : false;
+        $output['fields'] = $userType['fields'];
+        $output['collectPassword'] = isset($blockConfig['collectPassword']) ? $blockConfig['collectPassword'] : false;
         if ((isset($blockConfig['introduction'])) && ($blockConfig['introduction'] != "")) {
             $content = Manager::getService('Contents')->findById($blockConfig["introduction"], true, false);
             $output['contentId'] = $blockConfig["introduction"];
@@ -204,5 +201,5 @@ class SignUpController extends AbstractController
         $js = array();
         return $this->_sendResponse($output, $template, $css, $js);
     }
-     
+
 }
