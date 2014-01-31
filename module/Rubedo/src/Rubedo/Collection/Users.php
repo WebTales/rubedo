d68a2e29425b63109777b0791653c8b408998123
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-01-31 13:30:02 +0100

    add extra check

--- module/Rubedo/src/Rubedo/Collection/Users.php
+++ module/Rubedo/src/Rubedo/Collection/Users.php
@@ -240,7 +240,7 @@ class Users extends AbstractCollection implements IUsers
             foreach ($groupList['data'] as $group) {
                 $obj['groups'][] = $group['id'];
             }
-            if (!in_array($obj['defaultGroup'], $obj['groups'])){
+            if ((!isset($obj['defaultGroup']))||(!in_array($obj['defaultGroup'], $obj['groups']))){
                 if (isset($obj['groups'][0])){
                     $obj['defaultGroup']=$obj['groups'][0];
                 } else {
