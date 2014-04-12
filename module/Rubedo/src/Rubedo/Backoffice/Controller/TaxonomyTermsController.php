b9fea758a95e340a7c7280ddfa3db0f90cc895d4
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-04-12 23:35:01 +0200

    fix taxonomy term query

--- module/Rubedo/src/Rubedo/Backoffice/Controller/TaxonomyTermsController.php
+++ module/Rubedo/src/Rubedo/Backoffice/Controller/TaxonomyTermsController.php
@@ -17,6 +17,8 @@
 namespace Rubedo\Backoffice\Controller;
 
 use Rubedo\Services\Manager;
+use WebTales\MongoFilters\Filter;
+use Zend\Json\Json;
 
 /**
  * Controller providing CRUD API for the taxonomyTerms JSON
@@ -24,7 +26,7 @@ use Rubedo\Services\Manager;
  * Receveive Ajax Calls for read & write from the UI to the Mongo DB
  *
  *
- * @author jbourdin
+ * @author jbourdin, aDobre
  * @category Rubedo
  * @package Rubedo
  *         
@@ -40,6 +42,61 @@ class TaxonomyTermsController extends DataAccessController
         $this->_dataService = Manager::getService('TaxonomyTerms');
     }
 
+    public function indexAction()
+    {
+        $receivedID=$this->params()->fromQuery('id', null);
+        $postFilter=false;
+        $response = array();
+        $response['total']=0;
+        $response['data']=array();
+        if (!empty($receivedID)){
+            $receivedIDArray=explode(", ",$receivedID);
+            $preFilter=Filter::factory('InUid')->setValue($receivedIDArray);
+            $postFilter=Filter::factory('NotInUid')->setValue($receivedIDArray);
+            $preValues = $this->_dataService->getList($preFilter, null, null, null);
+            $response['total'] =$preValues['count'];
+            $response['data'] = $preValues['data'];
+        }
+
+        $filterJson = $this->params()->fromQuery('filter');
+        if (isset($filterJson)) {
+            $filters = Json::decode($filterJson, Json::TYPE_ARRAY);
+        } else {
+            $filters = null;
+        }
+        $sortJson = $this->params()->fromQuery('sort');
+        if (isset($sortJson)) {
+            $sort = Json::decode($sortJson, Json::TYPE_ARRAY);
+        } else {
+            $sort = null;
+        }
+        $startJson = $this->params()->fromQuery('start');
+        if (isset($startJson)) {
+            $start = Json::decode($startJson, Json::TYPE_ARRAY);
+        } else {
+            $start = null;
+        }
+        $limitJson = $this->params()->fromQuery('limit');
+        if (isset($limitJson)) {
+            $limit = Json::decode($limitJson, Json::TYPE_ARRAY);
+        } else {
+            $limit = null;
+        }
+
+        $mongoFilters = $this->_buildFilter($filters);
+        if ($postFilter){
+            $mongoFilters->addFilter($postFilter);
+        }
+        $dataValues = $this->_dataService->getList($mongoFilters, $sort, $start, $limit);
+
+
+        $response['total'] =$response['total']+$dataValues['count'];
+        $response['data'] = array_merge($response['data'],$dataValues['data']);
+        $response['success'] = TRUE;
+        $response['message'] = 'OK';
+
+        return $this->_returnJson($response);
+    }
     /**
      * Clear orphan terms in the collection
      *
