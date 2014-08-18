1bf3ee0f60e9e2d5961a28b3f743ceafbb100cb0
Gaël Demette <gael.demette@webtales.fr>
2014-08-18 10:39:23 +0200

    Entities API Language test

--- module/RubedoAPI/test/RubedoAPITest/Entities/API/LanguageTest.php
+++ module/RubedoAPI/test/RubedoAPITest/Entities/API/LanguageTest.php
@@ -2,6 +2,37 @@
 
 namespace RubedoAPITest\Entities\API;
 
+use RubedoAPI\Entities\API\Language;
+
 class LanguageTest extends \PHPUnit_Framework_TestCase {
 
+    public function testConstructWithFallback()
+    {
+        $language = new Language('en|fr');
+        $this->assertEquals($language->getLocale(), 'en');
+        $this->assertEquals($language->getFallback(), 'fr');
+    }
+
+    public function testConstructWithoutFallback()
+    {
+        $language = new Language('en');
+        $this->assertEquals($language->getLocale(), 'en');
+
+    }
+
+    /**
+     * @expectedException        \RubedoAPI\Exceptions\APIEntityException
+     */
+    public function testConstructLowerInput()
+    {
+        new Language('f');
+    }
+
+    /**
+     * @expectedException        \RubedoAPI\Exceptions\APIEntityException
+     */
+    public function testConstructGreaterInput()
+    {
+        new Language('enfoofr');
+    }
 }
