705eb776fe6790e4b790faee42a39635e52a054f
Gaël Demette <gael.demette@webtales.fr>
2014-08-22 17:41:49 +0200

    LazyServiceManager test

--- module/RubedoAPI/test/RubedoAPITest/Traits/LazyServiceManagerTest.php
+++ module/RubedoAPI/test/RubedoAPITest/Traits/LazyServiceManagerTest.php
@@ -2,6 +2,47 @@
 
 namespace RubedoAPITest\Traits;
 
-class LazyServiceManagerTest extends \PHPUnit_Framework_TestCase {
+use Rubedo\Services\Manager;
+use RubedoAPI\Traits\LazyServiceManager;
 
+class LazyServiceManagerTest extends \PHPUnit_Framework_TestCase {
+    use LazyServiceManager;
+    public function tearDown()
+    {
+        Manager::resetMocks();
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+    /**
+     * @expectedException \Zend\ServiceManager\Exception\ServiceNotFoundException
+     */
+    public function testServiceNotFoundException()
+    {
+        $this->getFooService();
+    }
+
+    public function testGetService()
+    {
+        $mockService = $this->getMock('stdClass');
+        Manager::setMockService('Foo', $mockService);
+        $this->assertEquals($mockService, $this->getFooService());
+
+        Manager::setMockService('API\\Collection\\Foo', $mockService);
+        $this->assertEquals($mockService, $this->getFooAPICollection());
+
+        Manager::setMockService('API\\Services\\Foo', $mockService);
+        $this->assertEquals($mockService, $this->getFooAPIService());
+    }
+
+    public function testCache()
+    {
+        $mockService = $this->getMock('stdClass');
+        Manager::setMockService('Foo', $mockService);
+        $this->getFooService();
+        $this->assertArrayHasKey('getFooService', $this->callCache);
+
+    }
 } 
