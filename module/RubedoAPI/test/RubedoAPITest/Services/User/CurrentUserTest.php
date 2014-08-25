0d90c6a456b08539417a2d6fc2d21820fd66ff43
Gaël Demette <gael.demette@webtales.fr>
2014-08-25 15:48:05 +0200

    currentUser has traits

--- module/RubedoAPI/test/RubedoAPITest/Services/User/CurrentUserTest.php
+++ module/RubedoAPI/test/RubedoAPITest/Services/User/CurrentUserTest.php
@@ -1,7 +1,23 @@
 <?php
 
-namespace RubedoAPI\Services\User;
+namespace RubedoAPITest\Services\User;
+
+use RubedoAPI\Services\User\CurrentUser;
 
 class CurrentUserTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var CurrentUser
+     */
+    protected $currentUser;
 
+    function setUp()
+    {
+        $this->currentUser = new CurrentUser();
+        parent::setUp();
+    }
+
+    function testLazyLoader()
+    {
+        $this->assertArrayHasKey('RubedoAPI\Traits\LazyServiceManager', class_uses($this->currentUser));
+    }
 } 
