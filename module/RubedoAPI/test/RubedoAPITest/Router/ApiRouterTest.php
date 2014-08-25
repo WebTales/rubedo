0913d8e64c42b2d610cb6c04438a728a55c55df6
Gaël Demette <gael.demette@webtales.fr>
2014-08-25 10:59:39 +0200

    API Router test

--- module/RubedoAPI/test/RubedoAPITest/Router/ApiRouterTest.php
+++ module/RubedoAPI/test/RubedoAPITest/Router/ApiRouterTest.php
@@ -2,6 +2,30 @@
 
 namespace RubedoAPITest\Router;
 
-class ApiRouterTest extends \PHPUnit_Framework_TestCase {
+use RubedoAPI\Router\ApiRouter;
+use Zend\Http\PhpEnvironment\Request;
 
-} 
+class ApiRouterTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var \RubedoAPI\Router\ApiRouter
+     */
+    protected $router;
+
+    public function setUp() {
+        $this->router = new ApiRouter();
+    }
+
+    public function testMatch()
+    {
+        $request = new Request();
+        $uri = $request->getUri();
+        $uri->setPath('/api/v1/pages');
+        $request->setUri($uri);
+        $match = $this->router->match($request);
+        $params = $match->getParams();
+        $this->assertEquals('RubedoApi\Frontoffice\Controller\Api', $params['controller']);
+        $this->assertEquals('index', $params['action']);
+        $this->assertEquals('v1', $params['version']);
+        $this->assertArrayHasKey('api', $params);
+    }
+}
