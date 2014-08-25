d29df11010637aad1e498105996e823172aeb038
Gaël Demette <gael.demette@webtales.fr>
2014-08-25 15:31:42 +0200

    Token test

--- module/RubedoAPI/test/RubedoAPITest/Services/Security/TokenTest.php
+++ module/RubedoAPI/test/RubedoAPITest/Services/Security/TokenTest.php
@@ -1,8 +1,28 @@
 <?php
 
-namespace RubedoAPI\Services\Security;
+namespace RubedoAPITest\Services\Security;
 
+use RubedoAPI\Services\Security\Token;
+
+class ExtendedSecurityToken extends Token {
+    public function newToken($userId) {
+        return parent::newToken($userId);
+    }
+}
 
 class TokenTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var ExtendedSecurityToken
+     */
+    protected $tokenService;
 
-} 
+    function setUp()
+    {
+        $this->tokenService = new ExtendedSecurityToken();
+        parent::setUp();
+    }
+    public function testNewToken()
+    {
+        $this->assertNotEquals($this->tokenService->newToken('foo'), $this->tokenService->newToken('foo'));
+    }
+}
