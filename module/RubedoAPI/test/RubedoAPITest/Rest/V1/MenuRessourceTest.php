e920b6f6910b0ec6affb8478aa09d35e0b8cb832
Gaël Demette <gael.demette@webtales.fr>
2014-08-26 13:19:19 +0200

    base tests Menu & Pages

--- module/RubedoAPI/test/RubedoAPITest/Rest/V1/MenuRessourceTest.php
+++ module/RubedoAPI/test/RubedoAPITest/Rest/V1/MenuRessourceTest.php
@@ -2,6 +2,29 @@
 
 namespace RubedoAPITest\Rest\V1;
 
-class MenuRessourceTest extends \PHPUnit_Framework_TestCase {
+use Rubedo\Services\Manager;
+use RubedoAPI\Rest\V1\MenuRessource;
 
+class MenuRessourceTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var \RubedoAPI\Rest\V1\MenuRessource
+     */
+    protected $ressource;
+
+    function setUp()
+    {
+        $this->ressource = new MenuRessource();
+        parent::setUp();
+    }
+
+    function tearDown()
+    {
+        Manager::resetMocks();
+        parent::tearDown();
+    }
+
+    public function testDefinition()
+    {
+        $this->assertNotNull($this->ressource->getDefinition()->getVerb('get'));
+    }
 } 
--- module/RubedoAPI/test/RubedoAPITest/Rest/V1/PagesRessourceTest.php
+++ module/RubedoAPI/test/RubedoAPITest/Rest/V1/PagesRessourceTest.php
@@ -2,7 +2,29 @@
 
 namespace RubedoAPITest\Rest\V1;
 
+use Rubedo\Services\Manager;
+use RubedoAPI\Rest\V1\PagesRessource;
 
 class PagesRessourceTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var \RubedoAPI\Rest\V1\PagesRessource
+     */
+    protected $ressource;
 
-} 
+    function setUp()
+    {
+        $this->ressource = new PagesRessource();
+        parent::setUp();
+    }
+
+    function tearDown()
+    {
+        Manager::resetMocks();
+        parent::tearDown();
+    }
+
+    public function testDefinition()
+    {
+        $this->assertNotNull($this->ressource->getDefinition()->getVerb('get'));
+    }
+}
