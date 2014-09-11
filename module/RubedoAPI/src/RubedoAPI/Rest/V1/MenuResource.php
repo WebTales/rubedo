03547b7860b4149cdfaacc4b404021fcb87ac024
Gaël Demette <gael.demette@webtales.fr>
2014-09-11 13:29:48 +0200

    Copyright updated

--- module/RubedoAPI/src/RubedoAPI/Rest/V1/MenuResource.php
+++ module/RubedoAPI/src/RubedoAPI/Rest/V1/MenuResource.php
@@ -11,7 +11,7 @@
  *
  * @category   Rubedo
  * @package    Rubedo
- * @copyright  Copyright (c) 2012-2013 WebTales (http://www.webtales.fr)
+ * @copyright  Copyright (c) 2012-2014 WebTales (http://www.webtales.fr)
  * @license    http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
  */
 
