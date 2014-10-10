8dd41f69639bb7ecd238c1baedfaf7d2dd21a7a3
Didier Fanchon <didier.fanchon@webtales.fr>
2014-10-10 14:24:40 +0200

    Search agg

--- module/Rubedo/src/Rubedo/Elastic/DataSearch.php
+++ module/Rubedo/src/Rubedo/Elastic/DataSearch.php
@@ -985,11 +985,13 @@ class DataSearch extends DataAbstract implements IDataSearch
         	$elasticaResultSet = $search->search($elasticaQuery);
         	
         } else {
-        	//if ($geoPrecision < 7) {
-        	//	$elasticaResultSet = $search->count($elasticaQuery,true);
-        	//} else {
+        	$noResults = $search->count($elasticaQuery,false);
+        	
+        	if ($noResults > 1000) {
+        		$elasticaResultSet = $search->count($elasticaQuery,true);
+        	} else {
         		$elasticaResultSet = $search->search($elasticaQuery);
-        	//}
+        	}
         	
         	$result ['Aggregations'] = $elasticaResultSet->getAggregation("agf")['hash'];
         	
