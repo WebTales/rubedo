28091019bb45d5baff0052343148847936d37a76
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-10-21 15:28:33 +0200

    fix themes for extensions

--- module/Rubedo/src/Rubedo/Frontoffice/Controller/ThemeController.php
+++ module/Rubedo/src/Rubedo/Frontoffice/Controller/ThemeController.php
@@ -39,70 +39,15 @@ class ThemeController extends AbstractActionController
     {
         $theme = $this->params()->fromRoute('theme');
         $filePath = $this->params()->fromRoute('filepath');
-        
-        $consolidatedFilePath = Manager::getService('FrontOfficeTemplates')->getFilePath($theme, $filePath);
-        
-        if ($consolidatedFilePath) {
-            $extension = pathinfo($consolidatedFilePath, PATHINFO_EXTENSION);
-            switch ($extension) {
-                case 'php':
-                    throw new \Rubedo\Exceptions\NotFound('File does not exist');
-                    break;
-                case 'js':
-                    $mimeType = 'application/javascript';
-                    break;
-                case 'css':
-                    $mimeType = 'text/css';
-                    break;
-                case 'html':
-                    $mimeType = 'text/html';
-                    break;
-                case 'json':
-                    $mimeType = 'application/json';
-                    break;
-                default:
-                    if (class_exists('finfo')) {
-                        $finfo = new \finfo(FILEINFO_MIME);
-                        $mimeType = $finfo->file($consolidatedFilePath);
-                    }
-                    break;
-            }
-
-            $publicThemePath = APPLICATION_PATH . '/public/theme';
-            $composedPath = $publicThemePath . '/' . $theme;
-            if (!file_exists($composedPath)) {
-                mkdir($composedPath, 0777);
-            }
-
-            $composedPath = $composedPath . '/' . dirname($filePath);
-            if (!file_exists($composedPath)) {
-                mkdir($composedPath, 0777, true);
-            }
-            $targetPath = $publicThemePath . '/' . $theme . '/' . $filePath;
-
-            $content = file_get_contents($consolidatedFilePath);
-
-            $config = manager::getService('Application')->getConfig();
-
-            if (isset($config['rubedo_config']['minify']) && $config['rubedo_config']['minify'] == true) {
-                if ($mimeType == 'text/css') {
-                    $content = \Minify_CSS::minify($content, array(
-                        'preserveComments' => false
-                    ));
-                } elseif ($mimeType == 'application/javascript') {
-                    $content = \JSMin::minify($content);
-                }
-            }
-        }
 
         /** @var \Rubedo\Collection\Directories $directoriesCollection */
         $directoriesCollection = Manager::getService('Directories');
         $filters = Filter::factory('And');
         $filters
             ->addFilter(
-            Filter::factory('Value')
-                ->setName('parentId')
-                ->setValue('root')
+                Filter::factory('Value')
+                    ->setName('parentId')
+                    ->setValue('root')
             )
             ->addFilter(
                 Filter::factory('Value')
@@ -121,16 +66,16 @@ class ThemeController extends AbstractActionController
                 $damCollection = Manager::getService('Dam');
                 $media = $damCollection->findOne(
                     Filter::factory('And')
-                    ->addFilter(
-                        Filter::factory('Value')
-                        ->setValue($file)
-                        ->setName('title')
-                    )
-                    ->addFilter(
-                        Filter::factory('Value')
-                        ->setValue($dirWhereSearch)
-                        ->setName('directory')
-                    )
+                        ->addFilter(
+                            Filter::factory('Value')
+                                ->setValue($file)
+                                ->setName('title')
+                        )
+                        ->addFilter(
+                            Filter::factory('Value')
+                                ->setValue($dirWhereSearch)
+                                ->setName('directory')
+                        )
                 );
                 if (!empty($media)) {
                     $fileService = Manager::getService('Files');
@@ -141,6 +86,69 @@ class ThemeController extends AbstractActionController
                     }
                 }
             }
+        }
+
+        if (!$hasFileInDatabase){
+        
+            $consolidatedFilePath = Manager::getService('FrontOfficeTemplates')->getFilePath($theme, $filePath);
+
+            if (!$consolidatedFilePath){
+                $consolidatedFilePath = Manager::getService('FrontOfficeTemplates')->getFilePath("default", $filePath);
+            }
+
+            if ($consolidatedFilePath) {
+                $extension = pathinfo($consolidatedFilePath, PATHINFO_EXTENSION);
+                switch ($extension) {
+                    case 'php':
+                        throw new \Rubedo\Exceptions\NotFound('File does not exist');
+                        break;
+                    case 'js':
+                        $mimeType = 'application/javascript';
+                        break;
+                    case 'css':
+                        $mimeType = 'text/css';
+                        break;
+                    case 'html':
+                        $mimeType = 'text/html';
+                        break;
+                    case 'json':
+                        $mimeType = 'application/json';
+                        break;
+                    default:
+                        if (class_exists('finfo')) {
+                            $finfo = new \finfo(FILEINFO_MIME);
+                            $mimeType = $finfo->file($consolidatedFilePath);
+                        }
+                        break;
+                }
+
+                $publicThemePath = APPLICATION_PATH . '/public/theme';
+                $composedPath = $publicThemePath . '/' . $theme;
+                if (!file_exists($composedPath)) {
+                    mkdir($composedPath, 0777);
+                }
+
+                $composedPath = $composedPath . '/' . dirname($filePath);
+                if (!file_exists($composedPath)) {
+                    mkdir($composedPath, 0777, true);
+                }
+                $targetPath = $publicThemePath . '/' . $theme . '/' . $filePath;
+
+                $content = file_get_contents($consolidatedFilePath);
+
+                $config = manager::getService('Application')->getConfig();
+
+                if (isset($config['rubedo_config']['minify']) && $config['rubedo_config']['minify'] == true) {
+                    if ($mimeType == 'text/css') {
+                        $content = \Minify_CSS::minify($content, array(
+                            'preserveComments' => false
+                        ));
+                    } elseif ($mimeType == 'application/javascript') {
+                        $content = \JSMin::minify($content);
+                    }
+                }
+            }
+
         }
         $response = new \Zend\Http\Response\Stream();
         $headers = array(
