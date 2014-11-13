35891c2419905536567fd84422b51d48cd863c78
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-11-13 13:21:46 +0100

    fix dam upload

--- module/Rubedo/src/Rubedo/Backoffice/Controller/DamController.php
+++ module/Rubedo/src/Rubedo/Backoffice/Controller/DamController.php
@@ -153,11 +153,9 @@ class DamController extends DataAccessController
             $fieldConfig = $field['config'];
             $name = $fieldConfig['name'];
             
-            $uploadResult = $this->_uploadFile($name, $damType['mainFileType']);
+            $uploadResult = $this->_uploadFile($name, $fieldConfig['fileType']);
             if (! is_array($uploadResult)) {
                 $obj['fields'][$name] = $uploadResult;
-            } else {
-                return $this->_returnJson($uploadResult);
             }
             
             if (! $fieldConfig['allowBlank'] && ! $obj['fields'][$name]) {
@@ -270,7 +268,14 @@ class DamController extends DataAccessController
     protected function _uploadFile($name, $fileType, $returnFullResult = false, $setMimeType = false)
     {
         $fileInfos = $this->params()->fromFiles($name);
-        
+
+        if (empty($fileInfos['tmp_name'])){
+            return(array(
+                "success"=>false,
+                "message"=>"No file uploaded for this field : ".$name
+            ));
+        }
+
         $mimeType = mime_content_type($fileInfos['tmp_name']);
         
         if (($name == 'originalFileId')||($setMimeType)) {
