e8095aaad788795fe86fb10ec5e1fe0444cfa372
Didier <didier.fanchon@webtales.fr>
2014-11-14 16:21:37 +0100

    importMode is in config

--- module/Rubedo/src/Rubedo/Backoffice/Controller/ImportController.php
+++ module/Rubedo/src/Rubedo/Backoffice/Controller/ImportController.php
@@ -337,7 +337,7 @@ class ImportController extends DataAccessController
 		$options['userEncoding'] = $this->params()->fromPost('encoding');
 		$options['workingLanguage'] = $this->params()->fromPost('workingLanguage', 'en');
 		$options['importKey'] = (string) new \MongoId();
-		$options['importMode'] = $this->params()->fromPost('importMode', 'update');
+		
 		$options['typeId'] = isset($configs['contentTypeId']) ? $configs['contentTypeId'] : null;
 		
 		if (! isset($options['userEncoding'])) {
@@ -361,8 +361,9 @@ class ImportController extends DataAccessController
 			} else {
 				// receive params
 				$configs = Json::decode($this->params()->fromPost('configs', "[ ]"), Json::TYPE_ARRAY);
-							
+				
 				// Get general params
+				$options['importMode'] = isset($configs['importMode']) ? $configs['importMode'] : 'update';
 				$options['isProduct'] = isset($configs['isProduct']) ? $configs['isProduct'] : false;
 				$options['vocabularies'] = array();
 
