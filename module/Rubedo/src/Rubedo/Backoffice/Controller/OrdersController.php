64cdde25d997a4c108a24eb38251b162680c8782
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-12-12 17:18:01 +0100

    orders export

--- module/Rubedo/src/Rubedo/Backoffice/Controller/OrdersController.php
+++ module/Rubedo/src/Rubedo/Backoffice/Controller/OrdersController.php
@@ -17,6 +17,8 @@
 namespace Rubedo\Backoffice\Controller;
 
 use Rubedo\Services\Manager;
+use WebTales\MongoFilters\Filter;
+
 
 /**
  * Controller providing CRUD API for the Orders JSON
@@ -37,6 +39,91 @@ class OrdersController extends DataAccessController
         parent::__construct();
         // init the data access service
         $this->_dataService = Manager::getService('Orders');
+    }
+
+    public function exportAction()
+    {
+        $params = $this->params()->fromQuery();
+        $filters = Filter::factory();
+        if (!empty($params['startDate'])) {
+            $filters->addFilter(
+                Filter::factory('OperatorTovalue')->setName('createTime')
+                    ->setOperator('$gte')
+                    ->setValue((int)$params['startDate'])
+            );
+        }
+        if (!empty($params['endDate'])) {
+            $filters->addFilter(
+                Filter::factory('OperatorTovalue')->setName('createTime')
+                    ->setOperator('$lte')
+                    ->setValue((int)$params['endDate'])
+            );
+        }
+        $orders = $this->_dataService->getList($filters);
+        $fileName = 'export_rubedo_orders_' . time() . '.csv';
+        $filePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
+        $csvResource = fopen($filePath, 'w+');
+
+        $csvHeader = array(
+            'OrderNumber', 'PaymentMean', 'Status', 'PriceInclTax', 'PriceExclTax',
+            'BillingAddress1', 'BillingAddress2', 'BillingCity', 'BillingPostalCode', 'BillingCountry', 'BillingRegionState',
+            'ShippingAddress1', 'ShippingAddress', 'ShippingCity', 'ShippingPostalCode', 'ShippingCountry', 'ShippingRegionState',
+            'ProductName', 'ProductSubtitle', 'ProductAmount', 'UnitPriceInclTax', 'UnitPriceExclTax', 'ProductPriceInclTax', 'ProductPriceExclTax'
+        );
+
+        /** @var \Rubedo\Internationalization\Translate $translationService */
+        $translationService = Manager::getService('Translate');
+        $workingLanguage = $this->params()->fromQuery('workingLanguage', 'en');
+        foreach ($csvHeader as &$head) {
+            $head = 'Backoffice.Exports.Orders.' . $head;
+            $head = $translationService->getTranslation($head, $workingLanguage);
+        }
+
+        fputcsv($csvResource, $csvHeader, ';');
+        foreach($orders['data'] as $order) {
+            $billingA = &$order['billingAddress'];
+            $shippingA = &$order['shippingAddress'];
+            $product = array_shift($order['detailedCart']['cart']);
+            $firstLine = array(
+                $order['orderNumber'], $order['paymentMeans'], $order['status'], $order['finalPrice'], $order['finalTFPrice'],
+                //Billing address
+                isset($billingA['address1'])?$billingA['address1']:'',
+                isset($billingA['address2'])?$billingA['address2']:'',
+                isset($billingA['city'])?$billingA['city']:'',
+                isset($billingA['postCode'])?$billingA['postCode']:'',
+                isset($billingA['country'])?$billingA['country']:'',
+                isset($billingA['regionState'])?$billingA['regionState']:'',
+                //Shipping address
+                isset($shippingA['address1'])?$shippingA['address1']:'',
+                isset($shippingA['address2'])?$shippingA['address2']:'',
+                isset($shippingA['city'])?$shippingA['city']:'',
+                isset($shippingA['postCode'])?$shippingA['postCode']:'',
+                isset($shippingA['country'])?$shippingA['country']:'',
+                isset($shippingA['regionState'])?$shippingA['regionState']:'',
+                $product['title'], $product['subtitle'], $product['amount'], $product['unitTaxedPrice'], $product['unitPrice'], $product['taxedPrice'], $product['price'],
+            );
+            fputcsv($csvResource, $firstLine, ';');
+            foreach ($order['detailedCart']['cart'] as $product) {
+                $line = array(
+                    $order['orderNumber'], '', '', '', '',
+                    '', '', '', '', '', '',
+                    '', '', '', '', '', '',
+                    $product['title'], $product['subtitle'], $product['amount'], $product['unitTaxedPrice'], $product['unitPrice'], $product['taxedPrice'], $product['price'],
+                );
+                fputcsv($csvResource, $line, ';');
+            }
+        }
+
+        $content = file_get_contents($filePath);
+        $response = $this->getResponse();
+        $headers = $response->getHeaders();
+        $headers->addHeaderLine('Content-Type', 'text/csv');
+        $headers->addHeaderLine('Content-Disposition', "attachment; filename=\"$fileName\"");
+        $headers->addHeaderLine('Accept-Ranges', 'bytes');
+        $headers->addHeaderLine('Content-Length', strlen($content));
+
+        $response->setContent($content);
+        return $response;
     }
 
 }
