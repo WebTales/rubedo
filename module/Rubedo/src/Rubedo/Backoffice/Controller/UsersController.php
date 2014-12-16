2080f4b832067427e0a5180dbea9ebd798e53748
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-12-16 17:16:09 +0100

    WIP users export

--- module/Rubedo/src/Rubedo/Backoffice/Controller/UsersController.php
+++ module/Rubedo/src/Rubedo/Backoffice/Controller/UsersController.php
@@ -17,6 +17,8 @@
 namespace Rubedo\Backoffice\Controller;
 
 use Rubedo\Services\Manager;
+use WebTales\MongoFilters\Filter;
+use Zend\Debug\Debug;
 use Zend\Json\Json;
 use Zend\View\Model\JsonModel;
 
@@ -101,5 +103,137 @@ class UsersController extends DataAccessController
         }
         return $this->_returnJson($returnArray);
         
+    }
+
+    public function exportAction(){
+        $params = $this->params()->fromQuery();
+        $filters = Filter::factory();
+        if (!empty($params['startDate'])) {
+            $filters->addFilter(
+                Filter::factory('OperatorTovalue')->setName('createTime')
+                    ->setOperator('$gte')
+                    ->setValue((int)$params['startDate'])
+            );
+        }
+        if (!empty($params['endDate'])) {
+            $filters->addFilter(
+                Filter::factory('OperatorTovalue')->setName('createTime')
+                    ->setOperator('$lte')
+                    ->setValue((int)$params['endDate'])
+            );
+        }
+        $userType=Manager::getService("UserTypes")->findById($params['typeId']);
+        $filters->addFilter(
+            Filter::factory('Value')->setName('typeId')
+                ->setValue($params['typeId'])
+        );
+        $users=$this->_dataService->getList($filters);
+        $fileName = 'export_rubedo_users_'.$userType['type'].'_' . time() . '.csv';
+        $filePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $fileName;
+        $csvResource = fopen($filePath, 'w+');
+        $fieldsArray = array(
+            "email",
+            "name"
+        );
+        $headerArray = array(
+            "email"=>"Email",
+            "name"=>"Name"
+        );
+        $fieldsArray[]="createTime";
+        $headerArray["createTime"]="Creation";
+        $exportableFieldTypes=[
+            "Ext.form.field.Text",
+            "Ext.form.field.TextArea",
+            "Ext.form.field.Number",
+            "Ext.form.field.ComboBox",
+            "Ext.form.field.Checkbox",
+            "Ext.form.RadioGroup",
+            "Ext.form.field.Date",
+            "Ext.form.field.Time",
+            "Ext.slider.Single",
+            "Rubedo.view.CKEField"
+        ];
+        foreach ($userType['fields'] as $typeField){
+            if (in_array($typeField['cType'],$exportableFieldTypes)){
+                $fieldsArray[]=$typeField['config']['name'];
+                $headerArray[$typeField['config']['name']]=$typeField['config']['fieldLabel'];
+            }
+        }
+        $taxoService=Manager::getService("Taxonomy");
+        $taxoTermsService=Manager::getService("TaxonomyTerms");
+        $taxoHeaderArray=array();
+        $taxoFieldsArray=array();
+        foreach($userType['vocabularies'] as $vocabId){
+            if (!empty($vocabId)&&$vocabId!="navigation"){
+                $vocabulary=$taxoService->findById($vocabId);
+                if ($vocabulary){
+                    $taxoHeaderArray[$vocabId]=$vocabulary['name'];
+                    $taxoFieldsArray[]=$vocabId;
+                }
+            }
+        }
+        $csvLine = array();
+        foreach ($fieldsArray as $field) {
+            $csvLine[] = $headerArray[$field];
+        }
+        foreach ($taxoFieldsArray as $field) {
+            $csvLine[] = $taxoHeaderArray[$field];
+        }
+        fputcsv($csvResource, $csvLine, ';');
+
+        foreach ($users['data'] as $user) {
+            $csvLine = array();
+            foreach ($fieldsArray as $field) {
+                switch ($field) {
+                    case 'createTime':
+                        $csvLine[] = date('d-m-Y H:i:s',$user["createTime"]);
+                        break;
+                    case 'email':
+                    case 'name':
+                        $csvLine[] = isset($user[$field]) ? $user[$field] : '';
+                        break;
+                    default:
+                        if (!isset($user['fields'][$field])){
+                            $csvLine[]='';
+                        } elseif (is_array($user['fields'][$field])){
+                            $csvLine[]=implode(", ",$user['fields'][$field]);
+                        } else {
+                            $csvLine[]=$user['fields'][$field];
+                        }
+                        break;
+                }
+            }
+            foreach ($taxoFieldsArray as $taxoField) {
+                if (!isset($user['taxonomy'][$taxoField])){
+                    $csvLine[]='';
+                } elseif (is_array($user['taxonomy'][$taxoField])) {
+                    $termLabelsArray=array();
+                    foreach($user['taxonomy'][$taxoField] as $taxoTermId){
+                        $foundTerm=$taxoTermsService->findById($taxoTermId);
+                        if ($foundTerm){
+                            $termLabelsArray[]=$foundTerm['text'];
+                        }
+                    }
+                    $csvLine[]=implode(", ",$termLabelsArray);
+                } else {
+                    $foundTerm=$taxoTermsService->findById($user['taxonomy'][$taxoField]);
+                    if ($foundTerm){
+                        $csvLine[]=$foundTerm['text'];
+                    } else {
+                        $csvLine[]='';
+                    }
+                }
+            }
+            fputcsv($csvResource, $csvLine, ';');
+        }
+        $content = file_get_contents($filePath);
+        $response = $this->getResponse();
+        $headers = $response->getHeaders();
+        $headers->addHeaderLine('Content-Type', 'text/csv');
+        $headers->addHeaderLine('Content-Disposition', "attachment; filename=\"$fileName\"");
+        $headers->addHeaderLine('Accept-Ranges', 'bytes');
+        $headers->addHeaderLine('Content-Length', strlen($content));
+        $response->setContent($content);
+        return $response;
     }
 }
