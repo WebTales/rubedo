f2282d9d7611c44ffc73bddf5ecc8c6016db9273
Didier Fanchon <didier.fanchon@webtales.fr>
2015-02-03 15:02:00 +0100

    Build index fix

--- module/Rubedo/src/Rubedo/Collection/AbstractCollection.php
+++ module/Rubedo/src/Rubedo/Collection/AbstractCollection.php
@@ -733,6 +733,7 @@ abstract class AbstractCollection implements IAbstractCollection
     {
         $result = true;
         foreach ($this->_indexes as $index) {
+        	$this->_dataService->deleteIndex($index['keys']);
             $result = $result && $this->_dataService->ensureIndex($index['keys'], isset($index['options']) ? $index['options'] : array());
         }
         return $result;
