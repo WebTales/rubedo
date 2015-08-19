ed4a2b04856d76e526473f3fd1072054c388d853
goncalvesm <goncalves.mickael.60@gmail.com>
2015-08-19 11:54:31 +0200

    Add new localization file for mailing list subscription block

--- module/Rubedo/config/localization.config.php
+++ module/Rubedo/config/localization.config.php
@@ -50,6 +50,7 @@ return array(
     'public/components/webtales/rubedo-localization/languagekey/FrontOffice/Blocks/ShoppingCart/shoppingcart.json',
     'public/components/webtales/rubedo-localization/languagekey/FrontOffice/Blocks/UserOrders/userOrders.json',
     'public/components/webtales/rubedo-localization/languagekey/FrontOffice/Blocks/ContentContribution/contrib.json',
+    'public/components/webtales/rubedo-localization/languagekey/FrontOffice/Blocks/Subscribe/subscribe.json',
     'public/components/webtales/rubedo-localization/languagekey/FrontOffice/Edition/edition.json',
     'public/components/webtales/rubedo-localization/languagekey/FrontOffice/Errors/errors.json',
     'public/components/webtales/rubedo-localization/languagekey/FrontOffice/Blocks/Navigation/nav.json',
