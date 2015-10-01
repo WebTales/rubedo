6f6b15e38547e13c0fbec2d6e86ad1d49ece78bf
Didier Fanchon <didier.fanchon@webtales.fr>
2015-10-01 08:41:06 +0200

    update source directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ ! -d /var/sources/${VERSION} ]; then
-    mkdir -p /var/sources/${VERSION}
+if [ ! -d /var/sources ]; then
+    mkdir -p /var/sources
 fi
 if [ "${VERSION}" != "**None**" ] && [ "${GITHUB_APIKEY}" != "**None**" ]; then
-    git clone -b "$VERSION" https://github.com/WebTales/rubedo.git /var/sources/${VERSION}
-    curl -sS https://getcomposer.org/installer | php -- --install-dir=/var/sources/${VERSION}
-    cd /var/sources/${VERSION}
+    git clone -b "$VERSION" https://github.com/WebTales/rubedo.git /var/sources
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/var/sources
+    cd /var/sources
     php composer.phar config -g github-oauth.github.com "$GITHUB_APIKEY"
     ./rubedo.sh
 fi
