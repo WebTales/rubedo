7cd8f0b0f0189f86eab9b211ce34666392399a7d
Didier Fanchon <didier.fanchon@webtales.fr>
2015-10-01 14:40:04 +0200

    Clean sources

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ if [ ! -d /var/sources ]; then
     mkdir -p /var/sources
 fi
 if [ "${VERSION}" != "**None**" ] && [ "${GITHUB_APIKEY}" != "**None**" ]; then
+    rm -rf /var/sources
     git clone -b "$VERSION" https://github.com/WebTales/rubedo.git /var/sources
     curl -sS https://getcomposer.org/installer | php -- --install-dir=/var/sources
     cd /var/sources
