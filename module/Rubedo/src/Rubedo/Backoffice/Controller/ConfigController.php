91ee38d8d278aa6534bb443016a4d60a83aaa3a4
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-11-03 15:31:08 +0100

    cluster system settings

--- module/Rubedo/src/Rubedo/Backoffice/Controller/ConfigController.php
+++ module/Rubedo/src/Rubedo/Backoffice/Controller/ConfigController.php
@@ -16,6 +16,7 @@
  */
 namespace Rubedo\Backoffice\Controller;
 
+use Rubedo\Services\Manager;
 use Zend\Json\Json;
 use Zend\Mvc\Controller\AbstractActionController;
 
@@ -52,31 +53,59 @@ class ConfigController extends AbstractActionController
 
     public function updateAction()
     {
-        $data = $this->params()->fromPost('data');
-        if (empty($data)){
-            $this->getResponse()->setStatusCode(500);
-            return new JsonModel(array("success"=>false));
+        $isMultiNode=false;
+        $config=Manager::getService("config");
+        if (isset($config["webCluster"])&&is_array($config["webCluster"])){
+            $isMultiNode=true;
         }
-        $updateData = Json::decode($data, Json::TYPE_ARRAY);
-        if (!isset($this->config["rubedo_config"])){
-            $this->config["rubedo_config"]=[];
+        $isReplicated=$this->params()->fromPost("isReplicated",null);
+        if ($isMultiNode&&!$isReplicated){
+            $mainRequest=$this->getRequest();
+            $path=$mainRequest->getUri()->getPath();
+            $post=$mainRequest->getPost()->toArray();
+            $post["isReplicated"]=true;
+            $cookie=$mainRequest->getCookie()->__toString();
+            $lastResult=[];
+            $allResults=[];
+            $protocol=isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS'] ? "https://" : "http://";
+            foreach($config["webCluster"] as $clusterHost){
+                $curlUrl = $protocol . $clusterHost . $path;
+                $curly = curl_init();
+                curl_setopt($curly,CURLOPT_URL, $curlUrl);
+                curl_setopt($curly,CURLOPT_POST, true);
+                curl_setopt($curly,CURLOPT_POSTFIELDS, $post);
+                curl_setopt($curly,CURLOPT_COOKIE, $cookie);
+                curl_setopt($curly, CURLOPT_RETURNTRANSFER, true);
+                $result=Json::decode(curl_exec($curly),Json::TYPE_ARRAY);
+                $lastResult=$result;
+                $allResults[]=$result;
+                curl_close($curly);
+            }
+            $lastResult["clusterResults"]=$allResults;
+            return new JsonModel($lastResult);
+        } else {
+            $data = $this->params()->fromPost('data');
+            if (empty($data)) {
+                $this->getResponse()->setStatusCode(500);
+                return new JsonModel(array("success" => false));
+            }
+            $updateData = Json::decode($data, Json::TYPE_ARRAY);
+            if (!isset($this->config["rubedo_config"])) {
+                $this->config["rubedo_config"] = [];
+            }
+            if (!isset($this->config["swiftmail"]["smtp"])) {
+                $this->config["swiftmail"] = [
+                    "smtp" => []
+                ];
+            }
+            if (isset($updateData["rubedo_config"]) && is_array($updateData["rubedo_config"])) {
+                $this->config["rubedo_config"] = array_merge($this->config["rubedo_config"], $updateData["rubedo_config"]);
+            }
+            if (isset($updateData["swiftmail"]["smtp"]) && is_array($updateData["swiftmail"]["smtp"])) {
+                $this->config["swiftmail"]["smtp"] = array_merge($this->config["swiftmail"]["smtp"], $updateData["swiftmail"]["smtp"]);
+            }
+            $this->installObject->saveLocalConfig($this->config);
+            return new JsonModel(array("success" => true));
         }
-        if (!isset($this->config["swiftmail"]["smtp"])){
-            $this->config["swiftmail"]=[
-                "smtp"=>[]
-            ];
-        }
-        if (isset($updateData["rubedo_config"])&&is_array($updateData["rubedo_config"])){
-            $this->config["rubedo_config"] = array_merge($this->config["rubedo_config"], $updateData["rubedo_config"]);
-        }
-        if (isset($updateData["swiftmail"]["smtp"])&&is_array($updateData["swiftmail"]["smtp"])){
-            $this->config["swiftmail"]["smtp"] = array_merge($this->config["swiftmail"]["smtp"], $updateData["swiftmail"]["smtp"]);
-        }
-        $this->installObject->saveLocalConfig($this->config);
-        return new JsonModel(array("success"=>true));
     }
-
-
-
-
 }
