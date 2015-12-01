e5417a1e7d86683296673e342f5c058a167dfc88
Robin Lebert <robin.lebert@gmail.com>
2015-12-01 14:05:52 +0100

    You no longer have to be in the rubedo folder to launch rubedo.sh

--- rubedo.sh
+++ rubedo.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $DIR
 
 #Try if CMDS exist
 command -v php > /dev/null || { echo "php command not found."; exit 1; }
