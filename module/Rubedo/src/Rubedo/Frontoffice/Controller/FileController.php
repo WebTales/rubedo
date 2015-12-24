7483aeefcfd91d91f5bbc8afd11d965a64071b9a
Didier Fanchon <didier.fanchon@webtales.fr>
2015-12-24 09:21:32 +0100

    Fix #1245

--- module/Rubedo/src/Rubedo/Frontoffice/Controller/FileController.php
+++ module/Rubedo/src/Rubedo/Frontoffice/Controller/FileController.php
@@ -18,6 +18,7 @@ namespace Rubedo\Frontoffice\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Rubedo\Services\Manager;
+use WebTales\MongoFilters\Filter;
 
 /**
  * Controller providing access to images in gridFS
@@ -34,124 +35,185 @@ use Rubedo\Services\Manager;
 class FileController extends AbstractActionController
 {
 
+	private $fileId;
+	private $start  = -1;
+	private $end    = -1;
+	private $size   = 0;
+	private $chunkSize;
+	private $contentType;
+	
     function indexAction()
     {
-        $fileId = $this->params()->fromQuery('file-id');
-        //$version = $this->params()->fromQuery('version', 1);
-
-        if (isset($fileId)) {
+       	$this->fileId = $this->params()->fromQuery('file-id');
+       	
+        if (isset($this->fileId)) {
 
             $fileService = Manager::getService('Files');
-            $obj = $fileService->findById($fileId);
+            $obj = $fileService->findById($this->fileId);
             if (!$obj instanceof \MongoGridFSFile) {
                 throw new \Rubedo\Exceptions\NotFound("No Image Found", "Exception8");
             }
 
-            $filelength = $obj->getSize();
-            $lastByte = (string)$filelength - 1;
-
+	        $this->size = $obj->getSize();
+	        $this->chunkSize = $obj->file["chunkSize"];
             $meta = $obj->file;
             $filename = $meta['filename'];
-            $type = $meta['Content-Type'];
-            $doNotDownload = false;
+            $this->contentType = $meta['Content-Type'];
+            $action = "inline";
 
-            list ($subtype) = explode('/', $type);
+            list ($subtype) = explode('/', $this->contentType);
 
-            switch ($type) {
+            switch ($this->contentType) {
                 case 'application/pdf':
-                    $doNotDownload = true;
+                    $action = "inline";
                     break;
                 default:
-                    $doNotDownload = false;
+                    $action = "download";
                     break;
             }
+            
+            switch ($this->params()->fromQuery('attachment', null)) {
+            	case 'download':
+            		$action = "download";
+            		break;
+            	case 'inline':
+            		$action = "inline";
+            		break;
+            	default:
+            		break;
+            }
+            
             if ($subtype == 'text') {
-                $doNotDownload = true;
+                $action = "inline";
             }
 
             if ($subtype == 'image') {
-                $doNotDownload = true;
+                $action = "inline";
             }
 
-            switch ($this->params()->fromQuery('attachment', null)) {
-                case 'download':
-                    $doNotDownload = false;
-                    break;
-                case 'inline':
-                    $doNotDownload = false;
-                    break;
-                default:
-                    break;
+            if ($subtype == 'video') {
+            	$action = "stream";
+            }
+			
+            switch ($action) {
+            	case "download":
+            		$response = new \Zend\Http\Response\Stream();
+            		$response->getHeaders()->addHeaders(array(
+            				'Content-Disposition' => 'attachment; filename="' . $filename
+            		));
+            		$response->getHeaders()->addHeaders(array(
+            				'Content-Type' => $meta['Content-Type']
+            		));
+            		$stream = $obj->getResource();
+            		$response->setStream($stream);
+            		return $response;
+            		break;
+            	case "inline":
+            		$response = new \Zend\Http\Response\Stream();
+            		$response->getHeaders()->addHeaders(array(
+            				'Content-Disposition' => 'inline; filename="' . $filename
+            		));
+            		$response->getHeaders()->addHeaders(array(
+            				'Content-Type' => $meta['Content-Type']
+            		));
+            		$stream = $obj->getResource();
+            		$response->setStream($stream);
+            		return $response;
+            		break;
+            	case "stream":
+            		$this->setHeader();
+            		$this->stream();
+            		exit;
             }
 
-            $seekStart = 0;
-            $seekEnd = -1;
-            if (isset($_SERVER['HTTP_RANGE']) || isset($HTTP_SERVER_VARS['HTTP_RANGE'])) {
-
-                $seekRange = isset($HTTP_SERVER_VARS['HTTP_RANGE']) ? substr($HTTP_SERVER_VARS['HTTP_RANGE'], strlen('bytes=')) : substr($_SERVER['HTTP_RANGE'], strlen('bytes='));
-                $range = explode('-', $seekRange);
-
-                if ($range[0] > 0) {
-                    $seekStart = intval($range[0]);
-                }
-
-                $seekEnd = ($range[1] > 0) ? intval($range[1]) : -1;
-            }
-
-            $response = new \Zend\Http\Response\Stream();
-
-            if (!$doNotDownload) {
-                $response->getHeaders()->addHeaders(array(
-                    'Content-Disposition' => 'attachment; filename="' . $filename
-                ));
-            } else {
-                $response->getHeaders()->addHeaders(array(
-                    'Content-Disposition' => 'inline; filename="' . $filename
-                ));
-            }
-
-            $response->getHeaders()->addHeaders(array(
-                'Content-Type' => $meta['Content-Type']
-            ));
-
-            $stream = $obj->getResource();
-
-            if ($seekStart >= 0 && $seekEnd > 0 && !($filelength == $seekEnd - $seekStart)) {
-                $response->getHeaders()->addHeaders(array(
-                    'Content-Length' => $seekEnd + 1 - $seekStart,
-                    'Content-Range' => "bytes $seekStart-$seekEnd/$filelength",
-                    'Accept-Ranges' => "bytes",
-                    'Status' => '206 Partial Content'
-                ));
-                $response->setStatusCode(206);
-                $response->setReasonPhrase('Partial Content');
-                $response->setContentLength($seekEnd + 1 - $seekStart);
-            } elseif ($seekStart > 0 && $seekEnd == -1) {
-                $response->getHeaders()->addHeaders(array(
-                    'Content-Length' => $filelength - $seekStart,
-                    'Content-Range' => "bytes $seekStart-$lastByte/$filelength",
-                    'Accept-Ranges' => "bytes",
-                    'Status' => '206 Partial Content'
-                ));
-                $response->setStatusCode(206);
-                $response->setReasonPhrase('Partial Content');
-            } else {
-                $response->getHeaders()->addHeaders(array(
-                    'Content-Length' => $filelength,
-                    'Content-Range' => "bytes 0-/$filelength",
-                    'Accept-Ranges' => "bytes"
-                ));
-            }
-            if ($seekStart) {
-                fseek($stream, $seekStart);
-            }
-            $response->setStream($stream);
-            return $response;
         } else {
             throw new \Rubedo\Exceptions\User("No Id Given", "Exception7");
         }
     }
-
+    /**
+     * Set proper header to stream files
+     */
+    private function setHeader()
+    {
+    	ob_get_clean();
+    	header("Content-Type: ".$this->contentType);
+    	header("Cache-Control: max-age=2592000, public");
+    	header("Expires: ".gmdate('D, d M Y H:i:s', time()+2592000) . ' GMT');
+    	$this->start = 0;
+    	$this->end   = $this->size - 1;
+    	header("Accept-Ranges: 0-".$this->end);
+    	if (isset($_SERVER['HTTP_RANGE'])) {
+    
+    		$c_start = $this->start;
+    		$c_end = $this->end;
+    
+    		list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+    		if (strpos($range, ',') !== false) {
+    			header('HTTP/1.1 416 Requested Range Not Satisfiable');
+    			header("Content-Range: bytes $this->start-$this->end/$this->size");
+    			exit;
+    		}
+    		if ($range == '-') {
+    			$c_start = $this->size - substr($range, 1);
+    		}else{
+    			$range = explode('-', $range);
+    			$c_start = $range[0];
+    			 
+    			$c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $c_end;
+    		}
+    		$c_end = ($c_end > $this->end) ? $this->end : $c_end;
+    		if ($c_start > $c_end || $c_start > $this->size - 1 || $c_end >= $this->size) {
+    			header('HTTP/1.1 416 Requested Range Not Satisfiable');
+    			header("Content-Range: bytes $this->start-$this->end/$this->size");
+    			exit;
+    		}
+    		$this->start = $c_start;
+    		$this->end = $c_end;
+    		$length = $this->end - $this->start + 1;
+    		header('HTTP/1.1 206 Partial Content');
+    		header("Content-Length: ".$length);
+    		header("Content-Range: bytes $this->start-$this->end/".$this->size);
+    	}
+    	else
+    	{
+    		header("Content-Length: ".$this->size);
+    	}
+    	 
+    }
+    
+    /**
+     * perform the streaming of calculated range
+     */
+    private function stream() {
+    	set_time_limit(0);
+    	$firstChunk = intval($this->start/$this->chunkSize);
+    	$lastChunk = intval($this->end/$this->chunkSize);
+    	$filter = [
+	    	'files_id' => new \MongoId($this->fileId),
+	    	'n' => [
+	    		'$gte' => $firstChunk,
+	    		'$lte' => $lastChunk
+	    	]
+    	];
+    	$mongoFilter=Filter::factory();
+    	$mongoFilter->addFilter(Filter::factory("Value")->setName("files_id")->setValue(new \MongoId($this->fileId)));
+    	$mongoFilter->addFilter(Filter::factory("OperatorToValue")->setName("n")->setOperator('$gte')->setValue($firstChunk));
+    	$mongoFilter->addFilter(Filter::factory("OperatorToValue")->setName("n")->setOperator('$lte')->setValue($lastChunk));
+    	$dataAccess = Manager::getService('MongoDataAccess');
+    	$dataAccess->init('fs.chunks');
+    	$cursor = $dataAccess->customFind($mongoFilter)->sort(['n' => 1]);
+    	$i = $this->start;
+    	foreach($cursor as $chunk) {
+    		$borneInf = $chunk['n'] * $this->chunkSize;
+    		$borneSup = $borneInf + $this->chunkSize - 1;
+    		$startRead = ($this->start <= $borneInf) ? 0 : $this->start - $borneInf;
+    		$bytesToRead = ($this->end <= $borneSup) ? $this->end - $borneInf + $startRead + 1 : $this->chunkSize - $startRead;
+    		echo substr($chunk['data']->bin, $startRead, $bytesToRead);
+    		flush();
+    		$i += $bytesToRead;
+    	}
+    }    
+    
     public function getThumbnailAction()
     {
         $iconPath = realpath(APPLICATION_PATH . '/public/components/webtales/rubedo-backoffice-ui/www/resources/icones/' . Manager::getService('Session')->get('iconSet', 'red') . '/128x128/attach_document.png');
