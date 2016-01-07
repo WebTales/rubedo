eb8d211c22b36834b98a91353d99812f798f6b98
Didier Fanchon <didier.fanchon@webtales.fr>
2016-01-07 13:05:19 +0100

    test controller

---
+++ module/Rubedo/src/Rubedo/Backoffice/Controller/MLController.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Rubedo -- ECM solution
+ * Copyright (c) 2014, WebTales (http://www.webtales.fr/).
+ * All rights reserved.
+ * licensing@webtales.fr
+ *
+ * Open Source License
+ * ------------------------------------------------------------------------------------------
+ * Rubedo is licensed under the terms of the Open Source GPL 3.0 license.
+ *
+ * @category   Rubedo
+ * @package    Rubedo
+ * @copyright  Copyright (c) 2012-2014 WebTales (http://www.webtales.fr)
+ * @license    http://www.gnu.org/licenses/gpl.html Open Source GPL 3.0 license
+ */
+namespace Rubedo\Backoffice\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Rubedo\Services\Manager;
+use WebTales\MongoFilters\Filter;
+use Zend\Debug\Debug;
+use BigML\BigML;
+
+
+/**
+ * Controller providing machine learning actions for rubedo
+ *
+ * @author dfanchon
+ * @category Rubedo
+ * @package Rubedo
+ *
+ */
+class MLController extends AbstractActionController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    /**
+     * The default index Action
+     *
+     */
+    public function indexAction()
+    {
+        $returnArray = array();
+        $returnArray['success'] = true;
+        $returnArray['data'] = 'none';
+        return new JsonModel($returnArray);
+    }
+
+
+
+}
