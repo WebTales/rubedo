15bbdb89777a424fe50be4ced1a78d23d0481ecd
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-02-08 14:19:49 +0100

    simplify code

--- module/Rubedo/src/Rubedo/Backoffice/Controller/ConfigController.php
+++ module/Rubedo/src/Rubedo/Backoffice/Controller/ConfigController.php
@@ -53,37 +53,7 @@ class ConfigController extends AbstractActionController
 
     public function updateAction()
     {
-        $isMultiNode=false;
-        $config=Manager::getService("config");
-        if (isset($config["webCluster"])&&is_array($config["webCluster"])){
-            $isMultiNode=true;
-        }
-        $isReplicated=$this->params()->fromPost("isReplicated",null);
-        if ($isMultiNode&&!$isReplicated){
-            $mainRequest=$this->getRequest();
-            $path=$mainRequest->getUri()->getPath();
-            $post=$mainRequest->getPost()->toArray();
-            $post["isReplicated"]=true;
-            $cookie=$mainRequest->getCookie()->__toString();
-            $lastResult=[];
-            $allResults=[];
-            $protocol=isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS'] ? "https://" : "http://";
-            foreach($config["webCluster"] as $clusterHost){
-                $curlUrl = $protocol . $clusterHost . $path;
-                $curly = curl_init();
-                curl_setopt($curly,CURLOPT_URL, $curlUrl);
-                curl_setopt($curly,CURLOPT_POST, true);
-                curl_setopt($curly,CURLOPT_POSTFIELDS, $post);
-                curl_setopt($curly,CURLOPT_COOKIE, $cookie);
-                curl_setopt($curly, CURLOPT_RETURNTRANSFER, true);
-                $result=Json::decode(curl_exec($curly),Json::TYPE_ARRAY);
-                $lastResult=$result;
-                $allResults[]=$result;
-                curl_close($curly);
-            }
-            $lastResult["clusterResults"]=$allResults;
-            return new JsonModel($lastResult);
-        } else {
+        
             $data = $this->params()->fromPost('data');
             if (empty($data)) {
                 $this->getResponse()->setStatusCode(500);
@@ -104,9 +74,8 @@ class ConfigController extends AbstractActionController
             if (isset($updateData["swiftmail"]["smtp"]) && is_array($updateData["swiftmail"]["smtp"])) {
                 $this->config["swiftmail"]["smtp"] = array_merge($this->config["swiftmail"]["smtp"], $updateData["swiftmail"]["smtp"]);
             }
-            $this->installObject->saveLocalConfig($this->config);
-            Manager::getService("MongoConf")->setRubedoConf($this->config);
+            $this->installObject->saveLocalConfig($this->config,true);
             return new JsonModel(array("success" => true));
-        }
+
     }
 }
