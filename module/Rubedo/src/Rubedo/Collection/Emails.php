b914f58328add774e61384a73c6d68e98401daaa
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-08-19 10:53:38 +0200

    fix email attach

--- module/Rubedo/src/Rubedo/Collection/Emails.php
+++ module/Rubedo/src/Rubedo/Collection/Emails.php
@@ -133,12 +133,11 @@ class Emails extends AbstractCollection implements IEmails
      */
     protected function attachImageFromFiles($id)
     {
-        $file = Manager::getService('Files')->findById($id);
-        if (!$file instanceof \MongoGridFSFile) {
+        $fs=Manager::getService("FSManager")->getFS();
+        if (!$fs->has($id)) {
             throw new \Rubedo\Exceptions\NotFound("No Image Found", "Exception8");
         }
-        $meta = $file->file;
-        return $this->swiftMessage->embed(Swift_Image::newInstance($file->getBytes(), str_shuffle("uvxyzabcdefghijklmnop") . $meta['filename'], $meta['Content-Type']));
+        return $this->swiftMessage->embed(Swift_Image::newInstance($fs->read($id), str_shuffle("uvxyzabcdefghijklmnop") . $id, $fs->getMimetype($id)));
     }
 
     /**
